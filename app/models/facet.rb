471e5b876e7c20572511079875f4956c6a42d7a4
Will Tomlins <will@tomlins.org.uk>
2014-01-27 16:01:48 +0000

    Facet initialises from a schema and takes a value

--- app/models/facet.rb
+++ app/models/facet.rb
@@ -2,6 +2,9 @@ class Facet
   attr_accessor :name, :key, :value
 
   def initialize(schema, value = nil)
+    @name = schema["name"]
+    @key = schema["key"]
+    @value = value.presence
   end
 
   def to_partial_path
