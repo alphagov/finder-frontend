6f3f85e3baea19f71305019ad117808b8ba73b15
Edd Sowden <e@e26.co.uk>
2014-02-07 16:29:32 +0000

    Favour markup over tag helpers
    It makes it easier to work out what is going on.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,19 +3,6 @@ module ApplicationHelper
     ['finder-page', finder.slug].join(' ')
   end
 
-  def document_metadata_for(document)
-    content_tag(
-      :dl,
-      Array(document.metadata).map { |metadata_entry|
-        [
-          content_tag(:dt, metadata_entry[:name]),
-          content_tag(:dd, document_metadata_value(metadata_entry[:value], metadata_entry[:type]))
-        ].join
-      }.join.html_safe
-    )
-  end
-
-private
   def document_metadata_value(value, type)
     case type
     when "date"
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,33 +1,11 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  describe ".document_metadata_for" do
+  describe ".document_metadata_value" do
     let(:document) { Document.new(metadata: metadata) }
     subject {
-      Nokogiri::HTML::DocumentFragment.parse(helper.document_metadata_for(document)).children.first
+      helper.document_metadata_value(document.metadata.first[:value], document.metadata.first[:type])
     }
-
-
-    context "with a document with text metadata" do
-      let(:metadata) { [
-        { type: 'text', name: 'case type', value: 'Merger inquiry' },
-        { type: 'text', name: 'document status', value: 'Archived' }
-      ] }
-
-      it "should render a dl with dt and dd as name and value respectively" do
-        subject.name.should == 'dl'
-
-        subject.children[0].name.should == 'dt'
-        subject.children[0].text.should == 'case type'
-        subject.children[1].name.should == 'dd'
-        subject.children[1].text.should == 'Merger inquiry'
-
-        subject.children[2].name.should == 'dt'
-        subject.children[2].text.should == 'document status'
-        subject.children[3].name.should == 'dd'
-        subject.children[3].text.should == 'Archived'
-      end
-    end
 
     context "with date type metadata" do
       let(:metadata) { [
@@ -35,7 +13,7 @@ describe ApplicationHelper do
       ] }
 
       it "should render the date as a long form date" do
-        subject.children[1].text.should == '30 December 2003'
+        subject.should == '30 December 2003'
       end
     end
   end
