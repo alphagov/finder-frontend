4d5950a9dd849abe26ec5892b155b45e1997cb8c
Edd Sowden <e@e26.co.uk>
2014-02-07 16:31:15 +0000

    Use assets group for asset gems
    So we don't need to include the gems in production

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups(assets: %w(development test)))
 
 module FinderFrontend
   class Application < Rails::Application
