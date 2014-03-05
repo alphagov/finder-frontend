b86c8022fe611360d1075512230894ff8ff6b511
Edd Sowden <e@e26.co.uk>
2014-03-05 11:18:27 +0000

    Use header-footer-only slimmer template
    So that we don't include lots of JavaScript and CSS which we don't use
    or need.
    
    The only obvious thing which needed fixing as a result of this was that
    the wrapper wasn't fiven a width.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  include Slimmer::Headers
+  before_filter :set_slimmer_headers
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper :application
+
+  def set_slimmer_headers
+    response.headers[Slimmer::Headers::TEMPLATE_HEADER] = "header_footer_only"
+  end
 end
