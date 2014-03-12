455d276948251e6ac535866c5d973d9c749ab266
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-03-12 10:42:37 +0000

    Fix rake asset:precompile
    When compiling assets the jasmine gem isn't defined because we
    specifically activate only the 'assets' gem group.
    
    The routes file tries to avoid mixing in the jasmine routes in this
    case, but this initializer causes the JasmineRails module to be always
    defined even when the jasmine gem isn't loaded.
    
    This changes the initializer to avoid defining the JasminRails module if
    it's not already defined.

--- config/initializers/jasmine_rails.rb
+++ config/initializers/jasmine_rails.rb
@@ -1,9 +1,11 @@
-module JasmineRails
-  class ApplicationController < ActionController::Base
-    before_filter :skip_slimmer
+if defined?(JasmineRails)
+  module JasmineRails
+    class ApplicationController < ActionController::Base
+      before_filter :skip_slimmer
 
-    def skip_slimmer
-      response.headers[Slimmer::Headers::SKIP_HEADER] = "true"
+      def skip_slimmer
+        response.headers[Slimmer::Headers::SKIP_HEADER] = "true"
+      end
     end
   end
 end
