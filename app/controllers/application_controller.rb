5400b5411cb9c3247d2600b69f1114b175d8fcd6
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-03-12 15:48:00 +0000

    Prevent google from crawling
    using a robots header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   include Slimmer::Headers
   before_filter :set_slimmer_headers
   before_filter :set_beta_notice
+  before_filter :set_robots_headers
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper :application
-
 
 private
   def set_slimmer_headers
@@ -16,5 +16,9 @@ private
 
   def set_beta_notice
     response.headers[Slimmer::Headers::BETA_HEADER] = "true"
+  end
+
+  def set_robots_headers
+    response.headers["X-Robots-Tag"] = "none"
   end
 end
