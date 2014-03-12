b18dacd26cd3b507fd748319a0b0c12042e426f3
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-03-12 09:51:31 +0000

    Add beta notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,20 @@
 class ApplicationController < ActionController::Base
   include Slimmer::Headers
   before_filter :set_slimmer_headers
+  before_filter :set_beta_notice
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper :application
 
+
+private
   def set_slimmer_headers
     response.headers[Slimmer::Headers::TEMPLATE_HEADER] = "header_footer_only"
+  end
+
+  def set_beta_notice
+    response.headers[Slimmer::Headers::BETA_HEADER] = "true"
   end
 end
