d87434c04e42fb7fa0dad48636aad20e1808abf9
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-03-28 17:35:12 +0000

    Remove beta banner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   include Slimmer::Headers
   before_filter :set_slimmer_headers
-  before_filter :set_beta_notice
   before_filter :set_robots_headers
 
   # Prevent CSRF attacks by raising an exception.
@@ -12,10 +11,6 @@ class ApplicationController < ActionController::Base
 private
   def set_slimmer_headers
     response.headers[Slimmer::Headers::TEMPLATE_HEADER] = "header_footer_only"
-  end
-
-  def set_beta_notice
-    response.headers[Slimmer::Headers::BETA_HEADER] = "true"
   end
 
   def set_robots_headers
