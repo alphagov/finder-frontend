c3211701198b829814367f3c1cc16fae6714a3a2
Dominic Baggott <dominic.baggott@gmail.com>
2014-03-28 21:21:20 +0000

    Add Logstasher
    With this Gem we can dump the Rails logs into logstash and make them
    available via Kibana.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,9 @@ FinderFrontend::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Enable JSON-style logging
+  config.logstasher.enabled = true
+  config.logstasher.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.json.log")
+  config.logstasher.suppress_app_log = true
 end
