0146c3d71c195b9ddccca83311e45ec70a015b13
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-03-31 14:17:28 +0100

    Use schema provided by finder-api over static file

--- lib/finder_frontend.rb
+++ lib/finder_frontend.rb
@@ -1,5 +1,7 @@
+require 'gds_api/finder_api'
+
 module FinderFrontend
   def self.finder_api
-    @finder_api ||= FinderApi.new
+    @finder_api ||= GdsApi::FinderApi.new(Plek.current.find('finder-api'))
   end
 end
