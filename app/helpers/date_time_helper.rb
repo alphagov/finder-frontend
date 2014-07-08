2f43b70f5b458ef082b86e17b13038ccb01ad117
Andrew Mitchell <themitchell@me.com>
2014-07-08 11:39:50 +0100

    Move time formatting to i18n

--- app/helpers/date_time_helper.rb
+++ app/helpers/date_time_helper.rb
@@ -2,7 +2,7 @@ module DateTimeHelper
 
   def formatted_date_html(time_string)
     time = Time.zone.parse(time_string.to_s)
-    html = content_tag(:time, time.strftime('%d %B %Y'), datetime: time.iso8601)
+    html = content_tag(:time, l(time, format: :as_date), datetime: time.iso8601)
     html.html_safe
   end
 
