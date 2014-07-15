7b937b7c8203bb407dcb3ccb204ab41c61f0ecf0
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-07-15 11:34:19 +0100

    Push date formatting onto SearchResultPresenter/template
    This makes the templates a bit more complex but stops us having to include all the helpers in the presenter which is a smell. Updated tests to follow.

--- app/presenters/search_result_presenter.rb
+++ app/presenters/search_result_presenter.rb
@@ -1,40 +1,47 @@
 class SearchResultPresenter
 
-  include ActionView::Context
-  include ActionView::Helpers::TagHelper
-  include ActionView::Helpers::TranslationHelper
-  include SpecialistDocumentsHelper
-  include DateTimeHelper
-
-  attr_reader :title, :slug, :formatted_metadata
+  attr_reader :title, :slug, :raw_metadata
 
   def initialize(search_result)
     @title = search_result.title
     @slug = search_result.slug
-    @formatted_metadata = format_metadata(search_result.metadata)
+    @raw_metadata = search_result.metadata
   end
 
   def to_hash
     {
       title: title,
       slug: slug,
-      metadata: formatted_metadata,
+      metadata: metadata,
     }
   end
 
-  def format_metadata(metadata)
-    data = metadata.map do |datum|
-      { html: document_metadata_attribute_html(datum[:name], datum[:type], datum[:value]) }
-    end
-    data
+  def metadata
+    raw_metadata.map { |datum|
+      case datum.fetch(:type)
+      when 'date'
+        build_date_metadata(datum)
+      when 'text'
+        build_text_metadata(datum)
+      end
+    }
   end
 
-  def  format_date_if_date(value, type)
-    case type
-    when "date"
-      Date.parse(value).strftime("%d %B %Y")
-    else
-      value
-    end
+  def build_text_metadata(data)
+    {
+      label: data.fetch(:name),
+      value: data.fetch(:value),
+      is_text: true,
+    }
+  end
+
+  def build_date_metadata(data)
+    date = Date.parse(data.fetch(:value))
+    {
+      label: data.fetch(:name),
+      is_date: true,
+      machine_date: date.iso8601,
+      human_date: date.strftime("%-d %B %Y"),
+    }
   end
 end
