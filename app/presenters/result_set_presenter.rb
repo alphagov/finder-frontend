9e2c2543b753b647dfd8b775dd4ec284ee97459a
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-07-15 11:38:37 +0100

    Move html building stuff into helpers

--- app/presenters/result_set_presenter.rb
+++ app/presenters/result_set_presenter.rb
@@ -1,8 +1,11 @@
 class ResultSetPresenter
-  include ActionView::Helpers::UrlHelper
-  attr_reader :result_count, :documents_noun, :applied_filters, :params, :result_set
+  include ApplicationHelper
+  include ActionView::Helpers
+
+  attr_reader :finder, :result_count, :documents_noun, :applied_filters, :params, :result_set
 
   def initialize(finder, facet_params)
+    @finder = finder
     @result_count = finder.results.count
     @result_set = finder.results
     @documents_noun = finder.document_noun
@@ -14,41 +17,16 @@ class ResultSetPresenter
     {
       count: result_count,
       pluralised_document_noun: documents_noun.pluralize(result_count),
-      applied_filters: describe_filters_in_sentence(applied_filters),
+      applied_filters: describe_filters_in_sentence,
       documents: documents,
     }
   end
 
-  def describe_filters_in_sentence(facets)
-     selections = facets.map do |facet|
-       "#{facet[:preposition]} #{facet_values_sentence(facet)}"
+  def describe_filters_in_sentence
+     selections = finder.facets.with_selected_values.map do |facet|
+       "#{facet.preposition} #{facet_values_sentence(facet)}"
      end
      selections.to_sentence
-   end
-
-  def facet_values_sentence(facet)
-    values = facet[:selected_values_hash].map do |option|
-
-      # build a link with out this facet selected
-      # make a content tag
-      # url_for(link_params_without_facet_value(facet.key, option.value))
-      link_url = link_params_without_facet_value(facet[:key], option[:value])
-      #content_tag(:strong, "#{option[:label]} #{link_to("×", link_url)}".html_safe)
-
-      content_tag(:strong, option[:label].html_safe)
-    end
-    values.to_sentence(last_word_connector: ' and ')
-  end
-
-  def link_params_without_facet_value(key, value)
-     new_params = params.dup
-     if new_params[key].is_a? Array
-       new_params[key] = new_params[key] - [value]
-       new_params.delete(key) if new_params[key].empty?
-     else
-       new_params.delete(key)
-     end
-     new_params
    end
 
    def documents
