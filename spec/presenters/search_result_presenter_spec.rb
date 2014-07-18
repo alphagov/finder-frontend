4e75ddc34b226195bc2db09f20fdfecd05d63749
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-07-18 11:34:15 +0100

    Update search_result_presenter tests
    Add new tests, remove old tests that are testing deleted code

--- spec/presenters/search_result_presenter_spec.rb
+++ spec/presenters/search_result_presenter_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SearchResultPresenter do
 
   subject(:presenter) { SearchResultPresenter.new(document)}
 
-  let(:document){
+  let(:document) {
     OpenStruct.new({
       title: title,
       slug: slug,
@@ -30,9 +30,9 @@ RSpec.describe SearchResultPresenter do
 
     let(:formatted_metadata) {
       [
-        { name: 'Case state', value: 'Open' },
-        { name: 'Opened date', value: '14 July 2006' },
-        { name: 'Case type', value: 'CA98 and civil cartels' },
+        { label: "Case state", value: "Open", is_text: true },
+        { label: "Opened date", is_date: true, machine_date: "2006-07-14", human_date: "14 July 2006" },
+        { label: "Case type", value: "CA98 and civil cartels", is_text: true },
       ]
     }
 
@@ -44,34 +44,51 @@ RSpec.describe SearchResultPresenter do
     end
   end
 
-  describe '#format_metadata' do
+  describe '#metadata' do
     it 'should return an array' do
-      subject.format_metadata(metadata).is_a?(Array).should == true
+      subject.metadata.is_a?(Array).should == true
     end
 
-    it 'should call format date on any date values' do
-      allow(presenter).to receive(:format_date_if_date).and_call_original
-      subject.format_metadata(metadata)
-      presenter.should have_received(:format_date_if_date).with 'Open', 'text'
-      presenter.should have_received(:format_date_if_date).with '2006-7-14', 'date'
-      presenter.should have_received(:format_date_if_date).with 'CA98 and civil cartels', 'text'
+    it 'should format metadata' do
+      allow(presenter).to receive(:build_text_metadata).and_call_original
+      allow(presenter).to receive(:build_date_metadata).and_call_original
+
+      subject.metadata
+      subject.should have_received(:build_date_metadata).with({:name=>"Opened date", :value=>"2006-7-14", :type=>"date"})
+      subject.should have_received(:build_text_metadata).with({:name=>"Case state", :value=>"Open", :type=>"text"})
+      subject.should have_received(:build_text_metadata).with({:name=>"Case state", :value=>"Open", :type=>"text"})
 
     end
   end
 
- describe '#format_date_if_date' do
-   let(:raw_date) { '2003-12-30' }
-   let(:formatted_date) { '30 December 2003' }
-   let(:a_date_type) { 'date'}
-   let(:not_a_date_type) { 'not a date' }
+  describe '#build_text_metadata' do
+    let(:data) { {name: 'some name', value: 'some value'} }
+    it 'should return a hash' do
+      subject.build_text_metadata(data).is_a?(Hash).should == true
+    end
+    it 'should set the type to text' do
+      subject.build_text_metadata(data).fetch(:is_text).should == true
+    end
+  end
 
-   it 'should return a formatted date if type = date' do
-     subject.format_date_if_date(raw_date, a_date_type).should == formatted_date
-   end
+  describe '#build_date_metadata' do
+    let(:data) { {name: 'some name', value: raw_date} }
+    let(:raw_date) { '2003-12-01' }
+    let(:formatted_date) { '1 December 2003' }
+    let(:iso_date) { '2003-12-01' }
 
-   it 'should return the unchanged value if type != date' do
-     subject.format_date_if_date(raw_date, not_a_date_type).should == raw_date
-   end
+    it 'should return a hash' do
+      subject.build_date_metadata(data).is_a?(Hash).should == true
+    end
 
- end
+    it 'should set the type to date' do
+      subject.build_date_metadata(data).fetch(:is_date).should == true
+    end
+
+    it 'should format the date' do
+      date_metadata = subject.build_date_metadata(data)
+      date_metadata.fetch(:human_date).should == formatted_date
+      date_metadata.fetch(:machine_date).should == iso_date
+    end
+  end
 end
