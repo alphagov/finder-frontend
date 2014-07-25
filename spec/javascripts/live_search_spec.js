55b48ab350e28663d193ca852a2965f75f44b8f3
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-07-25 15:01:18 +0100

    Update liveSearch tests to work with this project
    - Update tests to work with these templates
    - Remove tests for removed functionality
    - Fix some spelling errors in comments

--- spec/javascripts/live_search_spec.js
+++ spec/javascripts/live_search_spec.js
@@ -1,24 +1,43 @@
 describe("liveSearch", function(){
-  var $form, $results, _supportHistory;
+  var $form, $results, _supportHistory, liveSearch;
   var dummyResponse = {
-    "query":"fiddle",
-    "result_count_string":"1 result",
-    "result_count":1,
-    "results_any?":true,
-    "results":[
-      {"title":"my-title","link":"my-link","description":"my-description"}
+    "count":1,
+    "pluralised_document_noun":"reports",
+    "applied_filters":" \u003Cstrong\u003ECommercial - rotorcraft \u003Ca href='?format=json\u0026keywords='\u003E×\u003C/a\u003E\u003C/strong\u003E",
+    "documents":[
+      {
+        "title":"Test report",
+        "slug":"aaib-reports/test-report",
+        "metadata":[
+          {
+            "label":"Aircraft category",
+            "value":"General aviation - rotorcraft",
+            "is_text":true
+          },{
+            "label":"Report type",
+            "value":"Annual safety report",
+            "is_text":true
+          },{
+            "label":"Occurred",
+            "is_date":true,
+            "machine_date":"2013-11-03",
+            "human_date":"3 November 2013"
+          }
+        ]
+      }
     ]
   };
 
   beforeEach(function () {
     $form = $('<form action="/somewhere" class="js-live-search-form"><input type="checkbox" name="field" value="sheep" checked></form>');
-    $results = $('<div class="js-live-search-results-block"><div class="js-live-search-results-list">my result list</div></div>');
+    $results = $('<div class="js-live-search-results-block"></div>');
 
     $('body').append($form).append($results);
 
     _supportHistory = GOVUK.support.history;
     GOVUK.support.history = function(){ return true; };
-    GOVUK.liveSearch.resultCache = {};
+
+    liveSearch = new GOVUK.LiveSearch({$form: $form, $results: $results});
   });
 
   afterEach(function(){
@@ -28,169 +47,119 @@ describe("liveSearch", function(){
     GOVUK.support.history = _supportHistory;
   });
 
-  it("should save inital state", function(){
-    GOVUK.liveSearch.init();
-    expect(GOVUK.liveSearch.state).toEqual([{name: 'field', value: 'sheep'}]);
+  it("should save initial state", function(){
+    expect(liveSearch.state).toEqual([{name: 'field', value: 'sheep'}]);
   });
 
   it("should detect a new state", function(){
-    GOVUK.liveSearch.init();
-    expect(GOVUK.liveSearch.isNewState()).toBe(false);
+    expect(liveSearch.isNewState()).toBe(false);
     $form.find('input').prop('checked', false);
-    expect(GOVUK.liveSearch.isNewState()).toBe(true);
+    expect(liveSearch.isNewState()).toBe(true);
   });
 
   it("should update state to current state", function(){
-    GOVUK.liveSearch.init();
-    expect(GOVUK.liveSearch.state).toEqual([{name: 'field', value: 'sheep'}]);
+    expect(liveSearch.state).toEqual([{name: 'field', value: 'sheep'}]);
     $form.find('input').prop('checked', false);
-    GOVUK.liveSearch.saveState();
-    expect(GOVUK.liveSearch.state).toEqual([]);
+    liveSearch.saveState();
+    expect(liveSearch.state).toEqual([]);
   });
 
   it("should update state to passed in state", function(){
-    GOVUK.liveSearch.init();
-    expect(GOVUK.liveSearch.state).toEqual([{name: 'field', value: 'sheep'}]);
+    expect(liveSearch.state).toEqual([{name: 'field', value: 'sheep'}]);
     $form.find('input').prop('checked', false);
-    GOVUK.liveSearch.saveState({ my: "new", state: "object"});
-    expect(GOVUK.liveSearch.state).toEqual({ my: "new", state: "object"});
+    liveSearch.saveState({ my: "new", state: "object"});
+    expect(liveSearch.state).toEqual({ my: "new", state: "object"});
   });
 
   it("should not request new results if they are in the cache", function(){
-    GOVUK.liveSearch.resultCache["more=results"] = "exists";
-    GOVUK.liveSearch.state = { more: "results" };
-    spyOn(GOVUK.liveSearch, 'displayResults');
+    liveSearch.resultCache["more=results"] = "exists";
+    liveSearch.state = { more: "results" };
+    spyOn(liveSearch, 'displayResults');
     spyOn(jQuery, 'ajax');
 
-    GOVUK.liveSearch.updateResults();
-    expect(GOVUK.liveSearch.displayResults).toHaveBeenCalled();
+    liveSearch.updateResults();
+    expect(liveSearch.displayResults).toHaveBeenCalled();
     expect(jQuery.ajax).not.toHaveBeenCalled();
   });
 
   it("should return a promise like object if results are in the cache", function(){
-    GOVUK.liveSearch.resultCache["more=results"] = "exists";
-    GOVUK.liveSearch.state = { more: "results" };
-    spyOn(GOVUK.liveSearch, 'displayResults');
+    liveSearch.resultCache["more=results"] = "exists";
+    liveSearch.state = { more: "results" };
+    spyOn(liveSearch, 'displayResults');
     spyOn(jQuery, 'ajax');
 
-    var promise = GOVUK.liveSearch.updateResults();
+    var promise = liveSearch.updateResults();
     expect(typeof promise.done).toBe('function');
   });
 
   it("should return a promise like object if results aren't in the cache", function(){
-    GOVUK.liveSearch.state = { not: "cached" };
-    spyOn(GOVUK.liveSearch, 'displayResults');
+    liveSearch.state = { not: "cached" };
+    spyOn(liveSearch, 'displayResults');
     var ajaxCallback = jasmine.createSpyObj('ajax', ['done', 'error']);
-    ajaxCallback.done.andReturn(ajaxCallback);
-    spyOn(jQuery, 'ajax').andReturn(ajaxCallback);
+    ajaxCallback.done.and.returnValue(ajaxCallback);
+    spyOn(jQuery, 'ajax').and.returnValue(ajaxCallback);
 
-    GOVUK.liveSearch.updateResults();
+    liveSearch.updateResults();
     expect(jQuery.ajax).toHaveBeenCalledWith({url: '/somewhere.json', data: {not: "cached"}});
     expect(ajaxCallback.done).toHaveBeenCalled();
-    ajaxCallback.done.mostRecentCall.args[0]('response data')
-    expect(GOVUK.liveSearch.displayResults).toHaveBeenCalled();
-    expect(GOVUK.liveSearch.resultCache['not=cached']).toBe('response data');
-  });
-
-  it("should show and hide loading indicator when loading new results", function(){
-    GOVUK.liveSearch.state = { not: "cached" };
-    spyOn(GOVUK.liveSearch, 'displayResults');
-    spyOn(GOVUK.liveSearch, 'showLoadingIndicator');
-    spyOn(GOVUK.liveSearch, 'hideLoadingIndicator');
-    var ajaxCallback = jasmine.createSpyObj('ajax', ['done', 'error']);
-    ajaxCallback.done.andReturn(ajaxCallback);
-    spyOn(jQuery, 'ajax').andReturn(ajaxCallback);
-
-    GOVUK.liveSearch.updateResults();
-    expect(GOVUK.liveSearch.showLoadingIndicator).toHaveBeenCalled();
-    ajaxCallback.done.mostRecentCall.args[0]('response data')
-    expect(GOVUK.liveSearch.hideLoadingIndicator).toHaveBeenCalled();
+    ajaxCallback.done.calls.mostRecent().args[0]('response data')
+    expect(liveSearch.displayResults).toHaveBeenCalled();
+    expect(liveSearch.resultCache['not=cached']).toBe('response data');
   });
 
   it("should show error indicator when error loading new results", function(){
-    GOVUK.liveSearch.state = { not: "cached" };
-    spyOn(GOVUK.liveSearch, 'displayResults');
-    spyOn(GOVUK.liveSearch, 'showErrorIndicator');
+    liveSearch.state = { not: "cached" };
+    spyOn(liveSearch, 'displayResults');
+    spyOn(liveSearch, 'showErrorIndicator');
     var ajaxCallback = jasmine.createSpyObj('ajax', ['done', 'error']);
-    ajaxCallback.done.andReturn(ajaxCallback);
-    spyOn(jQuery, 'ajax').andReturn(ajaxCallback);
+    ajaxCallback.done.and.returnValue(ajaxCallback);
+    spyOn(jQuery, 'ajax').and.returnValue(ajaxCallback);
 
-    GOVUK.liveSearch.updateResults();
-    ajaxCallback.error.mostRecentCall.args[0]()
-    expect(GOVUK.liveSearch.showErrorIndicator).toHaveBeenCalled();
+    liveSearch.updateResults();
+    ajaxCallback.error.calls.mostRecent().args[0]()
+    expect(liveSearch.showErrorIndicator).toHaveBeenCalled();
   });
 
   it("should return cache items for current state", function(){
-    GOVUK.liveSearch.state = { not: "cached" };
-    expect(GOVUK.liveSearch.cache()).toBe(undefined);
-    GOVUK.liveSearch.cache('something in the cache');
-    expect(GOVUK.liveSearch.cache()).toBe('something in the cache');
+    liveSearch.state = { not: "cached" };
+    expect(liveSearch.cache('some-slug')).toBe(undefined);
+    liveSearch.cache('some-slug', 'something in the cache');
+    expect(liveSearch.cache('some-slug')).toBe('something in the cache');
   });
 
-  it("should return the search term from a state object", function(){
-    var state = [{ name: "q", value: "my-search-term" }, { name: "other", value: "something" }];
-
-    expect(GOVUK.liveSearch.searchTermValue(state)).toBe('my-search-term');
-    expect(GOVUK.liveSearch.searchTermValue(false)).toBe(false);
-    expect(GOVUK.liveSearch.searchTermValue(null)).toBe(false);
-  });
-
-  it("should only allow 15 organisations to be selected", function(){
-    var orgList = [];
-    for(var i=0;i<14;i++){ orgList.push( { name: 'filter_organisations[]' } ); }
-    spyOn(GOVUK.liveSearch.$form, 'serializeArray').andReturn(orgList);
-    spyOn(window, 'alert');
-    var event = jasmine.createSpyObj('event', ['preventDefault']);
-    expect(GOVUK.liveSearch.checkOrganisationLimit(event)).toBe(true);
-
-    orgList.push( { name: 'filter_organisations[]' } );
-    expect(GOVUK.liveSearch.checkOrganisationLimit(event)).toBe(false);
-  });
-
-  describe('with relevent dom nodes set', function(){
+  describe('with relevant dom nodes set', function(){
     beforeEach(function(){
-      GOVUK.liveSearch.$form = $form;
-      GOVUK.liveSearch.$resultsBlock = $results;
-      GOVUK.liveSearch.state = { field: "sheep" };
+      liveSearch.$form = $form;
+      liveSearch.$resultsBlock = $results;
+      liveSearch.state = { field: "sheep" };
     });
 
     it("should update save state and update results when checkbox is changed", function(){
       var promise = jasmine.createSpyObj('promise', ['done']);
-      spyOn(GOVUK.liveSearch, 'updateResults').andReturn(promise);
-      spyOn(GOVUK.liveSearch, 'pageTrack').andReturn(promise);
+      spyOn(liveSearch, 'updateResults').and.returnValue(promise);
+      //spyOn(liveSearch, 'pageTrack').and.returnValue(promise);
       $form.find('input').prop('checked', false);
 
-      GOVUK.liveSearch.checkboxChange();
-      expect(GOVUK.liveSearch.state).toEqual([]);
-      expect(GOVUK.liveSearch.updateResults).toHaveBeenCalled();
-      promise.done.mostRecentCall.args[0]();
-      expect(GOVUK.liveSearch.pageTrack).toHaveBeenCalled();
+      liveSearch.checkboxChange();
+      expect(liveSearch.state).toEqual([]);
+      expect(liveSearch.updateResults).toHaveBeenCalled();
+      promise.done.calls.mostRecent().args[0]();
+      //expect(liveSearch.pageTrack).toHaveBeenCalled();
     });
 
     it("should do nothing if state hasn't changed when a checkbox is changed", function(){
-      spyOn(GOVUK.liveSearch, 'updateResults');
-      GOVUK.liveSearch.checkboxChange();
-      expect(GOVUK.liveSearch.state).toEqual({ field: 'sheep'});
-      expect(GOVUK.liveSearch.updateResults).not.toHaveBeenCalled();
+      spyOn(liveSearch, 'updateResults');
+      liveSearch.checkboxChange();
+      expect(liveSearch.state).toEqual({ field: 'sheep'});
+      expect(liveSearch.updateResults).not.toHaveBeenCalled();
     });
 
     it("should display results from the cache", function(){
-      GOVUK.liveSearch.resultCache["the=first"] = dummyResponse;
-      GOVUK.liveSearch.state = { the: "first" };
-      GOVUK.liveSearch.displayResults();
-
-      expect($results.find('h3').text()).toBe('my-title');
-      expect($results.find('#js-live-search-result-count').text()).toMatch(/^\s+1 result/);
-    });
-
-    it("should restore checkbox values", function(){
-      GOVUK.liveSearch.state = [ { name: "field", value: "sheep" } ];
-      GOVUK.liveSearch.restoreCheckboxes();
-      expect($form.find('input').prop('checked')).toBe(true);
-
-      GOVUK.liveSearch.state = [ ];
-      GOVUK.liveSearch.restoreCheckboxes();
-      expect($form.find('input').prop('checked')).toBe(false);
+      liveSearch.resultCache["the=first"] = dummyResponse;
+      liveSearch.state = { the: "first" };
+      liveSearch.displayResults(dummyResponse);
+      expect($results.find('h3').text()).toBe('Test report');
+      expect($results.find('.result-count').text()).toMatch(/^\s*1\s*/);
     });
   });
 });
