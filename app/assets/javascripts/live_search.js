5fa0a8110f5cc26357e83dbb39ab97c73e3cdd2c
Alice Bartlett <alice.bartlett@digital.cabinet-office.gov.uk>
2014-07-31 11:59:03 +0100

    Add back in popState handling to JS
    - Still needs to update checkboxes

--- app/assets/javascripts/live_search.js
+++ app/assets/javascripts/live_search.js
@@ -19,7 +19,7 @@
     if(GOVUK.support.history()){
       this.saveState();
       this.$form.on('change', 'input[type=checkbox]', $.proxy(this.checkboxChange, this));
-      $(window).on('popstate', this.popState);
+      $(window).on('popstate', $.proxy(this.popState, this));
     } else {
       this.$form.find('.js-live-search-fallback').show();
     }
@@ -33,22 +33,15 @@
     this.state = state;
   };
 
-  // LiveSearch.prototype.popState = function popState(event){
-  //   if(event.originalEvent.state){
-  //     liveSearch.saveState(state);
-  //     liveSearch.updateResults();
-  //     liveSearch.restoreCheckboxes();
-  //     liveSearch.pageTrack();
-  //   }
-  // };
-  //
-  // LiveSearch.prototype.pageTrack = function pageTrack(){
-  //   if(window._gaq && _gaq.push){
-  //     _gaq.push(["_setCustomVar",5,"ResultCount",liveSearch.cache().result_count,3]);
-  //     _gaq.push(['_trackPageview']);
-  //   }
-  // };
-  //
+  LiveSearch.prototype.popState = function popState(event){
+    if(event.originalEvent.state){
+      this.saveState(event.originalEvent.state);
+      this.updateResults();
+      this.restoreCheckboxes();
+    }
+  };
+
+
   LiveSearch.prototype.checkboxChange = function checkboxChange(e){
     var pageUpdated;
     if(this.isNewState()){
@@ -58,7 +51,6 @@
         $.proxy(
           function(){
             history.pushState(this.state, '', window.location.pathname + "?" + $.param(this.state));
-            //liveSearch.pageTrack();
           },
           this)
       );
@@ -109,27 +101,25 @@
 
   LiveSearch.prototype.displayResults = function displayResults(results){
     this.$resultsBlock.mustache('finders/_results', results);
-    //this.$resultsBlock.find('.js-openable-filter').each(function(){
-    //  new GOVUK.CheckboxFilter({el:$(this)});
-    //})
   };
 
-  // LiveSearch.prototype.restoreCheckboxes = function restoreCheckboxes(){
-  //   liveSearch.$form.find('input[type=checkbox]').each(function(i, el){
-  //     var $el = $(el)
-  //     $el.prop('checked', liveSearch.isCheckboxSelected($el.attr('name'), $el.attr('value')));
-  //   });
-  // };
-  //
-  // LiveSearch.prototype.isCheckboxSelected = function isCheckboxSelected(name, value){
-  //   var i, _i;
-  //   for(i=0,_i=liveSearch.state.length; i<_i; i++){
-  //     if(liveSearch.state[i].name === name && liveSearch.state[i].value === value){
-  //       return true;
-  //     }
-  //   }
-  //   return false;
-  // };
+  LiveSearch.prototype.restoreCheckboxes = function restoreCheckboxes(){
+    var that = this;
+    this.$form.find('input[type=checkbox]').each(function(i, el){
+      var $el = $(el);
+      $el.prop('checked', that.isCheckboxSelected($el.attr('name'), $el.attr('value')));
+    });
+  };
+
+  LiveSearch.prototype.isCheckboxSelected = function isCheckboxSelected(name, value){
+    var i, _i;
+    for(i=0,_i=this.state.length; i<_i; i++){
+      if(this.state[i].name === name && this.state[i].value === value){
+        return true;
+      }
+    }
+    return false;
+  };
 
   GOVUK.LiveSearch = LiveSearch;
 }());
