7170ff9c7d3be382ccc4fe4789d15b4b93bd2e7e
Stephen Best <bestie@gmail.com>
2014-09-08 15:26:57 +0100

    Remove `published_at` from MHRA formats
    This information is not yet provided to Rummager

--- app/models/drug_safety_update.rb
+++ app/models/drug_safety_update.rb
@@ -4,21 +4,9 @@ class DrugSafetyUpdate < AbstractDocument
     attrs.fetch(:description)
   end
 
-  def self.date_metadata_keys
-    %w(
-      published_at
-    )
-  end
-
   def self.tag_metadata_keys
     %w(
       therapeutic_area
     )
-  end
-
-  def self.metadata_name_mappings
-    {
-      "published_at" => "Published",
-    }
   end
 end
--- app/models/medical_safety_alert.rb
+++ app/models/medical_safety_alert.rb
@@ -1,20 +1,8 @@
 class MedicalSafetyAlert < AbstractDocument
-  def self.date_metadata_keys
-    %w(
-      published_at
-    )
-  end
-
   def self.tag_metadata_keys
     %w(
       alert_type
       medical_specialism
     )
-  end
-
-  def self.metadata_name_mappings
-    {
-      "published_at" => "Published",
-    }
   end
 end
