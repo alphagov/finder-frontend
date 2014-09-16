ec3ea2a6aaa4d0fa19abbbf8da63704280d68660
Andrew Mitchell <themitchell@me.com>
2014-09-16 15:12:27 +0100

    Introduce email alert subscription workflow
    This is the basis of the workflow for fetching topic urls from govuk_delivery api. It would be nice to remove the reliance on the Finder so heavily however as an MVP this should be sufficient. We may require the finder later on.
    
    We have tried to decouple the EmailAlertSignupAPI wrapper from the Finder as much as possible so if we do wish to change this later it should be much simpler.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ FinderFrontend::Application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 
   get '/:slug' => 'finders#show', as: :finder
-  get '/:slug/email-signup' => 'finders#email_signup', as: :email_signup
+
+  get '/:slug/email-signup' => 'email_alert_subscriptions#new', as: :new_email_alert_subscriptions
+  post '/:slug/email-signup' => 'email_alert_subscriptions#create', as: :email_alert_subscriptions
 end
