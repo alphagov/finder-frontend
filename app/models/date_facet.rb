33761920f61433b3742edd3251584f05c3145526
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-09-22 15:11:18 +0100

    Add Date Facet sentence fragment and spec
    Also refactor the view to not break when there's no value present.

--- app/models/date_facet.rb
+++ app/models/date_facet.rb
@@ -3,7 +3,26 @@ class DateFacet < Facet
     serialized_values.join(",")
   end
 
+  def sentence_fragment
+    return nil unless present_values.any?
+
+    OpenStruct.new(
+      preposition: [preposition, additional_preposition].compact.join(' '),
+      values: value_fragments,
+    )
+  end
+
 private
+  def value_fragments
+    present_values.map { |k, date|
+      OpenStruct.new(
+        label: date.date.strftime("%e %B %Y"),
+        parameter_key: key,
+        other_params: present_values.reject { |key, _| key == k }
+      )
+    }
+  end
+
   def serialized_values
     present_values.map { |key, date|
       "#{key}:#{date.to_iso8601}"
@@ -24,6 +43,23 @@ private
 
   def safe_date_parse(date_string)
     DateInput.new(date_string) rescue nil
+  end
+
+  def additional_preposition
+    if present_values.length == 2
+      "between"
+    else
+      present_values.map { |k, _|
+        preposition_mappings[k]
+      }.first
+    end
+  end
+
+  def preposition_mappings
+    {
+      "from" => "after",
+      "to" => "before",
+    }.with_indifferent_access
   end
 
   class DateInput
