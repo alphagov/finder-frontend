20c409fc41e9ee897b096cd9a50f5a36fc6ab535
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-09-22 15:08:31 +0100

    Refactor Result Set Presenter and Finder
    With the new sentence generation by fragment,
    finder#facet_sentence_fragments returns an array containing all the
    fragments which is then joined together by result_set_presenter.
    Also with the link to the Finder without that filter applied, we now
    use the other_params portion of the sentence fragment.

--- spec/presenters/result_set_presenter_spec.rb
+++ spec/presenters/result_set_presenter_spec.rb
@@ -10,15 +10,69 @@ RSpec.describe ResultSetPresenter do
     OpenStruct.new({
       results: result_set,
       document_noun: document_noun,
-      facets: facets,
+      facets: [ a_facet, another_facet ],
       keywords: keywords,
     })
+  end
+
+  let(:a_facet) do
+    OpenStruct.new(
+      key:'key_1',
+      selected_values:[
+        OpenStruct.new({ value: 'ca98-and-civil-cartels', label: 'CA98 and civil cartels' }),
+        OpenStruct.new({ value: 'mergers', label: 'Mergers' }),
+      ],
+      sentence_fragment: [
+        OpenStruct.new(
+          preposition: 'of type',
+          values: [
+            OpenStruct.new(
+              label: 'CA98 and civil cartels',
+              parameter_key: 'key_1',
+              other_params: ['mergers'],
+            ),
+            OpenStruct.new(
+              label: 'Mergers',
+              parameter_key: 'key_1',
+              other_params: ['ca98-and-civil-cartels'],
+            ),
+          ]
+        )
+      ],
+    )
+  end
+
+  let(:another_facet) do
+    OpenStruct.new(
+      key:'key_2',
+      preposition: 'about',
+      selected_values: [
+        OpenStruct.new({ value: 'farming', label: 'Farming' }),
+        OpenStruct.new({ value: 'chemicals', label: 'Chemicals' }),
+      ],
+      sentence_fragment: [
+        OpenStruct.new(
+          preposition: 'about',
+          values: [
+            OpenStruct.new(
+              label: 'Farming',
+              parameter_key: 'key_2',
+              other_params: ['chemicals'],
+            ),
+            OpenStruct.new(
+              label: 'Chemicals',
+              parameter_key: 'key_2',
+              other_params: ['farming'],
+            ),
+          ]
+        )
+      ],
+    )
   end
 
   let(:keywords){ '' }
   let(:document_noun){ 'case' }
   let(:count) { 2 }
-  let(:facets) {[ :facet_1, :facet_2 ]}
 
   let(:result_set) do
     OpenStruct.new({ count: count, documents: [ document ] })
@@ -39,17 +93,17 @@ RSpec.describe ResultSetPresenter do
 
   describe '#to_hash' do
     before(:each) do
-      subject.stub(:describe_filters_in_sentence).and_return("a sentence summarising the selected filters")
-      subject.stub(:documents).and_return({ key: 'value' })
-      subject.stub(:any_filters_applied?).and_return(true)
+      presenter.stub(:describe_filters_in_sentence).and_return("a sentence summarising the selected filters")
+      presenter.stub(:documents).and_return({ key: 'value' })
+      presenter.stub(:any_filters_applied?).and_return(true)
     end
 
     it 'returns an appropriate hash' do
-      subject.to_hash[:count].should == count
-      subject.to_hash[:pluralised_document_noun].present?.should == true
-      subject.to_hash[:documents].present?.should == true
-      subject.to_hash[:applied_filters].present?.should == true
-      subject.to_hash[:any_filters_applied].present?.should == true
+      presenter.to_hash[:count].should == count
+      presenter.to_hash[:pluralised_document_noun].present?.should == true
+      presenter.to_hash[:documents].present?.should == true
+      presenter.to_hash[:applied_filters].present?.should == true
+      presenter.to_hash[:any_filters_applied].present?.should == true
     end
 
     it 'calls pluralize on the document noun with the results_count' do
@@ -73,51 +127,22 @@ RSpec.describe ResultSetPresenter do
 
   describe '#describe_filters_in_sentence' do
 
-    let(:facets) do
-       OpenStruct.new(with_selected_values: [ a_facet, another_facet ])
-     end
-
-     let(:a_facet) do
-       OpenStruct.new(
-         key:'key_1',
-         preposition: 'of type',
-         selected_values:
-           [
-             OpenStruct.new({ value: 'CA98 and civil cartels', label: '   ' }),
-             OpenStruct.new({ value: 'Mergers', label: '   '  }),
-           ]
-       )
-     end
-
-     let(:another_facet) do
-       OpenStruct.new(
-         key:'key_2',
-         preposition: 'about',
-         selected_values:
-           [
-             OpenStruct.new({ value: 'Farming', label: '   '  }),
-             OpenStruct.new({ value: 'Chemicals', label: '   '  }),
-           ]
-       )
-     end
-
     before(:each) do
-      subject.stub(:facet_values_sentence)
-      subject.stub(:link_without_facet_value)
+      presenter.stub(:link_without_facet_value)
+      finder.stub(:facet_sentence_fragments).and_return( [a_facet, another_facet].flat_map { |f| f.sentence_fragment }.compact )
     end
 
-    it 'calls facet_values_sentence for all selected_values in a facet' do
-      allow(presenter).to receive(:facet_values_sentence)
+    it 'calls selected_filter_descriptions' do
+      allow(presenter).to receive(:selected_filter_descriptions)
       presenter.describe_filters_in_sentence
-      facets.each do | facet |
-        presenter.should have_received(:facet_values_sentence).with facet
-      end
+
+      presenter.should have_received(:selected_filter_descriptions)
     end
 
     it 'includes prepositions for each facet' do
       sentence = presenter.describe_filters_in_sentence
-      facets.each do | facet |
-        sentence.include?(facet[:preposition]).should == true
+      finder.facet_sentence_fragments.each do | fragment |
+        sentence.include?(fragment[:preposition]).should == true
       end
     end
 
@@ -132,55 +157,57 @@ RSpec.describe ResultSetPresenter do
 
   describe '#facet_values_sentence' do
 
-    let(:a_facet) do
-       OpenStruct.new(
-         key:'key_1',
-         preposition: 'of type',
-         selected_values:
-           [
-             OpenStruct.new({ label: 'CA98 and civil cartels', value: 'ca98_and_civil_cartels' }),
-             OpenStruct.new({ label: 'Mergers', value: 'mergers'}),
-           ]
-       )
-     end
-
     before(:each) do
-      subject.stub(:link_params_without_facet_value).and_return({})
+      presenter.stub(:link_params_without_facet_value).and_return({})
+      finder.stub(:facet_sentence_fragments).and_return( [a_facet, another_facet].flat_map { |f| f.sentence_fragment }.compact )
+    end
+
+    let(:a_facet) do
+      OpenStruct.new(
+        sentence_fragment: OpenStruct.new(
+          preposition: 'about',
+          values: [
+            OpenStruct.new(
+              label: 'Farming',
+              parameter_key: 'key_1',
+              other_params: ['chemicals'],
+            ),
+            OpenStruct.new(
+              label: 'Chemicals',
+              parameter_key: 'key_1',
+              other_params: ['farming'],
+            ),
+          ]
+        )
+      )
     end
 
     it 'returns a string with all the facets passed to it in strong tags' do
-      subject.stub(:link_params_without_facet_value).and_return({param_1: 'one'})
+      presenter.stub(:link_params_without_facet_value).and_return({param_1: 'one'})
 
-      sentence = subject.facet_values_sentence(a_facet)
-      a_facet.selected_values.each do | value |
+      sentence = presenter.selected_filter_descriptions
+      a_facet.sentence_fragment.values.each do | value |
         sentence.include?("<strong>#{value.label}").should == true
-        sentence.include?(subject.link_params_without_facet_value.to_query).should == true
+        sentence.include?(presenter.link_params_without_facet_value.to_query).should == true
       end
     end
 
     it 'calls link_params_without_facet_value for each option in a facet' do
-      subject.facet_values_sentence(a_facet)
-      a_facet.selected_values.each do | value |
-        presenter.should have_received(:link_params_without_facet_value).with(a_facet.key, value.value)
+      presenter.selected_filter_descriptions
+      a_facet.sentence_fragment.values.each do | value |
+        presenter.should have_received(:link_params_without_facet_value).with(value.parameter_key, value.other_params)
       end
     end
   end
 
   describe '#link_params_without_facet_value' do
     let(:params){ { "first_key" => ["one", "two"], "second_key" => "three" } }
-    it "removes a string value" do
-      subject.link_params_without_facet_value('second_key', 'three').should == { "first_key" => ["one", "two"] }
+    it "removes an empty parameter" do
+      presenter.link_params_without_facet_value("second_key", []).should == { "first_key" => ["one", "two"] }
     end
 
-    it "removes a array value" do
-      subject.link_params_without_facet_value('first_key', 'two').should == { "first_key" => ["one"], "second_key" => "three" }
-    end
-
-    context 'has an array of one' do
-    let(:params){ { "first_key" => ["one"], "second_key" => "three" } }
-      it "removes an array of one item" do
-        subject.link_params_without_facet_value('first_key', 'one').should == { "second_key" => "three" }
-      end
+    it "removes a single value" do
+      presenter.link_params_without_facet_value('first_key', ["two"]).should == { "first_key" => ["two"], "second_key" => "three" }
     end
   end
 
@@ -190,7 +217,7 @@ RSpec.describe ResultSetPresenter do
         OpenStruct.new({ count: count, documents: [ document ] })
       end
       it 'creates a new search_result_presenter hash for each result' do
-        search_result_objects = subject.documents
+        search_result_objects = presenter.documents
         search_result_objects.count.should == 1
         search_result_objects.first.is_a?(Hash).should == true
       end
@@ -201,7 +228,7 @@ RSpec.describe ResultSetPresenter do
         OpenStruct.new({ count: count, documents: [ document, document, document ] })
       end
       it 'creates a new document for each result' do
-        search_result_objects = subject.documents
+        search_result_objects = presenter.documents
         search_result_objects.count.should == 3
       end
     end
