b29898c32b8de8e9606afbd369ec4572cfe0a1a9
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2014-11-04 11:29:01 +0000

    Beta label for certain Finders
    As we launch Finders which may be considered in Beta, we want a way to
    display this on a per finder basis. We set this in the metadata in
    Finder API, which gets sent the content store as part of the details
    hash.
    
    This commit sets this on the Finder object and displays the beta label
    if this is true.

--- app/parsers/finder_parser.rb
+++ app/parsers/finder_parser.rb
@@ -1,9 +1,10 @@
 module FinderParser
-  def self.parse(finder_hash)
+  def self.parse(content_item, finder_hash)
     Finder.new(
-      name: finder_hash['name'],
+      name: content_item['title'],
       slug: finder_hash['slug'],
       document_noun: finder_hash['document_noun'],
+      beta: content_item['details']['beta'],
       facets: FacetCollectionParser.parse(finder_hash['facets']),
       organisations: finder_hash['organisations'],
       related: finder_hash['related'],
