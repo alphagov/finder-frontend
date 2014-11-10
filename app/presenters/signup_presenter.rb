d94fb64122a3dc68142f987d5d3eaf2271ad5d1a
Tom Russell <tom@cishub.co.uk>
2014-11-10 16:28:32 +0000

    Update finder frontend email signup to match actual prototype.

--- app/presenters/signup_presenter.rb
+++ app/presenters/signup_presenter.rb
@@ -3,16 +3,12 @@ class SignupPresenter < Struct.new(:content_item)
     "#{name} emails"
   end
 
-  def title
-    "#{name} email alert subscription"
-  end
-
   def name
     content_item.title
   end
 
   def body
-    content_item.details["description"]
+    content_item.description
   end
 
   def choices?
