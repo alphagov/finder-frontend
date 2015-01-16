4cdc4755078bc07096915550f3ad2c288ed84258
James Darling <james@abscond.org>
2015-01-16 16:27:48 +0000

    Add validation for email signup checkboxes
    Provides an error to the user if there are checkboxes and they select
    none of them on email signup.

--- app/controllers/email_alert_subscriptions_controller.rb
+++ app/controllers/email_alert_subscriptions_controller.rb
@@ -7,17 +7,31 @@ class EmailAlertSubscriptionsController < ApplicationController
 
   def new
     if content
-      @signup = SignupPresenter.new(content)
+      @signup = signup_presenter
     else
       error_not_found
     end
   end
 
   def create
-    redirect_to email_alert_signup_api.signup_url
+    if valid_choices?
+      redirect_to email_alert_signup_api.signup_url
+    else
+      @signup = signup_presenter
+      @error_message = "Please choose an email alert"
+      render action: :new
+    end
   end
 
 private
+
+  def valid_choices?
+    available_choices.blank? || chosen_options.present?
+  end
+
+  def signup_presenter
+    @signup_presenter ||= SignupPresenter.new(content)
+  end
 
   def content
     @content ||= content_store.content_item(request.path)
@@ -35,20 +49,28 @@ private
     finder.document_type
   end
 
+  def available_choices
+    content.details.email_signup_choice
+  end
+
   def email_alert_signup_api
     EmailAlertSignupAPI.new(
       email_alert_api: email_alert_api,
       attributes: email_signup_attributes,
       subscription_list_title_prefix: content.details.subscription_list_title_prefix,
-      available_choices: content.details.email_signup_choice,
+      available_choices: available_choices,
       filter_key: content.details.email_filter_by,
     )
+  end
+
+  def chosen_options
+    params["filter"]
   end
 
   def email_signup_attributes
     {
       "format" => [finder_format],
-      "filter" => params["filter"],
+      "filter" => chosen_options,
     }
   end
 
