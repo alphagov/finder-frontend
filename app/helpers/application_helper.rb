33097c627bae123d1a571826aa2ea29a731178f6
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-01-27 18:41:33 +0000

    Remove some dead code
    I think this has been unused since: ee95969b32b4862c15bee05ac6d5e557918c1574

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,4 @@
 module ApplicationHelper
-  def finder_page_class(finder)
-    ['finder-page', finder.slug].join(' ')
-  end
-
   def input_checked(key, value)
     if facet_has_any_selected_values?(key)
       if params[key].is_a?(Array) && params[key].include?(value)
