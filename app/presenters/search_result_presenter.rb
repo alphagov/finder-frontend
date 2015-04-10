65500b1bfbe2a65e6150effef42640840105f861
Tommy Palmer <tommy.palmer@digital.cabinet-office.gov.uk>
2015-04-10 16:19:19 +0100

    Refactor search result presenter
    This commit moves the initialization of instance vars in the
    SearchResultPresenter to use delegation.

--- app/presenters/search_result_presenter.rb
+++ app/presenters/search_result_presenter.rb
@@ -1,20 +1,13 @@
 class SearchResultPresenter
 
-  attr_reader :title,
-              :link,
-              :raw_metadata,
-              :summary,
-              :is_historic,
-              :government_name,
+  delegate :title,
+           :summary,
+           :is_historic,
+           :government_name,
+           to: :search_result
 
   def initialize(search_result)
-    @title = search_result.title
-    @link = search_result.path
-    @summary = search_result.summary
-
-    @raw_metadata = search_result.metadata
-    @is_historic = search_result.is_historic
-    @government_name = search_result.government_name
+    @search_result = search_result
   end
 
   def to_hash
@@ -26,6 +19,10 @@ class SearchResultPresenter
       government_name: government_name,
       metadata: metadata,
     }
+  end
+
+  def link
+    search_result.path
   end
 
   def metadata
@@ -55,5 +52,12 @@ class SearchResultPresenter
       machine_date: date.iso8601,
       human_date: date.strftime("%-d %B %Y"),
     }
+  end
+
+private
+  attr_reader :search_result
+
+  def raw_metadata
+    search_result.metadata
   end
 end
