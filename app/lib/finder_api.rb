a360a62c0e764bf84b2feeef5f2d478b2441dbd7
Dominic Baggott <dominic.baggott@gmail.com>
2015-05-21 16:17:22 +0100

    Add `FinderApi`
    This provides a `#fetch` method which takes a base_path for the finder
    and the user params and gets the finder from content store and the
    search results from rummager, and combines them together into a single
    response.
    
    It's using `FinderPresenter` to provide the parameter parsing logic,
    which is the next thing that needs to be fixed.

---
+++ app/lib/finder_api.rb
@@ -0,0 +1,48 @@
+require 'delegate'
+
+class FinderApi
+  def initialize(content_api:, search_api:)
+    @content_api = content_api
+    @search_api = search_api
+  end
+
+  def fetch(base_path, params)
+    content_item = fetch_content_item(base_path)
+    results = fetch_search_results(content_item, params)
+
+    ResultsDecorator.new(
+      content_item,
+      results,
+    )
+  end
+
+private
+  attr_reader :content_api, :search_api
+
+  def fetch_content_item(base_path)
+    content_api.content_item!(base_path)
+  end
+
+  def fetch_search_results(content_item, params)
+    # TODO This is very temporary. The necessary functionality in
+    # `FinderPresenter` should be extracted and added to `SearchQueryBuilder`
+    finder = FinderPresenter.new(content_item, params)
+    query = SearchQueryBuilder.new(
+      base_filter: finder.filter.to_h,
+      metadata_fields: finder.facet_keys,
+      default_order: finder.default_order,
+      params: finder.search_params,
+    ).call
+
+    search_api.unified_search(query).to_hash
+  end
+
+  class ResultsDecorator < SimpleDelegator
+    attr_reader :results
+
+    def initialize(finder, results)
+      super(finder)
+      @results = results
+    end
+  end
+end
