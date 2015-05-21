f94db443c2aec86cc4f22f76c7142ea52765072e
Dominic Baggott <dominic.baggott@gmail.com>
2015-05-21 14:42:53 +0100

    Move `SearchQueryBuilder` into its own file

---
+++ app/lib/search_query_builder.rb
@@ -0,0 +1,67 @@
+class SearchQueryBuilder
+  def initialize(base_filter:, metadata_fields:, default_order:, params:)
+    @base_filter = base_filter
+    @metadata_fields = metadata_fields
+    @default_order = default_order || "-public_timestamp"
+    @params = params
+  end
+
+  def call
+    default_params.merge(massaged_params)
+  end
+
+private
+
+  attr_reader :base_filter, :metadata_fields, :default_order, :params
+
+  def default_params
+    {
+      "count"  => "1000",
+      "fields" => return_fields.join(","),
+    }
+  end
+
+  def return_fields
+    base_return_fields.concat(metadata_fields).uniq
+  end
+
+  def base_return_fields
+    %w(
+      title
+      link
+      description
+      public_timestamp
+    )
+  end
+
+  def massaged_params
+    keyword_param
+      .merge(filter_params)
+      .merge(order_param)
+  end
+
+  def keyword_param
+    if params.has_key?("keywords")
+      {"q" => params.fetch("keywords")}
+    else
+      {}
+    end
+  end
+
+  def order_param
+    if params.has_key?("keywords")
+      {}
+    else
+      {"order" => default_order}
+    end
+  end
+
+  def filter_params
+    params
+      .except("keywords")
+      .merge(base_filter)
+      .reduce({}) { |memo, (k,v)|
+        memo.merge("filter_#{k}" => v)
+      }
+  end
+end
