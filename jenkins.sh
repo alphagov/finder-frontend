3f9128c171b4a5f87706832c46be72c6995f6240
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-11-22 14:18:02 +0000

    Change default govuk-content-schemas branch
    Change the branch from master to deployed-to-production. This will help
    prevent changes being merged in to the Finder Frontend app that don't
    work with the currently deployed govuk-content-schemas (this changes
    comes from GOV.UK RFC 59).

--- jenkins.sh
+++ jenkins.sh
@@ -40,7 +40,7 @@ bundle exec rake assets:precompile
 
 # Clone govuk-content-schemas depedency for tests
 rm -rf tmp/govuk-content-schemas
-git clone git@github.com:alphagov/govuk-content-schemas.git tmp/govuk-content-schemas
+git clone --branch deployed-to-production git@github.com:alphagov/govuk-content-schemas.git tmp/govuk-content-schemas
 
 RAILS_ENV=test GOVUK_CONTENT_SCHEMAS_PATH=tmp/govuk-content-schemas bundle exec rake
 
