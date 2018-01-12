024b58bfb05dce9d7706fbd680ff45ca44389d9a
Paul Hayes <fofr@users.noreply.github.com>
2018-01-12 12:05:25 +0000

    Update option-select component test to use RSpec

--- spec/components/option_select_spec.rb
+++ spec/components/option_select_spec.rb
@@ -1,8 +1,12 @@
-require 'govuk_component_test_helper'
+require 'spec_helper'
 
-class OptionSelectTestCase < ComponentTestCase
+describe 'components/_option-select.html.erb', type: :view do
   def component_name
-    "option-select"
+    'option-select'
+  end
+
+  def render_component(component_arguments)
+    render partial: "components/#{component_name}", locals: component_arguments
   end
 
   def option_key
@@ -29,64 +33,64 @@ class OptionSelectTestCase < ComponentTestCase
     }
   end
 
-  test "renders a heading for the option select box containing the title" do
+  it "renders a heading for the option select box containing the title" do
     render_component(option_select_arguments)
-    assert_select ".option-select-label", text: "Market sector"
+    expect(rendered).to have_selector(".option-select-label", text: 'Market sector')
   end
 
-  test "renders a container with the id passed in" do
+  it "renders a container with the id passed in" do
     render_component(option_select_arguments)
-    assert_select "\#list-of-sectors.options-container"
+    expect(rendered).to have_selector("\#list-of-sectors.options-container")
   end
 
-  test "renders a list of checkboxes" do
+  it "renders a list of checkboxes" do
     render_component(option_select_arguments)
 
-    assert_label_and_checkbox("Aerospace", "aerospace", "aerospace")
-    assert_label_and_checkbox("Label", "ID", "value")
+    expect_label_and_checkbox("Aerospace", "aerospace", "aerospace")
+    expect_label_and_checkbox("Label", "ID", "value")
   end
 
-  test "can set checkboxes to be pre-selected" do
+  it "can set checkboxes to be pre-selected" do
     arguments = option_select_arguments
     arguments[:options][0][:checked] = true
     arguments[:options][1][:checked] = true
     render_component(arguments)
 
-    assert_label_and_checked_checkbox("Aerospace", "aerospace", "aerospace")
-    assert_label_and_checked_checkbox("Label", "ID", "value")
+    expect_label_and_checked_checkbox("Aerospace", "aerospace", "aerospace")
+    expect_label_and_checked_checkbox("Label", "ID", "value")
   end
 
-  test "can indicate that the checkboxes control content displayed via aria-controls-id" do
+  it "can indicate that the checkboxes control content displayed via aria-controls-id" do
     arguments = option_select_arguments
     arguments[:aria_controls_id] = "aria-controls-id"
     render_component(arguments)
 
-    assert_select '[data-input-aria-controls="aria-controls-id"]', count: 1
+    expect(rendered).to have_selector '[data-input-aria-controls="aria-controls-id"]', count: 1
   end
 
-  test "can begin with the options box closed on load" do
+  it "can begin with the options box closed on load" do
     arguments = option_select_arguments
     arguments[:closed_on_load] = true
     render_component(arguments)
 
-    assert_select '.app-c-option-select[data-closed-on-load="true"]'
+    expect(rendered).to have_selector('.app-c-option-select[data-closed-on-load="true"]')
   end
 
-  def assert_label_and_checked_checkbox(label, id, value)
-    assert_label_and_checkbox(label, id, value, true)
+  def expect_label_and_checked_checkbox(label, id, value)
+    expect_label_and_checkbox(label, id, value, true)
   end
 
-  def assert_label_and_checkbox(label, id, value, checked = false)
+  def expect_label_and_checkbox(label, id, value, checked = false)
     expected_name = "[name='#{option_key}[]']"
     expected_id = "[id='#{option_key}-#{id}']"
     expected_value = "[value='#{value}']"
     expected_checked = checked ? "[checked='checked']" : ""
 
-    assert_select expected_name
-    assert_select expected_id
-    assert_select expected_value
+    expect(rendered).to have_selector expected_name
+    expect(rendered).to have_selector expected_id
+    expect(rendered).to have_selector expected_value
 
-    assert_select "label[for='#{option_key}-#{id}']", text: label
-    assert_select "input[type='checkbox']#{expected_name}#{expected_id}#{expected_value}#{expected_checked}"
+    expect(rendered).to have_selector "label[for='#{option_key}-#{id}']", text: label
+    expect(rendered).to have_selector "input[type='checkbox']#{expected_name}#{expected_id}#{expected_value}#{expected_checked}"
   end
 end
