1028a8669fbbef65ae72016ca06537404426f894
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2018-11-19 16:23:30 +0000

    Add class to generate subscription email title
    Co-Authored-By: Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>

---
+++ lib/email_alert_title_builder.rb
@@ -0,0 +1,61 @@
+class EmailAlertTitleBuilder
+  def self.call(*args)
+    new(*args).call
+  end
+
+  def initialize(filter:, subscription_list_title_prefix:, facets:)
+    @filter = filter
+    @subscription_list_title_prefix = subscription_list_title_prefix
+    @facets = facets
+  end
+
+  def call
+    (prefix.to_s + suffix.to_s).upcase_first
+  end
+
+private
+
+  attr_reader :filter, :subscription_list_title_prefix, :facets
+
+  def topic_names(facet)
+    filter.fetch(facet["facet_id"], []).map { |key| choice_hash_by_key(facet, key)["topic_name"] }
+  end
+
+  def topic_names_sentence(facet)
+    topic_names(facet).to_sentence
+  end
+
+  def choice_hash_by_key(facet, key)
+    facet["facet_choices"].detect { |choice| choice["key"] == key }
+  end
+
+  def selected_facets
+    facets.select { |facet| filter[facet["facet_id"]].present? }
+  end
+
+  def plural_or_single
+    if filter.fetch(facets.first["facet_id"], []).length == 1
+      "singular"
+    else
+      "plural"
+    end
+  end
+
+  def prefix
+    if facets.size == 1
+      subscription_list_title_prefix[plural_or_single].to_s
+    elsif selected_facets.empty?
+      subscription_list_title_prefix.to_s
+    elsif subscription_list_title_prefix
+      "#{subscription_list_title_prefix}with "
+    end
+  end
+
+  def suffix
+    if facets.size == 1
+      topic_names_sentence(facets.first)
+    elsif selected_facets.present?
+      selected_facets.map { |facet| facet["facet_name"] + " of " + topic_names_sentence(facet) }.to_sentence
+    end
+  end
+end
