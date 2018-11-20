70a2692ea9cac2e79c2cf2eecbf6385fbd018444
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-11-20 11:51:36 +0000

    Don't send nil tags to email-alert-api

--- lib/email_alert_signup_api.rb
+++ lib/email_alert_signup_api.rb
@@ -33,7 +33,9 @@ private
     attributes.dup.tap do |massaged_attributes|
       available_choices.each do |choice|
         facet_id = choice["facet_id"]
-        massaged_attributes[facet_id] = (massaged_attributes['filter'] || {})[facet_id]
+        value = (massaged_attributes['filter'] || {})[facet_id]
+        next unless value
+        massaged_attributes[facet_id] = value
       end
       massaged_attributes.delete("filter")
     end
