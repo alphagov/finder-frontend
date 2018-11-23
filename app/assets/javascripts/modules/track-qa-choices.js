bed2ad90c7a98d84aa7f8fe010043910b57120e8
Steve Laing <steve.laing@gmail.com>
2018-11-23 13:35:00 +0000

    Track QA choices as events
    Taking the same approach as is used to track radio buttons
    we can submit events for each checked option when the QA
    form is submitted.

---
+++ app/assets/javascripts/modules/track-qa-choices.js
@@ -0,0 +1,30 @@
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {};
+
+(function (global, GOVUK) {
+  'use strict'
+
+  var $ = global.jQuery
+
+  GOVUK.Modules.TrackQaChoices = function () {
+    this.start = function (element) {
+      track(element)
+    }
+
+    function track (element) {
+      element.on('submit', function (event) {
+        var $checkedOption, eventLabel, options
+        var $submittedForm = $(event.target)
+        var $checkedOptions = $submittedForm.find('input:checked')
+
+        $checkedOptions.each(function(index) {
+          $checkedOption = $(this)
+          eventLabel = $checkedOption.attr('name').replace('[]', '')
+          options = { transport: 'beacon', label: eventLabel }
+
+          GOVUK.analytics.trackEvent('QA option chosen', $checkedOption.val(), options)
+        })
+      })
+    }
+  }
+})(window, window.GOVUK);
