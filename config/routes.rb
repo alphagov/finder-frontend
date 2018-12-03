003e00791724a36c6c3104b0d39d2b34b5588b83
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2018-12-03 12:59:26 +0000

    Adds UK nations in the EU triage question / answer to content page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ FinderFrontend::Application.routes.draw do
   # Q&A frontend for "Find EU Exit guidance for your business" (www.gov.uk/find-eu-exit-guidance-business)
   get '/prepare-business-uk-leaving-eu' => 'qa#show'
 
+  # Q&A frontend for "UK Nationals in the EU" (www.gov.uk/uk-nationals-in-the-eu)
+  get '/uk-nationals-living-in-the-eu' => 'qa_to_content#show'
+
   get '/search/advanced' => 'advanced_search_finder#show'
 
   get '/*slug' => 'finders#show', as: :finder
