1cfb2c913e06cf56922f1d1037c8b7a29ba0dbe6
William Franklin <william.franklin@digital.cabinet-office.gov.uk>
2019-01-16 11:20:57 +0000

    Add default preposition
    This will allow no preposition to be set, which some finder
    content items dont have

--- app/models/filterable_facet.rb
+++ app/models/filterable_facet.rb
@@ -1,8 +1,10 @@
 class FilterableFacet < Facet
   attr_accessor :value
 
+  DEFAULT_PREPOSITION = 'related to'.freeze
+
   def preposition
-    facet['preposition']
+    facet['preposition'] || DEFAULT_PREPOSITION
   end
 
   def to_partial_path
