2ef7b285b1975bc56dbf60472840788d8d1734a3
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-01-23 11:19:57 +0000

    Fix autocomplete clearing

--- app/assets/javascripts/modules/remove-filter.js
+++ app/assets/javascripts/modules/remove-filter.js
@@ -11,46 +11,49 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
 
     function toggleFilter(e) {
       e.preventDefault();
+      var $el = $(e.target);
 
-      var removeFilterName = $(this).data('name');
-      var removeFilterValue = $(this).data('value');
-      var removeFilterFacet = $(this).data('facet');
-      var removeFilterAutocomplete = !!$('#' + removeFilterFacet +'__listbox').length;
-      
-      var $input = getInput(removeFilterName, removeFilterValue, removeFilterFacet, removeFilterAutocomplete);
+      var removeFilterName = $el.data('name');
+      var removeFilterValue = $el.data('value');
+      var removeFilterFacet = $el.data('facet');
+      var isAutoComplete = !!$('#' + removeFilterFacet +'-select').length;
 
-      var elementType = $input.prop('tagName');
-      var inputType = $input.prop('type');
-
-      setInputState(elementType, inputType, $input, removeFilterValue, removeFilterFacet, removeFilterAutocomplete);
+      var $input = getInput(removeFilterName, removeFilterValue, removeFilterFacet, isAutoComplete);
+      clearFacet($input, isAutoComplete, removeFilterValue, removeFilterFacet);
     }
 
-    function setInputState(elementType, inputType, $input, removeFilterValue, removeFilterFacet, removeFilterAutocomplete) {
+    function clearFacet($input, isAutoComplete, removeFilterValue, removeFilterFacet) {
+      var elementType = $input.prop('tagName');
+      var inputType = $input.prop('type');
+      var currentVal = $input.val();
+
       if (inputType == 'checkbox') {
         $input.prop("checked", false);
         $input.trigger('change');
       }
       else if (inputType == 'text' || inputType == 'search') {
-        var currentVal = $input.val();
-        var valToReplace = removeFilterAutocomplete ? currentVal : removeFilterValue;
-        var newVal = $.trim(currentVal.replace(valToReplace, ''));
-
-        $input.val(newVal).trigger('change');
+        if (isAutoComplete) {
+          var onConfirm = $('#' + $input.attr('id') + '-select').data('onconfirm'); // get the onConfirm function for the autocomplete
+          $input.val('');
+          onConfirm('', true); // call autocomplete onConfirm to clear it and hide the suggestions menu
+        } else {
+          $input.val(currentVal.replace(removeFilterValue, '').replace(/\s+/g,' ').trim()).change();
+        }
       }
       else if (elementType == 'OPTION') {
-        $('#' + removeFilterFacet).val("").trigger('change');
+        $('#' + removeFilterFacet).val('').trigger('change');
       }
-    };
+    }
 
-    function getInput(removeFilterName, removeFilterValue, removeFilterFacet, removeFilterAutocomplete) {
+    function getInput(removeFilterName, removeFilterValue, removeFilterFacet, isAutoComplete) {
       var selector = (!!removeFilterName) ? " input[name='" + removeFilterName + "']" : " [value='" + removeFilterValue + "']";
 
-      if (removeFilterAutocomplete) {
+      if (isAutoComplete) {
         return $('#' + removeFilterFacet);
       }
       else {
         return $('#' + removeFilterFacet).find(selector);
       }
     }
-  }
+  };
 })(window, window.GOVUK);
