a59a4d2cbb4389121b8f4ba72ccc93f69cde7944
Vanita Barrett <vanita.barrett@digital.cabinet-office.gov.uk>
2019-02-06 15:16:11 +0000

    Expand stop words

--- app/lib/search_query_builder.rb
+++ app/lib/search_query_builder.rb
@@ -131,14 +131,28 @@ private
   end
 
   def remove_brexit_terms
-    refined_keywords = keywords.downcase
-    refinements = [" brexit ", " eu ", " exit ", " after "]
-    refinements.each {|refinement| refined_keywords.gsub!(refinement, "")}
+    # Borrowed from Oscar Wyatt's list of stop words in his search prototype PLUS some brexit-y terms
+    stop_words = %w(a about above after again against all am an and any are arent as at be
+      because been before being below between both but by cant cannot could couldnt
+      did didnt do does doesnt doing dont down during each few for from further
+      had hadnt has hasnt have havent having he hed hell hes her here heres hers
+      herself him himself his how hows i id ill im ive if in into is isnt it
+      its its itself lets me more most mustnt my myself no nor not of off on
+      once only or other ought our oursourselves out over own same shant she shed
+      shell shes should shouldnt so some such than that thats the their theirs them
+      themselves then there theres these they theyd theyll theyre theyve this those through
+      to too under until up very was wasnt we wed well were weve were werent what
+      whats when whens where wheres which while who whos whom why whys with wont would
+      wouldnt you youd youll youre youve your yours yourself yourselves brexit eu exit after can i use)
 
-    # Also make driving licence -> "driving licence"
-    #refined_keywords = refined_keywords.gsub("driving licence", "\"driving licence\"")
+    removed_punctuation = keywords.gsub(/[^a-z0-9\s]/i, '')
+    filtered_keywords = ""
 
-    refined_keywords.strip
+    removed_punctuation.downcase.split(" ").each do |word|
+      filtered_keywords << word + " " unless stop_words.include?(word)
+    end
+
+    filtered_keywords
   end
 
   def keywords
