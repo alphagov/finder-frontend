0b68af9ee32b3301be6cd658dc569fe23b3db246
William Franklin <william.franklin@digital.cabinet-office.gov.uk>
2019-02-14 13:31:00 +0000

    Add PeopleRegistry
    This new registry will enable us to provide values
    when provided with people slugs (either in email
    alert title builder or by finder facets).
    
    We have similar registries for e.g. organisations so
    this is a continuation of that pattern.
    
    https://trello.com/c/J6ZHmdgi/349-improve-email-signups-description
    
    A followup commit will make use of this registry
    in email alert title builder.
    
    Authors: Fli.* & Bill F

---
+++ spec/lib/registries/people_registry_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+RSpec.describe Registries::PeopleRegistry do
+  let(:slug) { 'cornelius-fudge' }
+  let(:default_rummager_params) {
+    {
+      "count" => 1500,
+      "fields" => %w(slug title),
+      "filter_format" => "person"
+    }
+  }
+  let(:rummager_params) { default_rummager_params.merge("start" => 0) }
+  let(:second_rummager_params) { default_rummager_params.merge("start" => 1) }
+  let(:rummager_url) { "#{Plek.current.find('search')}/search.json?#{rummager_params.to_query}" }
+  let(:second_rummager_url) { "#{Plek.current.find('search')}/search.json?#{second_rummager_params.to_query}" }
+
+  describe "when rummager is available" do
+    before do
+      stub_request(:get, rummager_url).to_return(body: rummager_results)
+      stub_request(:get, second_rummager_url).to_return(body: { "results": [] }.to_json)
+      clear_cache
+    end
+
+    it "will fetch person breadcrumb information by slug" do
+      person = described_class.new[slug]
+      expect(person).to eq(
+        'title' => 'Cornelius Fudge',
+        'slug' => slug
+      )
+    end
+  end
+
+  describe "when rummager is unavailable" do
+    before do
+      rummager_is_unavailable
+      clear_cache
+    end
+
+    it "will return an (uncached) empty hash" do
+      person = described_class.new[slug]
+      expect(person).to be_nil
+      expect(Rails.cache.fetch(described_class::CACHE_KEY)).to be_nil
+    end
+  end
+
+  def rummager_is_unavailable
+    stub_request(:get, rummager_url).to_return(status: 500)
+  end
+
+  def clear_cache
+    Rails.cache.delete(described_class::CACHE_KEY)
+  end
+
+  def rummager_results
+    %|{
+      "results": [
+        {
+          "title": "Cornelius Fudge",
+          "slug": "cornelius-fudge",
+          "_id": "a field that we're not using"
+        },
+        {
+          "title": "Rufus Scrimgeour",
+          "slug": "rufus-scrimgeour",
+          "_id": "/government/people/companies-house"
+        }
+      ],
+      "total": 2,
+      "start": 0,
+      "aggregates": {},
+      "suggested_queries": []
+    }|
+  end
+end
