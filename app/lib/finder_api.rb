3690294664c303d214153b89df44c98e2ce902b9
Oscar Wyatt <oscar.wyatt@digital.cabinet-office.gov.uk>
2019-03-06 15:19:18 +0000

    Adds email subscription to policy papers and consultations finder Trello: https://trello.com/c/tIXZB4LG/482-make-post-review-changes-to-policy-papers-and-consultations-finder

--- app/lib/finder_api.rb
+++ app/lib/finder_api.rb
@@ -165,7 +165,7 @@ private
   # "/guidance-and-regulation" => "guidance_and_regulation"
   FINDERS_IN_DEVELOPMENT = {
     "/policy-papers-and-consultations" => 'policy_and_engagement',
-    "/statistics" => "statistics"
+    "/statistics" => "statistics",
   }.freeze
 
   def development_env_finder_json
