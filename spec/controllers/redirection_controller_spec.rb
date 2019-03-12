d667485229d4d261a96a3783cbf882f41cdd1d12
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2019-03-12 10:53:42 +0000

    Adds in routes for upcoming and published stats
    Adds additional redirect routes from the old statistics finder, converting the upcoming and published pages into equivalent radio button selections.
    This also includes some refactoring so that common parameters are shared
    across each route.
    
    Changes reroutes to search/ paths instead
    
    Refactors tests to match requirements
    
    Fixes incoming parameters for upcoming_publications route
    
    Adds a method that handles common parameters

--- spec/controllers/redirection_controller_spec.rb
+++ spec/controllers/redirection_controller_spec.rb
@@ -62,4 +62,57 @@ describe RedirectionController, type: :controller do
       expect(response).to redirect_to finder_path('search/all', format: :atom, params: { keywords: %w[one two] })
     end
   end
+
+  describe '#published_statistics' do
+    it "redirects to the statistics page" do
+      get :published_statistics
+      expect(response).to redirect_to finder_path('search/statistics')
+    end
+    it 'passes on a set of params' do
+      get :published_statistics, params: {
+        keywords: %w[one two],
+        taxons: %w[one],
+        departments: %w[one two],
+        from_date: '01/01/2014',
+        to_date: '01/01/2014'
+      }
+      expect(response).to redirect_to finder_path('search/statistics', params: {
+        keywords: %w[one two],
+        level_one_taxon: 'one',
+        organisations: %w[one two],
+        public_timestamp: { from: '01/01/2014', to: '01/01/2014' }
+      })
+    end
+    it 'redirects to the atom feed' do
+      get :published_statistics, params: { keywords: %w[one two] }, format: :atom
+      expect(response).to redirect_to finder_path('search/statistics', format: :atom, params: { keywords: %w[one two] })
+    end
+  end
+
+  describe '#upcoming_statistics' do
+    it "redirects to the statistics page" do
+      get :upcoming_statistics
+      expect(response).to redirect_to finder_path('search/statistics', params: { content_store_document_type: :statistics_upcoming })
+    end
+    it 'passes on a set of params' do
+      get :upcoming_statistics, params: {
+        keywords: %w[one two],
+        topics: %w[one],
+        organisations: %w[one two],
+        from_date: '01/01/2014',
+        to_date: '01/01/2014'
+      }
+      expect(response).to redirect_to finder_path('search/statistics', params: {
+        keywords: %w[one two],
+        level_one_taxon: 'one',
+        organisations: %w[one two],
+        content_store_document_type: :statistics_upcoming,
+        public_timestamp: { from: '01/01/2014', to: '01/01/2014' }
+      })
+    end
+    it 'redirects to the atom feed' do
+      get :upcoming_statistics, params: { keywords: %w[one two] }, format: :atom
+      expect(response).to redirect_to finder_path('search/statistics', format: :atom, params: { keywords: %w[one two], content_store_document_type: :statistics_upcoming })
+    end
+  end
 end
