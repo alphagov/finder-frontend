6f299b066e0c0864f83b6fddb8f3be1ea614b880
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-05-30 11:42:02 +0100

    Consolidate application js and sass

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,11 +2,7 @@ Rails.application.config.assets.precompile += [
   "application-ie6.css",
   "application-ie7.css",
   "application-ie8.css",
-  "application-site-search-ie6.css",
-  "application-site-search-ie7.css",
-  "application-site-search-ie8.css",
-  "application-site-search.css",
-  "application-site-search.js",
+  "application.js",
   "print.css",
   "hogan-2.0.0.js"
 ]
