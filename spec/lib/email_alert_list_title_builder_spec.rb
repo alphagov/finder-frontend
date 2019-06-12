39918afaeb6d5ee93770cd1214ccf55e49180db0
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-06-12 12:32:50 +0100

    Update example sectors in spec tests
    The example sectors are no longer present in the fixture data, so
    this updates them to current values.

--- spec/lib/email_alert_list_title_builder_spec.rb
+++ spec/lib/email_alert_list_title_builder_spec.rb
@@ -19,25 +19,25 @@ describe EmailAlertListTitleBuilder do
   context "one choice for one facet selected" do
     let(:filter) do
       {
-        "sector_business_area" => %w(banking-market-infrastructure),
+        "sector_business_area" => %w(agriculture),
       }
     end
 
     it "will join one choices from one facets in a list" do
-      is_expected.to eq("EU Exit guidance in the following category: 'Banking, markets and infrastructure'")
+      is_expected.to eq("EU Exit guidance in the following category: 'Agriculture and forestry (including wholesale)'")
     end
   end
 
   context "one choice for multiple facets selected" do
     let(:filter) do
       {
-        "sector_business_area" => %w(banking-market-infrastructure),
+        "sector_business_area" => %w(agriculture),
         "business_activity" => %w(buying)
       }
     end
 
     it "will join one choice from multiple facets in a list, separated with commas" do
-      is_expected.to eq("EU Exit guidance in the following categories: 'Banking, markets and infrastructure', 'Buy products or goods from abroad'")
+      is_expected.to eq("EU Exit guidance in the following categories: 'Agriculture and forestry (including wholesale)', 'Buy products or goods from abroad'")
     end
   end
 
@@ -45,26 +45,26 @@ describe EmailAlertListTitleBuilder do
   context "a selected facet with one overridden facet_choice is overwritten" do
     let(:filter) do
       {
-        "sector_business_area" => %w(banking-market-infrastructure),
+        "sector_business_area" => %w(agriculture),
         "public_sector_procurement" => %w(defence-contracts)
       }
     end
 
     it "will include the overwritten facet_choice" do
-      is_expected.to eq("EU Exit guidance in the following categories: 'Banking, markets and infrastructure', 'Public sector procurement - defence contracts'")
+      is_expected.to eq("EU Exit guidance in the following categories: 'Agriculture and forestry (including wholesale)', 'Public sector procurement - defence contracts'")
     end
   end
 
   context "multiple selected facets with multiple facet_choice overrides are overwritten" do
     let(:filter) do
       {
-        "sector_business_area" => %w(banking-market-infrastructure),
+        "sector_business_area" => %w(agriculture),
         "public_sector_procurement" => %w(defence-contracts civil-government-contracts)
       }
     end
 
     it "will include the overwritten facet_choices" do
-      is_expected.to eq("EU Exit guidance in the following categories: 'Banking, markets and infrastructure', 'Public sector procurement - defence contracts', 'Public sector procurement - civil government contracts'")
+      is_expected.to eq("EU Exit guidance in the following categories: 'Agriculture and forestry (including wholesale)', 'Public sector procurement - defence contracts', 'Public sector procurement - civil government contracts'")
     end
   end
 
@@ -72,13 +72,13 @@ describe EmailAlertListTitleBuilder do
   context "multiple choices for multiple facets selected" do
     let(:filter) do
       {
-        "sector_business_area" => %w(banking-market-infrastructure electronics),
+        "sector_business_area" => %w(agriculture electronics-parts-machinery),
         "business_activity" => %w(buying selling)
       }
     end
 
     it "will join multiple choices from multiple facets in a list, separated with commas" do
-      is_expected.to eq("EU Exit guidance in the following categories: 'Banking, markets and infrastructure', 'Electronics', 'Buy products or goods from abroad', 'Sell products or goods from abroad'")
+      is_expected.to eq("EU Exit guidance in the following categories: 'Agriculture and forestry (including wholesale)', 'Electronics, parts and machinery', 'Buy products or goods from abroad', 'Sell products or goods abroad'")
     end
   end
 end
