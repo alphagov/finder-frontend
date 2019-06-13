010cd2004ff35e1344f066184e0ebda0fb772000
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2019-06-13 11:57:35 +0100

    Creates new topic taxonomy registry
    A new registry to import all topic taxons
    to the Rails cache, look up is by basepath.

--- spec/helpers/taxonomy_spec_helper.rb
+++ spec/helpers/taxonomy_spec_helper.rb
@@ -40,8 +40,25 @@ module TaxonomySpecHelper
     taxons
   end
 
+  def stub_root_taxon(taxons)
+    content_store_has_item("/", root_taxon(taxons))
+  end
+
+  def full_topic_taxonomy_has_taxons(taxons)
+    taxons.each do |taxon|
+      content_store_has_item(taxon['base_path'], taxon)
+      unless taxon['links'].empty?
+        full_topic_taxonomy_has_taxons(taxon['links']['child_taxons'])
+      end
+    end
+  end
+
   def clear_taxon_cache
     Rails.cache.delete(taxon_cache_key)
+  end
+
+  def clear_full_taxon_cache
+    Rails.cache.delete('test/registries/full_topic_taxonomy')
   end
 
   def taxon_cache_key
---
+++ spec/lib/registries/full_topic_taxonomy_registry_spec.rb
@@ -0,0 +1,48 @@
+require "securerandom"
+require 'spec_helper'
+require "helpers/taxonomy_spec_helper"
+
+RSpec.describe Registries::FullTopicTaxonomyRegistry do
+  include TaxonomySpecHelper
+  let(:level_one_taxons) {
+    JSON.parse(File.read(Rails.root.join("features", "fixtures", "level_one_taxon.json")))
+  }
+
+  let(:base_path) { "/basepath" }
+
+  describe "when topic taxonomy API is unavailable" do
+    it "will return an (uncached) empty hash" do
+      clear_full_taxon_cache
+      topic_taxonomy_api_is_unavailable
+      expect(described_class.new[base_path]).to be_nil
+      expect(described_class.new.taxonomy).to eql({})
+      expect(Rails.cache.fetch(taxon_cache_key)).to be_nil
+    end
+  end
+
+  describe "when topic taxonomy api is available" do
+    before :each do
+      clear_full_taxon_cache
+      stub_root_taxon(level_one_taxons)
+      full_topic_taxonomy_has_taxons(level_one_taxons)
+    end
+
+    after { clear_full_taxon_cache }
+
+    let(:registry) { described_class.new }
+    let(:child_base_path) { "/environment/countryside-stewardship" }
+    let(:first_level_base_path) { "/environment" }
+    let(:child_content_id) { "013fc5e0-280c-4f73-9598-47de68f13dcd" }
+    let(:first_level_content_id) { "3cf97f69-84de-41ae-bc7b-7e2cc238fa58" }
+
+    it "can look up a child taxon by basepath" do
+      fetched_document = registry[child_base_path]
+      expect(fetched_document['content_id']).to eq(child_content_id)
+    end
+
+    it "can look up a level one taxon by basepath" do
+      fetched_document = registry[first_level_base_path]
+      expect(fetched_document['content_id']).to eq(first_level_content_id)
+    end
+  end
+end
