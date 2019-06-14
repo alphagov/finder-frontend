50eec7c8a856a9d224dceaa1e1a458a07ca64bc3
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2019-06-14 16:53:50 +0100

    WIP use document list component for finder results
    - data structure expected by component is not the same as current so have to restructure it
    - lots of debug still in place
    - original code still in place

--- app/presenters/advanced_search_result_set_presenter.rb
+++ app/presenters/advanced_search_result_set_presenter.rb
@@ -6,8 +6,16 @@ class AdvancedSearchResultSetPresenter < ResultSetPresenter
       metadata = metadata_presenter_class.new(result.metadata).present
       {
         document: AdvancedSearchResultPresenter.new(result, metadata).to_hash,
-        document_index: index + 1,
+        document_index: index + 1
       }
+    end
+  end
+
+  def document_list_documents
+    results.each_with_index.map do |result, index|
+      metadata = metadata_presenter_class.new(result.metadata).present
+      doc = AdvancedSearchResultPresenter.new(result, metadata).to_hash
+      structure_document_content(doc, result, index)
     end
   end
 end
--- app/presenters/result_set_presenter.rb
+++ app/presenters/result_set_presenter.rb
@@ -29,6 +29,7 @@ class ResultSetPresenter
       page_count: documents.count,
       finder_name: finder.name,
       debug_score: debug_score,
+      document_list_documents: document_list_documents
     }
   end
 
@@ -47,6 +48,68 @@ class ResultSetPresenter
         }
       end
     end
+  end
+
+  def document_list_documents
+    @document_list_documents ||= begin
+      results.each_with_index.map do |result, index|
+        metadata = metadata_presenter_class.new(result.metadata).present
+        doc = SearchResultPresenter.new(result, metadata).to_hash
+        structure_document_content(doc, result, index)
+      end
+    end
+  end
+
+  def structure_document_content(doc, result, index)
+    component_metadata = {}
+
+    if index === 0 && highlight_top_result?
+      doc[:top_result] = true
+      doc[:summary] = result.truncated_description
+      highlight_text = "Most relevant result"
+    end
+
+    if doc[:show_metadata]
+      #metadata.each do |meta| #this doesn't work in advanced search
+      doc[:metadata].each do |meta| #this works in advanced search
+        label = meta[:hide_label] ? "<span class='govuk-visually-hidden'>#{meta[:label]}:</span>" : "#{meta[:label]}:"
+
+        if meta[:is_date]
+          value = "<time datetime='#{meta[:machine_date]}'>#{meta[:human_date]}</time>"
+        else
+          value = meta[:value]
+        end
+
+        component_metadata[meta[:label]] = "#{label} #{value}".html_safe
+      end
+    end
+
+    published_text = "First published during the #{doc[:government_name]}" if doc[:is_historic]
+    debug_text = "<span class='debug-results debug-results--link'>#{doc[:link]}</span>"\
+                 "<span class='debug-results debug-results--meta'>Score: #{doc[:es_score] || "no score (sort by relevance)"}</span>"\
+                 "<span class='debug-results debug-results--meta'>Format: #{doc[:format]}</span>" if debug_score
+    subtext = "#{published_text}#{debug_text}".html_safe if published_text || debug_text
+
+    {
+      link: {
+        text: doc[:title],
+        path: doc[:link],
+        description: doc[:summary],
+        data_attributes: {
+          track_category: "navFinderLinkClicked",
+          track_action: "#{finder.name}.#{index + 1}",
+          track_label: doc[:link],
+          track_options: {
+            dimension28: documents.count,
+            dimension29: doc[:title]
+          }
+        }
+      },
+      metadata: component_metadata,
+      subtext: subtext,
+      highlight: doc[:top_result],
+      highlight_text: highlight_text
+    }
   end
 
   def user_supplied_date(date_facet_key, date_facet_from_to)
