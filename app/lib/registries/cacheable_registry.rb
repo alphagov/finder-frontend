cdeaeef6cd10b80f539086ff5ca624c188fbf8e7
Bill Franklin <b.franklin@outlook.com>
2019-06-21 00:52:17 +0100

    Make CacheableRegistry strictly typed
    This changes the typed mode for this module from
    'true' to 'strict', which requires annotations
    for methods and variables.

--- app/lib/registries/cacheable_registry.rb
+++ app/lib/registries/cacheable_registry.rb
@@ -1,12 +1,15 @@
-# typed: true
+# typed: strict
 module Registries
   module CacheableRegistry
+    extend T::Sig
     include Kernel
 
+    sig {returns(TrueClass)}
     def can_refresh_cache?
       true
     end
 
+    sig {returns(T::Boolean)}
     def refresh_cache
       Rails.cache.write(cache_key, cacheable_data)
     rescue GdsApi::HTTPServerError, GdsApi::HTTPBadGateway
@@ -14,6 +17,7 @@ module Registries
       false
     end
 
+    sig {returns(T::Hash)}
     def fetch_from_cache
       Rails.cache.fetch(cache_key) do
         cacheable_data
@@ -23,14 +27,17 @@ module Registries
       {}
     end
 
+    sig {void}
     def cacheable_data
       raise NotImplementedError, "Please supply a cacheable_data method"
     end
 
+    sig {void}
     def cache_key
       raise NotImplementedError, "Please supply a cache_key method"
     end
 
+    sig {void}
     def report_error
       raise NotImplementedError, "Please supply a report_error method"
     end
