644605ce0ef5e1360af7d64029b7b81674b67021
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-08-14 17:37:33 +0100

    adds in results to actions page

--- app/controllers/action_list_controller.rb
+++ app/controllers/action_list_controller.rb
@@ -59,6 +59,10 @@ private
     request.query_parameters.except(:page)
   end
 
+  ###
+  # Paths
+  ###
+
   def qa_path
     prepare_everyone_uk_leaving_eu_path + "?" + filtered_params.to_query
   end
@@ -73,4 +77,56 @@ private
     qa_config['feed_base_path'] + "?" + filtered_params.to_query
   end
   helper_method :feed_path
+
+  ###
+  # Search
+  ###
+
+  def finder_content_item
+    @finder_content_item ||= ContentItem.from_content_store('/find-eu-exit-guidance-business')
+  end
+
+  def initialize_search_query(topic_filter = {})
+    search_params = filter_params.merge(topic_filter)
+    Search::Query.new(
+      finder_content_item,
+      search_params
+    )
+  end
+
+  def formatted_topic_search_results(search_results)
+    search_results.map do |result|
+      {
+        link: {
+          text: result["title"],
+          path: result["link"]
+        },
+        metadata: {
+          due_date: ("Due by 31 October 2019" if result["facet_values"].include? "7283b8e1-840f-49da-967f-c0a512a3f531")
+        }
+      }
+    end
+  end
+
+  def topic_search_results
+    @results ||= begin
+      results = []
+      qa_config['results_page_config']['topics'].each do |topic|
+        topic_query_params = {}
+        topic['query_params'].each do |query|
+          topic_query_params[query["key"]] = query["value"]
+        end
+        search_results = initialize_search_query(topic_query_params).search_results
+        results.push(
+          label: topic["label"],
+          guidance: topic["guidance"],
+          results: formatted_topic_search_results(search_results["results"])
+        )
+      end
+      results
+    end
+  end
+  helper_method :topic_search_results
+
+
 end
