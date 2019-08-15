d6afc0ee3285d769829414c544cd3fd83a9ba2f0
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-08-15 11:32:01 +0100

    Adds Questions and Answer flow for Dynamic checklists
    Please note that the redirect to actions page still needs to be done.
    
    Had to excluse the routes on robo as it kept failing on CI
    
    Trello: https://trello.com/c/9yu7oI35/1-figure-out-how-to-build-this-in-production

---
+++ spec/controllers/checklist_controller_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe ChecklistController, type: :controller do
+  include FixturesHelper
+  render_views
+
+  describe "GET show" do
+    let(:find_brexit_guidance_yaml) { find_brexit_guidance_config }
+    let(:questions)                           { find_brexit_guidance_yaml["questions"] }
+    let(:current_question)                    { questions.first }
+    let(:params)                              { {} }
+    let(:base_path) { request.path }
+
+    before do
+      allow_any_instance_of(ChecklistController).to receive(:qa_config).and_return(find_brexit_guidance_yaml)
+      get :show, params: params
+    end
+
+    describe "Q&A page" do
+      it "correctly renders a Q&A page" do
+        get :show
+        expect(response.status).to eq(200)
+      end
+
+      context "on a question page" do
+        before { get :show }
+
+        it "sets a robots no-index metatag" do
+          expect(response.body).to include('<meta name="robots" content="noindex">')
+        end
+
+        it "renders the first facet's question" do
+          expect(response.body).to include(current_question["question"])
+        end
+
+        it "renders the description" do
+          expect(response.body).to include(current_question["description"])
+        end
+
+        it "sets next_page_url to itself" do
+          expect(response.body).to have_css("form#finder-qa-facet-filter-selection[action='#{base_path}']")
+        end
+
+        it "sets skips_url_link to page 2" do
+          expect(response.body).to have_link("Skip this question", href: "#{base_path}?page=2")
+        end
+      end
+
+      context "submitting final selections to the Q&A" do
+        let(:params) { { page: questions.count + 1 } }
+
+        it "shows the action list page" do
+          expect(response.body).to include("This is the actions page")
+        end
+      end
+    end
+  end
+end
