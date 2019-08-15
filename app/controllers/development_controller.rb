760b030ed04cce26f1229a4fc53a0988169d9010
Tijmen Brommet <tijmen@gmail.com>
2019-08-15 13:52:37 +0100

    Add development page for checklists
    We have built a facet group into content-tagger
    (https://github.com/alphagov/content-tagger/blob/f917f70f33b97e8fc04b3ae
    953f4631f285f1c37/lib/data/find-eu-exit-guidance-dynamic-lists.yml) to
    allow us to tag content so that it will appear on checklists.
    
    Currently we have no way to see all of the things that are tagged, and
    how they're tagged.
    
    This page contains a list of anything that *could* show up on a
    checklist page, with the facets that they've been given and a link to
    content-tagger.
    
    https://trello.com/c/nKAnfZKE

--- app/controllers/development_controller.rb
+++ app/controllers/development_controller.rb
@@ -1,7 +1,19 @@
 class DevelopmentController < ApplicationController
   layout "development_layout"
 
+  # https://github.com/alphagov/content-tagger/blob/f917f70f33b97e8fc04b3ae953f4631f285f1c37/lib/data/find-eu-exit-guidance-dynamic-lists.yml#L2
+  CHECKLIST_FACET_GROUP = "e6dc1c00-453a-4fac-96ec-66a2f11ae327".freeze
+
   def index
     @rendered_pages = Services.rummager.search(filter_rendering_app: "finder-frontend", count: 1000, order: "title")["results"]
+  end
+
+  def checklists
+    @all_possible_checklist_items = Services.rummager.search(
+      filter_facet_groups: [CHECKLIST_FACET_GROUP],
+      count: 1000,
+      order: "title",
+      fields: %w[title link content_id],
+    )["results"]
   end
 end
