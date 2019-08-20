e5db0e726ae3cc4d149244ae81c9d045ffc331b4
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2019-08-20 23:25:41 +0100

    Add Criterion class and definition file
    This adds a Criterion class that can be used to represent criteria
    in which a user meets. These criteria are set by the answers given
    to question and then can be used to select appropiate actions for
    a user.

---
+++ app/lib/checklists/criterion.rb
@@ -0,0 +1,21 @@
+class Checklists::Criterion
+  attr_reader :key, :text, :depends_on
+
+  def initialize(params)
+    @key = params['key']
+    @text = params['text']
+    @depends_on = params.fetch('depends_on', [])
+  end
+
+  def self.load(criteria_keys)
+    file = YAML.load_file("lib/checklists/criteria.yaml")
+
+    criteria = file['criteria'].map do |c|
+      Checklists::Criterion.new(c) if criteria_keys.include?(c['key'])
+    end
+
+    criteria.select do |c|
+      c.present? && (c.depends_on - criteria_keys).blank?
+    end
+  end
+end
