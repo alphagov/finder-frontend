413c6f3f0109373851652d53ca249ad0effea0e3
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2019-08-20 12:47:55 +0100

    Update grouped_result_set_presenter_spec to use real instances
    Add some factories to help instantiating required objects.
    Update the unit tests for GroupedResultSetPresenter so that it
    no longer uses doubles but actual objects.

---
+++ spec/factories/date_facet.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :date_facet, class: DateFacet do
+    key { 'published_at' }
+    filterable { true }
+    type { 'date' }
+    display_as_result_metadata { true }
+    initialize_with { new(attributes.deep_stringify_keys, {}) }
+  end
+end
