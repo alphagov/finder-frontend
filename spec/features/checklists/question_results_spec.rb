1dca027541baca3926d2ed9358fa83dcebd25869
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-08-27 14:16:55 +0100

    Show 'lead times' instead of 'due dates'
    Previously we showed a specific due date for each action. This replaces
    the dates with an textual lead time e.g. '3 months'.
    
    This also fixes the feature spec to actually check which actions are
    shown, as the associated method was not previously being called. In order
    to mitigate the increasing number of feature steps, this combines the
    question steps together, since the flow is custom for each scenario.

--- spec/features/checklists/question_results_spec.rb
+++ spec/features/checklists/question_results_spec.rb
@@ -1,47 +1,47 @@
 require "spec_helper"
 
 RSpec.feature "Questions workflow", type: :feature do
-  scenario "User answers questions" do
-    given_i_visit_the_checklist_question_and_answer_flow
-    and_i_have_a_business
-    and_i_say_that_i_sell_goods
-    and_i_say_that_i_am_a_eu_citizen
+  scenario "basic questions" do
+    when_i_visit_the_checklist_flow
+    and_i_answer_the_basic_questions
     then_i_should_see_the_results_page
+    and_i_should_see_a_passport_action
+    and_i_should_not_see_an_eori_action
   end
 
-  scenario "User answers conditional questions" do
-    given_i_visit_the_checklist_question_and_answer_flow
-    and_i_dont_have_a_business
-    and_i_say_that_i_am_a_eu_citizen
+  scenario "business activity question" do
+    when_i_visit_the_checklist_flow
+    and_i_also_answer_a_business_question
     then_i_should_see_the_results_page
-    and_i_should_not_see_unrelated_actions
+    and_i_should_see_a_passport_action
+    and_i_should_see_an_eori_action
   end
 
-  def given_i_visit_the_checklist_question_and_answer_flow
+  def when_i_visit_the_checklist_flow
     visit '/find-brexit-guidance'
   end
 
-  def and_i_have_a_business
+  def and_i_answer_the_basic_questions
     expect(page).to have_content "Do you own a business?"
-    choose "Yes"
+    choose "No"
     click_on "Next"
-  end
 
-  def and_i_dont_have_a_business
-    expect(page).to have_content "Do you own a business?"
+    expect(page).to have_content "Are you an EU national living in the UK?"
     choose "No"
     click_on "Next"
   end
 
-  def and_i_say_that_i_sell_goods
+  def and_i_also_answer_a_business_question
+    expect(page).to have_content "Do you own a business?"
+    choose "Yes"
+    click_on "Next"
+
     expect(page).to have_content "Does your business do any of the following activities?"
     check "Sell goods or provide services in the UK"
     click_on "Next"
-  end
 
-  def and_i_say_that_i_am_a_eu_citizen
     expect(page).to have_content "Are you an EU national living in the UK?"
-    choose "Yes"
+    choose "No"
     click_on "Next"
   end
 
@@ -49,11 +49,22 @@ RSpec.feature "Questions workflow", type: :feature do
     expect(page).to have_content "What you may need to know to prepare for Brexit"
   end
 
-  def and_i_should_see_the_related_actions
-    expect(page).to have_content "Get a new passport"
+  def and_i_should_see_a_passport_action
+    action = Checklists::Action.find_by_title("Get a new passport")
+    expect(page).to have_link(action.title, href: action.path)
+    expect(page).to have_content action.lead_time
+    expect(page).to have_content action.description
   end
 
-  def and_i_should_not_see_unrelated_actions
-    expect(page).not_to have_content "Get an EORI number that starts with GB to move goods in or out of the UK"
+  def and_i_should_not_see_an_eori_action
+    action = Checklists::Action.find_by_title("Get an EORI number")
+    expect(page).to_not have_link(action.title, href: action.path)
+  end
+
+  def and_i_should_see_an_eori_action
+    action = Checklists::Action.find_by_title("Get an EORI number")
+    expect(page).to have_link(action.title, href: action.path)
+    expect(page).to have_content action.lead_time
+    expect(page).to have_content action.description
   end
 end
