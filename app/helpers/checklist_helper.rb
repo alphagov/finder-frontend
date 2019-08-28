74279f67041aa69eb14de0ecab574a302792a50a
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2019-08-28 11:59:32 +0100

    Add PageService to calculate pages in Checklists to avoid redirects
    Calculating which page to show next was done as a helper inline in the
    controller. This functionality has been extracted to a separate model.
    
    This also allows us to calculate the next viewable page directly
    without having to redirect multiple times.
    
    The Paging service also keeps track of the next page and whether
    to redirect

--- app/helpers/checklist_helper.rb
+++ app/helpers/checklist_helper.rb
@@ -1,11 +1,4 @@
 module ChecklistHelper
-  def next_viewable_page(page, questions, criteria_keys)
-    next_page = (page..questions.length).find do |index|
-      questions[index - 1].show?(criteria_keys)
-    end
-    next_page || questions.length + 1
-  end
-
   def format_criteria_list(criteria)
     criteria.map { |criterion| { readable_text: criterion.text } }
   end
--- spec/helpers/checklist_helper_spec.rb
+++ spec/helpers/checklist_helper_spec.rb
@@ -1,48 +1,6 @@
 require 'spec_helper'
 
 describe ChecklistHelper, type: :helper do
-  describe "#next_viewable_page" do
-    let(:page) { 1 }
-    let(:criteria_keys) { [1, 2, 3] }
-    let(:questions) do
-      [
-        Checklists::Question.new('depends_on' => [1, 2, 3]),
-        Checklists::Question.new('depends_on' => [1, 2, 3]),
-        Checklists::Question.new('depends_on' => [1, 2, 3])
-      ]
-    end
-
-    subject { next_viewable_page(page, questions, criteria_keys) }
-
-    context "when its the first page and the question is not dependent" do
-      it { is_expected.to eq(1) }
-    end
-
-    context "when the question's criteria are not met" do
-      it 'returns the next page' do
-        questions[0] = Checklists::Question.new('depends_on' => [4])
-        expect(subject).to eq(2)
-      end
-    end
-
-    context "when consecutive questions' criteria are not met" do
-      it 'returns the page number of first applicable question' do
-        questions[0] = Checklists::Question.new('depends_on' => [4])
-        questions[1] = Checklists::Question.new('depends_on' => [4])
-        expect(subject).to eq(3)
-      end
-    end
-
-    context "when its the last page and question's criteria are not met" do
-      let(:page) { 3 }
-
-      it 'returns the next page' do
-        questions[2] = Checklists::Question.new('depends_on' => [4])
-        expect(subject).to eq(4)
-      end
-    end
-  end
-
   describe "#filter_actions" do
     let(:action1) { Checklists::Action.new('criteria' => []) }
     let(:action2) { Checklists::Action.new('criteria' => %w[A]) }
