3322dba160a139121474ca4f852466bbddb9f7d4
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2019-08-28 11:16:37 +0100

    Add a test for when the subscriber list needs to be created

--- spec/features/checklists/email_signup_spec.rb
+++ spec/features/checklists/email_signup_spec.rb
@@ -5,8 +5,26 @@ RSpec.feature "Checklist email signup", type: :feature do
   include GdsApi::TestHelpers::ContentStore
   include GdsApi::TestHelpers::EmailAlertApi
 
-  scenario "user clicks to signup to email alerts" do
+  let(:subscriber_list) do
+    {
+      "title" => "Your Brexit Checklist",
+      "slug" => "brexit-checklist-does-not-own-business-eu-national",
+      "tags" => { "brexit_checklist_criteria" => { "any" => %w[does-not-own-business eu-national] } },
+      "url" => "/results?c[]=does-not-own-business&c[]=eu-national"
+    }
+  end
+
+  scenario "user clicks to signup to email alerts with existing subscriber list" do
     given_im_on_the_results_page
+    and_email_alert_api_has_subscriber_list
+    then_i_click_to_signup_to_emails
+    and_i_am_taken_to_email_alert_frontend
+  end
+
+  scenario "user clicks to signup to email alerts without existing subscriber list" do
+    given_im_on_the_results_page
+    and_email_alert_api_does_not_have_subscriber_list
+    and_email_alert_api_creates_subscriber_list
     then_i_click_to_signup_to_emails
     and_i_am_taken_to_email_alert_frontend
   end
@@ -15,14 +33,19 @@ RSpec.feature "Checklist email signup", type: :feature do
     visit "/find-brexit-guidance/results?do_you_own_a_business%5B%5D=does-not-own-business&eu_national_in_uk%5B%5D=eu-national"
   end
 
-  def then_i_click_to_signup_to_emails
-    stub_email_alert_api_has_subscriber_list(
-      "title" => "General title",
-      "slug" => "brexit-checklist-does-not-own-business-eu-national",
-      "tags" => { "brexit_checklist_criteria" => { "any" => %w[does-not-own-business eu-national] } },
-      "url" => "/results?c[]=eu-national"
-    )
+  def and_email_alert_api_has_subscriber_list
+    stub_email_alert_api_has_subscriber_list(subscriber_list)
+  end
 
+  def and_email_alert_api_does_not_have_subscriber_list
+    stub_email_alert_api_does_not_have_subscriber_list(subscriber_list)
+  end
+
+  def and_email_alert_api_creates_subscriber_list
+    stub_email_alert_api_creates_subscriber_list(subscriber_list)
+  end
+
+  def then_i_click_to_signup_to_emails
     click_on "Sign up for emails"
   end
 
