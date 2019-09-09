96f82ca4c506129998134f5e02d37ea0e2708b5f
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-09-09 17:29:01 +0100

    Iterate notification emails again
    This updates the Brexit checker emails to reflect the latest design
    iteration. We decided to use a static title/subject for all emails to
    better reflect the checklist results page as a single 'content item' on
    GOV.UK, rather than trying to hack-in action-specific messaging.
    
    Part of the new design is to have a static piece of copy after the (now
    static) title, which introduces the type of change, so that the final
    ('Changed') section is now exclusively for showing a change note.

--- app/mailers/checklist_mailer.rb
+++ app/mailers/checklist_mailer.rb
@@ -2,6 +2,12 @@ class ChecklistMailer < ApplicationMailer
   def change_notification(change_note)
     @change_note = change_note
     @action = @change_note.action
-    mail(subject: @action.title)
+    mail(subject: subject)
+  end
+
+private
+
+  def subject
+    I18n.t!("checklists_mailer.change_notification.title")
   end
 end
