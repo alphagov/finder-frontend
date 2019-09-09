c95821b3cb3fd8d91654958ed6b8d57e2c167300
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-09-09 10:26:14 +0100

    Tweak notifications titles to remove categories
    This got missed in the main copy updates.

--- app/mailers/checklist_mailer.rb
+++ app/mailers/checklist_mailer.rb
@@ -2,13 +2,6 @@ class ChecklistMailer < ApplicationMailer
   def change_notification(change_note)
     @change_note = change_note
     @action = @change_note.action
-    mail(subject: subject)
-  end
-
-private
-
-  def subject
-    prefix = @change_note.type == "addition" ? "Added" : "Changed"
-    "#{prefix}: #{@action.title}"
+    mail(subject: @action.title)
   end
 end
