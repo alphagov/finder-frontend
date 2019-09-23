d7f6bc11316a7961938ccb06e449949d37edd520
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-09-23 11:08:28 +0100

    Pass criteria to previous Brexit Checker pages
    This ensures that when users click 'Back' on a Brexit Checker question page,
    their previous selections are preserved and displayed on previous pages.

--- spec/features/brexit_checker/question_navigation_spec.rb
+++ spec/features/brexit_checker/question_navigation_spec.rb
@@ -1,13 +1,12 @@
 require "spec_helper"
 
 RSpec.feature "Navigating Brexit Checker questions", type: :feature do
-  scenario "navigate to previous page" do
+  scenario "navigate to previous question" do
     when_i_visit_the_brexit_checker_flow
-    and_i_answer_the_nationality_question
-    then_i_should_see_the_living_question
-
-    when_i_click_on_the_back_link
-    then_i_should_see_the_nationality_question
+    and_i_answer_travel_questions
+    and_i_click_on_the_back_link
+    then_i_should_see_the_activities_question
+    and_my_previous_activities_choice_is_selected
   end
 
   def when_i_visit_the_brexit_checker_flow
@@ -15,22 +14,23 @@ RSpec.feature "Navigating Brexit Checker questions", type: :feature do
     expect(page).to have_link("Back", href: "/get-ready-brexit-check")
   end
 
-  def and_i_answer_the_nationality_question
-    answer_question("nationality", "British")
+  def and_i_answer_travel_questions
+    3.times { click_on "Next" }
+    answer_question("travelling", "To another EU country, Iceland, Liechtenstein, Norway or Switzerland")
+    answer_question("activities", "Take your pet")
   end
 
-  def then_i_should_see_the_living_question
-    question = BrexitChecker::Question.find_by_key("living")
-    expect(page).to have_content(question.text)
-  end
-
-  def when_i_click_on_the_back_link
+  def and_i_click_on_the_back_link
     click_on "Back"
   end
 
-  def then_i_should_see_the_nationality_question
-    question = BrexitChecker::Question.find_by_key("nationality")
+  def then_i_should_see_the_activities_question
+    question = BrexitChecker::Question.find_by_key("activities")
     expect(page).to have_content(question.text)
+  end
+
+  def and_my_previous_activities_choice_is_selected
+    expect(page).to have_checked_field("Take your pet")
   end
 
   def answer_question(key, *options)
