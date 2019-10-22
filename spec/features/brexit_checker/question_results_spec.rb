db202441e669b6f3a6c8b8df5bf3b03f5e4f2b48
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2019-10-22 11:35:04 +0100

    Add business question and new travel workflow
    There are extra requirements that business travellers need to
    take that were not being catered for with existing questions.
    
    This change breaks out the questions in a way that makes more
    sense to Users and departments.

--- spec/features/brexit_checker/question_results_spec.rb
+++ spec/features/brexit_checker/question_results_spec.rb
@@ -56,6 +56,7 @@ RSpec.feature "Brexit Checker workflow", type: :feature do
     answer_question("nationality", "British")
     answer_question("living", "UK")
     answer_question("employment")
+    answer_question("travelling-business", "Yes")
     answer_question("travelling", "To another EU country, Iceland, Liechtenstein, Norway or Switzerland")
     answer_question("activities", "Take your pet")
   end
