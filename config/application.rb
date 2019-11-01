0d33dc01941440ad6e69eaf7d4da292f308cf0e5
Michael Walker <mike@barrucadu.co.uk>
2019-11-01 14:07:53 +0000

    Run rails app:update task

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,18 @@
-require File.expand_path("boot", __dir__)
+require_relative 'boot'
 
+require "rails"
 # Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
 # require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
 require "sprockets/railtie"
-require "active_model/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -15,17 +22,14 @@ Bundler.require(*Rails.groups)
 if !Rails.env.production? || ENV["HEROKU_APP_NAME"].present?
   require "govuk_publishing_components"
 end
-
 module FinderFrontend
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     # config.i18n.default_locale = :de
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
 
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*yml")]
     config.action_view.raise_on_missing_translations = true
@@ -40,5 +44,9 @@ module FinderFrontend
 
     config.eager_load_paths << Rails.root.join("lib")
     config.autoload_paths << Rails.root.join("lib")
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
