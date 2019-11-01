a7e5a4a4c15e11eec0203cffa05f3b2620b96b67
Michael Walker <mike@barrucadu.co.uk>
2019-11-01 14:50:47 +0000

    Fix custom hostname for govuk-docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Hostname for govuk-docker
+  config.hosts << "finder-frontend.dev.gov.uk"
 end
