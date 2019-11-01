d51b90cd0ccfde19f4017f818c3b368ca0d37642
Michael Walker <mike@barrucadu.co.uk>
2019-11-01 14:29:42 +0000

    Disable CSS compressor
    We can re-enable this when libsass adds support for min/max.

--- config/application.rb
+++ config/application.rb
@@ -48,5 +48,8 @@ module FinderFrontend
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # https://github.com/alphagov/govuk-frontend/issues/1350
+    config.assets.css_compressor = nil
   end
 end
