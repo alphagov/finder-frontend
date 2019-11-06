4d6545ca4e5c7efc0d353c76f81206a802e75681
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2019-11-06 13:20:14 +0000

    Ruby lint double-quoted strings

--- app/parsers/date_parser.rb
+++ app/parsers/date_parser.rb
@@ -48,21 +48,21 @@ private
   end
 
   def process_number_only_inputs(date_string)
-    date_string = case date_string.length
-                  when 4
-      # Catches if user inputs just year which Chronic would parse as a time. e.g. "2008" as "8:08pm"
-                    "01/01/#{date_string}"
-                  when 8
-      # Catches fully padded dates without delimiters, eg 01012001
-                    date_string.gsub(/(\d{2})(\d{2})(\d{4})/, '\1/\2/\3')
-                  when 6
-      # Catches fully padded dates without delimiters with abreviated year, eg 010101
-                    date_string.gsub(/(\d{2})(\d{2})(\d{2})/, '\1/\2/\3')
-                  else
-      # Catches if user inputs an incorrect number of digits, which Chronic would parse as a time. e.g
-      # "100" as todays's date at 13:00, or "10011" as today's date at 13:00:11
-                    date_string.replace(" ")
-                  end
+    case date_string.length
+    when 4
+    # Catches if user inputs just year which Chronic would parse as a time. e.g. "2008" as "8:08pm"
+      "01/01/#{date_string}"
+    when 8
+    # Catches fully padded dates without delimiters, eg 01012001
+      date_string.gsub(/(\d{2})(\d{2})(\d{4})/, '\1/\2/\3')
+    when 6
+    # Catches fully padded dates without delimiters with abreviated year, eg 010101
+      date_string.gsub(/(\d{2})(\d{2})(\d{2})/, '\1/\2/\3')
+    else
+    # Catches if user inputs an incorrect number of digits, which Chronic would parse as a time. e.g
+    # "100" as todays's date at 13:00, or "10011" as today's date at 13:00:11
+      date_string.replace(" ")
+    end
   end
 
   # Catches if user input could be a month name which chronic would parse as this year
@@ -72,8 +72,7 @@ private
     if guessed_date
       guessed_year = guessed_date.year
       this_year = Time.now.year
-      date =
-        guessed_year != this_year ? guessed_date - 1.year : guessed_date
+      guessed_year != this_year ? guessed_date - 1.year : guessed_date
     end
   end
 end
