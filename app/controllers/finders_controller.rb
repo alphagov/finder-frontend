2acb8704b731bfec1b6d2559a4297cf5b608de6d
kr8n3r <jani@kraner.dev>
2019-11-13 12:01:47 +0000

    Hide keyword tags in A/B test
    We want to hide the keyword facet tags from users when they
    perform a text search until they've interacted with a filter and have
    it selected.
    
    Once they have no more filters selected, we hide the tags again.
    
    This sets up the HideKeywordFacets controller with all the logic and
    imports it into the finders_controller.
    
    We attach a CSS class to the groups of facet tags basedcon their
    [:data_facet] value, so that we can target keywords group.
    
    We use javascript to check for the A/B test meta tag value. If the
    user is in the B variant, we hide the keyword tags in they're the
    only tags present (which meant users hasn't used any of the filters)

--- app/controllers/finders_controller.rb
+++ app/controllers/finders_controller.rb
@@ -3,6 +3,7 @@ class FindersController < ApplicationController
   include ShinglesABTestable
   include FinderPopularityAbTestable
   include SpellingSuggestionsABTestable
+  include HideKeywordFacetTagsABTestable
 
   layout "finder_layout"
   before_action :remove_search_box
@@ -118,7 +119,7 @@ private
       content_item,
       filter_params,
       override_sort_for_feed: is_for_feed,
-      ab_params: popularity_ab_test.merge(spelling_suggestions_ab_test),
+      ab_params: popularity_ab_test.merge(spelling_suggestions_ab_test).merge(hide_keyword_facet_tags_ab_test),
     )
   end
 
