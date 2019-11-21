a35b6ca99afd64c20dd2eb0047ed1a7eed14cd6b
kr8n3r <jani@kraner.dev>
2019-11-21 15:55:29 +0000

    Show message on submitted scores
    When scores are submitted we redirect to /search page and
    - show a thank you message if scores have been saved
    - show an error message if scores have not been saved

--- app/controllers/relevancy_controller.rb
+++ app/controllers/relevancy_controller.rb
@@ -5,9 +5,12 @@ class RelevancyController < ApplicationController
     @judgement_set = JudgementSet.new(judgement_attrs)
     @scores = scores(@judgement_set)
     if @judgement_set.save
-      redirect_to search_path #and_show_confirmation_message
+      redirect_to search_path, 
+        notice: "<h2>Thank you</h2><p>Your scores have been saved. You can perform another search to score more results.</p>"
     else
-      redirect_to "/search", status: 300
+      redirect_to "/search", 
+        status: 300, 
+        alert: "<h2>There has been an error</h2><p>Please perform another search to score the results.</p>"
     end
   end
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-FinderFrontend::Application.config.session_store :disabled
+FinderFrontend::Application.config.session_store :cookie_store, key: "_judgements"
