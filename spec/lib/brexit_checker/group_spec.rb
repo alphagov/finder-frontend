732ef6f5b8eff27bbda13174d6a557a40e5fd80e
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2019-12-02 15:03:59 +0000

    Add groups data and object for results page
    group.rb line 23 re-uses a line previously declared in criterion.rb:
    @load_all = nil if Rails.env.development?
    
    This ensures that in development mode it reloads the group file. So if Rails is running in development mode, changes in the groups.yaml file will be immediately visible without having to restart Rails.

---
+++ spec/lib/brexit_checker/group_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+RSpec.describe BrexitChecker::Group do
+  describe "factories" do
+    it "has a valid default factory" do
+      group = FactoryBot.build(:brexit_checker_group)
+      expect(group.valid?).to be(true)
+    end
+  end
+
+  describe "#hash & #eql?" do
+    let(:group1) { FactoryBot.build(:brexit_checker_group, key: "visiting-eu") }
+    let(:group2) { FactoryBot.build(:brexit_checker_group, key: "visiting-eu") }
+    let(:group3) { FactoryBot.build(:brexit_checker_group, key: "living-uk") }
+
+    it "correctly removes duplicates from an array by key" do
+      expect([group1, group2, group3].uniq.map(&:key)).to eq(%w[visiting-eu living-uk])
+    end
+  end
+
+  describe "#actions" do
+    let(:action1) { FactoryBot.build(:brexit_checker_action, id: "S01", grouping_criteria: %w(visiting-eu)) }
+    let(:action2) { FactoryBot.build(:brexit_checker_action, id: "S02", grouping_criteria: %w(living-ie)) }
+    let(:action3) { FactoryBot.build(:brexit_checker_action, id: "S03", grouping_criteria: %w(living-ie)) }
+    let(:group1) { FactoryBot.build(:brexit_checker_group, key: "visiting-eu") }
+    let(:group2) { FactoryBot.build(:brexit_checker_group, key: "living-ie") }
+    let(:group3) { FactoryBot.build(:brexit_checker_group, key: "studying-uk") }
+
+    before :each do
+      allow(BrexitChecker::Action).to receive(:load_all).and_return([action1, action2, action3])
+    end
+
+    it "retuns an action when a grouping_criteira matches the group key" do
+      expect(group1.actions).to match_array([action1])
+    end
+
+    it "retuns multiple actions when multiple grouping_criteira match the group key" do
+      expect(group2.actions).to match_array([action2, action3])
+    end
+
+    it "retuns an empty array when no action's grouping_criteira match the group key" do
+      expect(group3.actions).to match_array([])
+    end
+  end
+end
