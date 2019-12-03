dd82c3e361e6bf5d08db100c2cee4be849f7eae4
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2019-12-03 11:32:33 +0000

    Add business and citizen results to checker controller
    We should now be able to use these in the view instead of actions

--- app/controllers/brexit_checker_controller.rb
+++ app/controllers/brexit_checker_controller.rb
@@ -34,6 +34,9 @@ class BrexitCheckerController < ApplicationController
     all_actions = BrexitChecker::Action.load_all
     @criteria = BrexitChecker::Criterion.load_by(criteria_keys)
     @actions = filter_items(all_actions, criteria_keys)
+    audience_actions = @actions.group_by(&:audience)
+    @business_results = BrexitChecker::ResultsAudiences.populate_business_groups(audience_actions["business"], @criteria)
+    @citizen_results_groups = BrexitChecker::ResultsAudiences.populate_citizen_groups(audience_actions["citizen"], @criteria)
   end
 
   def email_signup; end
