505af2c5299e14df36f01f8fd9c0c8daa6ae7f6e
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2019-12-11 18:32:03 +0000

    Add API to parse citizens groups to ResultsAuidence

--- lib/brexit_checker/results_audiences.rb
+++ lib/brexit_checker/results_audiences.rb
@@ -5,24 +5,24 @@ class BrexitChecker::ResultsAudiences
 
       {
         actions: audience_actions,
-        criteria: filter_criteria_by_actions(audience_actions, selected_criteria),
+        criteria: audience_actions.flat_map(&:all_criteria).uniq,
       }
     end
 
-  private
+    def populate_citizen_groups(audience_actions, selected_criteria)
+      return [] if audience_actions.blank? || selected_criteria.blank?
 
-    def filter_actions_by_group(actions, group_key)
-      actions.select { |action| action.grouping_criteria.include?(group_key) }
-    end
-
-    def filter_criteria_by_actions(actions, criteria)
-      return [] if actions.empty? || criteria.empty?
-
-      action_criteria = actions.flat_map do |action|
-        BrexitChecker::Criteria::Extractor.extract(action.criteria).to_a
+      BrexitChecker::Group.load_all.each_with_object([]) do |group, result|
+        selected_actions = group.actions & audience_actions
+        unless selected_actions.empty?
+          result << {
+            group: group,
+            actions: selected_actions,
+            criteria: selected_actions.flat_map(&:all_criteria).uniq,
+          }
+        end
+        result
       end
-      criteria_keys = criteria.map(&:key) & action_criteria.uniq
-      BrexitChecker::Criterion.load_by(criteria_keys)
     end
   end
 end
