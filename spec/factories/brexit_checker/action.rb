11dd5f283a0ac93f1fe66c1b925fb61ff6b26d2b
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2019-12-17 14:46:14 +0000

    Change factory to accept sequenced ID

--- spec/factories/brexit_checker/action.rb
+++ spec/factories/brexit_checker/action.rb
@@ -2,11 +2,12 @@ FactoryBot.define do
   factory :brexit_checker_action, class: BrexitChecker::Action do
     title { "A title" }
     title_url { "http://www.gov.uk" }
-    id { SecureRandom.uuid }
     consequence { "A consequence" }
     priority { 5 }
     criteria { %w(construction) }
     audience { "business" }
+
+    sequence(:id) { |n| "Action#{n}" }
 
     trait :citizen do
       audience { "citizen" }
