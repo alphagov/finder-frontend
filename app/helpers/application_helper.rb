0244cea04b71600cf4c5ace7be9b611941bb7189
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2019-12-18 13:49:23 +0000

    Remove unused helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,4 @@
 module ApplicationHelper
-  def input_checked(key, value)
-    if facet_has_any_selected_values?(key)
-      if params[key].is_a?(Array) && params[key].include?(value)
-        ' checked="checked"'
-      elsif params[key] == value
-        ' checked="checked"'
-      end
-    end
-  end
-
-  def facet_has_any_selected_values?(key)
-    params.has_key?(key)
-  end
-
   def absolute_url_for(path)
     URI.join(Plek.current.website_root, path)
   end
