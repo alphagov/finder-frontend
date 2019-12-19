e6a571c03749cbfd4cbbb4a43881f16d888fac3c
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2019-12-19 12:09:38 +0000

    TOSQUASH: Add ecommerce tracking

--- spec/features/brexit_checker/question_results_spec.rb
+++ spec/features/brexit_checker/question_results_spec.rb
@@ -170,6 +170,8 @@ RSpec.feature "Brexit Checker workflow", type: :feature do
     expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-track-action='Your business or organisation - 1.2 - Guidance']")
     expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-track-category='brexit-checker-results']")
     expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-track-label='#{action.guidance_url}']")
+    expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-ecommerce-path='#{action.guidance_url.sub('https://www.gov.uk','')}']")
+    expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-ecommerce-row]")
   end
 
   def and_the_ce_mark_link_should_have_tracking_analytics
@@ -177,6 +179,8 @@ RSpec.feature "Brexit Checker workflow", type: :feature do
     expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-track-action='Your business or organisation - 1.1 - Guidance']")
     expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-track-category='brexit-checker-results']")
     expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-track-label='#{action.guidance_url}']")
+    expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-ecommerce-path='#{action.guidance_url.sub('https://www.gov.uk','')}']")
+    expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-ecommerce-row]")
   end
 
   def and_the_pet_link_should_have_tracking_analytics
@@ -184,5 +188,7 @@ RSpec.feature "Brexit Checker workflow", type: :feature do
     expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-track-action='You and your family - Visiting the EU - 1.2 - Guidance']")
     expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-track-category='brexit-checker-results']")
     expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-track-label='#{action.guidance_url}']")
+    expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-ecommerce-path='#{action.guidance_url.sub('https://www.gov.uk','')}']")
+    expect(page).to have_css(".govuk-link[href='#{action.guidance_url}'][data-ecommerce-row]")
   end
 end
