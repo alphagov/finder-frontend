88ed6ce593faea4ab0119e598e71063ac45dc07f
Michael Walker <mike@barrucadu.co.uk>
2020-03-25 12:42:22 +0000

    Show topical event title as extra header context
    We've decided not to use the facet tag for this because there's no way
    to get this filter back if you remove it (other than following
    whatever link you found to the page again).

---
+++ app/helpers/title_context_helper.rb
@@ -0,0 +1,13 @@
+module TitleContextHelper
+  def title_context(filter_params = nil)
+    filter_params ||= params
+
+    topical_events = filter_params["topical_events"]
+    topical_events = [topical_events] if topical_events.is_a? String
+
+    if topical_events && topical_events.count == 1
+      registry = Services.registries.all["full_topical_events"]
+      (registry[topical_events.first] || {})["title"]
+    end
+  end
+end
---
+++ spec/helpers/title_context_helper.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe TitleContextHelper, type: :helper do
+  include RegistrySpecHelper
+
+  let(:filter_params) { {} }
+
+  before :each do
+    stub_full_topical_events_registry_request
+  end
+
+  describe "#title_context" do
+    subject(:subject) { title_context(filter_params) }
+
+    context "there are no topical events" do
+      it "gives no context" do
+        expect(subject).to be_nil
+      end
+    end
+
+    context "there is one topical event (as a string)" do
+      let(:filter_params) { { "topical_events" => "2014-overseas-territories-joint-ministerial-council" } }
+
+      it "gives context" do
+        expect(subject).to eq("2014 Overseas Territories Joint Ministerial Council")
+      end
+    end
+
+    context "there is one topical event (as an array)" do
+      let(:filter_params) { { "topical_events" => %w(2014-overseas-territories-joint-ministerial-council) } }
+
+      it "gives context" do
+        expect(subject).to eq("2014 Overseas Territories Joint Ministerial Council")
+      end
+    end
+
+    context "there is more than one topical event" do
+      let(:filter_params) { { "topical_events" => %w(2014-overseas-territories-joint-ministerial-council anti-corruption-summit-london-2016) } }
+
+      it "gives no context" do
+        expect(subject).to be_nil
+      end
+    end
+  end
+end
