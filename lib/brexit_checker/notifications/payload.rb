f3783ca6c19b3c012b608a44a14261cafeb0801c
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-04-16 13:56:08 +0100

    Extract brexit checker payload into class
    This resolves a new linting error about defining (top-level) methods
    inside rake tasks, and has the nice side-effect of simplifying the
    task definition to focus on the main actions performed.

---
+++ lib/brexit_checker/notifications/payload.rb
@@ -0,0 +1,42 @@
+class BrexitChecker::Notifications::Payload
+  attr_reader :notification
+
+  def initialize(notification)
+    @notification = notification
+  end
+
+  def to_h
+    mail = BrexitCheckerMailer.change_notification(notification)
+    criteria = notification.criteria.presence || notification.action.criteria
+
+    {
+      title: mail.subject,
+      url: notification.action.title_url,
+      body: mail.body.raw_source,
+      sender_message_id: notification.id,
+      criteria_rules: criteria_rules(criteria),
+    }
+  end
+
+private
+
+  def criteria_rules(criteria)
+    if criteria.is_a? String
+      return {
+        type: "tag",
+        key: "brexit_checklist_criteria",
+        value: criteria,
+      }
+    end
+
+    if criteria.is_a? Hash
+      return Hash[criteria.map { |k, v| [k, criteria_rules(v)] }]
+    end
+
+    if criteria.is_a? Array
+      return criteria.map { |c| criteria_rules(c) }
+    end
+
+    raise "Unexpected criteria class #{criteria}"
+  end
+end
