90d536a10c1f016516d312b36d57cbf67675db41
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2020-06-26 16:41:58 +0100

    Remove unused redirects
    These aren't routed to, and the actual [redirects are handled in Whitehall](https://github.com/alphagov/whitehall/blob/master/app/controllers/publications_controller.rb#L9)

--- app/controllers/redirection_controller.rb
+++ app/controllers/redirection_controller.rb
@@ -1,37 +1,6 @@
 class RedirectionController < ApplicationController
-  include PublicationsRoutes
-  DEFAULT_PUBLICATIONS_PATH = "search/all".freeze
-
   def redirect_covid
     redirect_to(finder_path(params[:slug], params: covid_topic_and_other_params))
-  end
-
-  def announcements
-    respond_to do |format|
-      format.html { redirect_to(finder_path("search/news-and-communications", params: convert_common_parameters)) }
-      format.atom { redirect_to(finder_path("search/news-and-communications", params: convert_common_parameters, format: :atom)) }
-    end
-  end
-
-  def publications
-    respond_to do |format|
-      format.html { redirect_to(finder_path(publications_base_path, params: convert_common_parameters.merge(content_store_document_type: set_document_type).compact)) }
-      format.atom { redirect_to(finder_path(publications_base_path, params: convert_common_parameters.merge(content_store_document_type: set_document_type).compact, format: :atom)) }
-    end
-  end
-
-  def published_statistics
-    respond_to do |format|
-      format.html { redirect_to(finder_path("search/statistics", params: convert_common_parameters)) }
-      format.atom { redirect_to(finder_path("search/statistics", params: convert_common_parameters, format: :atom)) }
-    end
-  end
-
-  def upcoming_statistics
-    respond_to do |format|
-      format.html { redirect_to(finder_path("search/statistics", params: convert_common_parameters.merge(content_store_document_type: :statistics_upcoming))) }
-      format.atom { redirect_to(finder_path("search/statistics", params: convert_common_parameters.merge(content_store_document_type: :statistics_upcoming), format: :atom)) }
-    end
   end
 
   def advanced_search
@@ -59,26 +28,6 @@ class RedirectionController < ApplicationController
   end
 
 private
-
-  def publications_base_path
-    base_path = PUBLICATIONS_ROUTES.dig(params[:publication_filter_option], :base_path)
-    base_path || DEFAULT_PUBLICATIONS_PATH
-  end
-
-  def set_document_type
-    PUBLICATIONS_ROUTES.dig(params[:publication_filter_option], :special_params, :content_store_document_type)
-  end
-
-  def convert_common_parameters
-    { keywords: params["keywords"],
-      level_one_taxon: params["taxons"].try(:first) || params["topics"].try(:first),
-      level_two_taxon: params["subtaxons"].try(:first),
-      organisations: params["departments"] || params["organisations"],
-      people: params["people"],
-      roles: params["roles"],
-      world_locations: params["world_locations"],
-      public_timestamp: { from: params["from_date"], to: params["to_date"] }.compact.presence }.compact
-  end
 
   def covid_topic_and_other_params
     {
