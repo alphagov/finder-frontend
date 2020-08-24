70b1a182c41cbffb51a94b614890fb0e9d944e97
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-08-24 12:26:59 +0100

    Move Brexit/Transition checker routes into a scope
    There are multiple routes with the same prefix in the slug, so moving
    them all into a scope makes the code much easier to read (with less
    typing when adding new routes).

--- config/routes.rb
+++ config/routes.rb
@@ -18,20 +18,19 @@ FinderFrontend::Application.routes.draw do
     get "/test-search/search/opensearch" => "search#opensearch"
   end
 
-  # Routes for the for Transition/Brexit Checker
-  get "/transition-check/results" => "brexit_checker#results", as: :transition_checker_results
-  get "/transition-check/questions" => "brexit_checker#show", as: :transition_checker_questions
-  get "/transition-check/email-signup" => "brexit_checker#email_signup", as: :transition_checker_email_signup
-  post "/transition-check/email-signup" => "brexit_checker#confirm_email_signup", as: :transition_checker_confirm_email_signup
+  scope "/transition-check" do
+    get "/results" => "brexit_checker#results", as: :transition_checker_results
+    get "/questions" => "brexit_checker#show", as: :transition_checker_questions
+    get "/email-signup" => "brexit_checker#email_signup", as: :transition_checker_email_signup
+    post "/email-signup" => "brexit_checker#confirm_email_signup", as: :transition_checker_confirm_email_signup
+    get "/save-your-results" => "brexit_checker#save_results", as: :transition_checker_save_results
+  end
 
   # Transition/Brexit checker email signup routes
   get "/email/subscriptions/new", to: proc { [200, {}, [""]] }, as: :email_alert_frontend_signup
 
   get "/*slug/email-signup" => "email_alert_subscriptions#new", as: :new_email_alert_subscriptions
   post "/*slug/email-signup" => "email_alert_subscriptions#create", as: :email_alert_subscriptions
-
-  # GOV.UK Accounts discovery routes
-  get "/transition-check/save-your-results" => "brexit_checker#save_results", as: :transition_checker_save_results
 
   # Q&A frontend for "UK Nationals in the EU" (www.gov.uk/uk-nationals-in-the-eu)
   get "/uk-nationals-living-eu" => "qa_to_content#show"
