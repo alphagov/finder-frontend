33a7195c7a360aad4d3e2730709711d72fa490c7
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2020-09-16 17:29:23 +0100

    Update to how we group actions with multiple grouping criteria
    - If an action has more than one grouping criteria, and a user selects
    all of the ANY_OF criteria, we should show duplicate actions under both groups.
    - If an action has more than one grouping criteria, and a user selects
    some of the ANY_OF criteria, we should only show the action under the grouping that
    has a key in common with the user's selected criteria.
    - If an action has only one grouping criteria then we should only show the action
    in that one group.

--- app/lib/brexit_checker/results_audiences.rb
+++ app/lib/brexit_checker/results_audiences.rb
@@ -12,17 +12,36 @@ class BrexitChecker::ResultsAudiences
     def populate_citizen_groups(audience_actions, selected_criteria)
       return [] if audience_actions.blank? || selected_criteria.blank?
 
-      BrexitChecker::Group.load_all.each_with_object([]) do |group, result|
-        selected_actions = group.actions & audience_actions
-        unless selected_actions.empty?
-          result << {
+      audience_actions.each_with_object([]) do |action, grouped_actions|
+        next if action.grouping_criteria.empty?
+
+        group_keys =
+          multiple_grouping_criteria?(action) ? selected_group(action, selected_criteria) : action.grouping_criteria
+        group_keys.each do |key|
+          group = BrexitChecker::Group.find_by(key)
+          next if grouped_actions.any? { |actions| actions[:group] == group }
+          selected_actions = group.actions & audience_actions
+          grouped_actions << {
             group: group,
             actions: selected_actions,
             criteria: selected_actions.flat_map(&:all_criteria).uniq & selected_criteria,
           }
         end
-        result
+        grouped_actions
       end
+    end
+
+    def selected_group(action, selected_criteria)
+      criteria_keys = selected_criteria.map(&:key)
+      action.grouping_criteria.select { |group_key| criteria_keys.include? group_key }
+    end
+
+    def all_groups
+      @all_groups ||= BrexitChecker::Group.load_all
+    end
+
+    def multiple_grouping_criteria?(action)
+      action.grouping_criteria.count > 1
     end
   end
 end
--- spec/lib/brexit_checker/results_audiences_spec.rb
+++ spec/lib/brexit_checker/results_audiences_spec.rb
@@ -2,81 +2,122 @@ require "spec_helper"
 
 describe BrexitChecker::ResultsAudiences do
   describe "#populate_citizen_groups" do
-    let(:action1) { FactoryBot.build(:brexit_checker_action, :citizen, criteria: %w[living-uk living-row], grouping_criteria: %w[living-uk]) }
-    let(:action2) { FactoryBot.build(:brexit_checker_action, :citizen, criteria: %w[join-family-uk-yes], grouping_criteria: %w[living-uk]) }
-    let(:action3) { FactoryBot.build(:brexit_checker_action, :citizen, criteria: %w[nationality-uk], grouping_criteria: %w[living-uk]) }
-    let(:action4) { FactoryBot.build(:brexit_checker_action, :citizen, criteria: %w[visiting-driving], grouping_criteria: %w[visiting-eu]) }
-    let(:action5) { FactoryBot.build(:brexit_checker_action, :citizen, criteria: %w[studying-eu], grouping_criteria: %w[studying-eu]) }
-    let(:action6) { FactoryBot.build(:brexit_checker_action, :citizen, criteria: %w[living-uk visiting-ie], grouping_criteria: %w[living-uk visiting-ie]) }
+    living_uk = FactoryBot.build(:brexit_checker_criterion, key: "living-uk")
+    visiting_driving = FactoryBot.build(:brexit_checker_criterion, key: "visiting-driving")
+    visiting_bring_pet = FactoryBot.build(:brexit_checker_criterion, key: "visiting-bring-pet")
+    visiting_eu = FactoryBot.build(:brexit_checker_criterion, key: "visiting-eu")
+    visiting_ie = FactoryBot.build(:brexit_checker_criterion, key: "visiting-ie")
 
-    let(:criteria1) { FactoryBot.build(:brexit_checker_criterion, key: "living-uk", text: "Living in the UK") }
-    let(:criteria2) { FactoryBot.build(:brexit_checker_criterion, key: "join-family-uk-yes", text: "You plan to join an EU or EEA family member in the UK") }
-    let(:criteria3) { FactoryBot.build(:brexit_checker_criterion, key: "nationality-uk", text: "You are a British national") }
-    let(:criteria4) { FactoryBot.build(:brexit_checker_criterion, key: "visiting-driving", text: "You need to drive abroad") }
-    let(:criteria5) { FactoryBot.build(:brexit_checker_criterion, key: "studying-eu", text: "You are studying in the EU") }
-    let(:criteria6) { FactoryBot.build(:brexit_checker_criterion, key: "living-row", text: "You do not live in the UK or the EU") }
-    let(:criteria7) { FactoryBot.build(:brexit_checker_criterion, key: "visiting-ie", text: "Visiting Ireland") }
+    action1_criteria = [{ "all_of" => [living_uk.key] }]
+    action1 = FactoryBot.build(:brexit_checker_action, :citizen, title: "action 1", criteria: action1_criteria, grouping_criteria: %w[living-uk])
 
-    let(:group_living_uk) { FactoryBot.build(:brexit_checker_group, key: "living-uk", heading: "Living in the UK") }
-    let(:group_visiting_eu) { FactoryBot.build(:brexit_checker_group, key: "visiting-eu", heading: "Visiting the EU") }
-    let(:group_studying_eu) { FactoryBot.build(:brexit_checker_group, key: "studying-eu", heading: "Studying in the EU") }
-    let(:group_visiting_ie) { FactoryBot.build(:brexit_checker_group, key: "visiting-ie", heading: "Visiting Ireland") }
+    action2_criteria = [{ "all_of" => [visiting_driving.key] }]
+    action2 = FactoryBot.build(:brexit_checker_action, :citizen, title: "action 2", criteria: action2_criteria, grouping_criteria: %w[visiting-eu])
 
-    let(:selected_criteria) { [criteria1, criteria2, criteria3, criteria4, criteria5] }
-    let(:actions) { [action1, action2, action3, action4, action5, action6] }
+    action3_criteria = [{ "all_of" => [living_uk.key, visiting_bring_pet.key, { "any_of" => [visiting_eu.key, visiting_ie.key] }] }]
+    action3 = FactoryBot.build(:brexit_checker_action, :citizen, title: "action 3", criteria: action3_criteria, grouping_criteria: %w[visiting-eu visiting-ie])
+
+    group_visiting_eu = FactoryBot.build(:brexit_checker_group, key: "visiting-eu", heading: "Visiting the EU")
+    group_visiting_ie = FactoryBot.build(:brexit_checker_group, key: "visiting-ie", heading: "Visiting Ireland")
+    group_living_uk = FactoryBot.build(:brexit_checker_group, key: "living-uk", heading: "Living in the UK")
+
+    all_actions = [action1, action2, action3]
+    all_criteria = [living_uk, visiting_driving, visiting_bring_pet, visiting_eu, visiting_ie]
+    all_groups = [group_visiting_eu, group_visiting_ie, group_living_uk]
 
     before :each do
-      allow(BrexitChecker::Action).to receive(:load_all).and_return([action1, action2, action3, action4, action5, action6])
-      allow(BrexitChecker::Criterion).to receive(:load_all).and_return([criteria1, criteria2, criteria3, criteria4, criteria5, criteria6, criteria7])
-      allow(BrexitChecker::Group).to receive(:load_all).and_return([group_living_uk, group_visiting_eu, group_studying_eu, group_visiting_ie])
+      allow(BrexitChecker::Action).to receive(:load_all).and_return(all_actions)
+      allow(BrexitChecker::Criterion).to receive(:load_all).and_return(all_criteria)
+      allow(BrexitChecker::Group).to receive(:load_all).and_return(all_groups)
     end
 
-    context "actions are provided but there are no criteria" do
-      let(:result) { described_class.populate_citizen_groups(actions, []) }
+    context "when actions are provided but there are no criteria" do
+      let(:grouped_citizen_actions) { described_class.populate_citizen_groups(all_actions, []) }
       it "returns an empty array" do
-        expect(result).to be_empty
+        expect(grouped_citizen_actions).to be_empty
       end
     end
 
-    context "criteria are provided but there are no actions" do
-      let(:result) { described_class.populate_citizen_groups([], selected_criteria) }
+    context "when criteria are provided but there are no actions" do
+      let(:grouped_citizen_actions) { described_class.populate_citizen_groups([], all_criteria) }
       it "returns an empty array" do
-        expect(result).to be_empty
+        expect(grouped_citizen_actions).to be_empty
       end
     end
 
-    context "actions and criteria are provided" do
-      let(:result) { described_class.populate_citizen_groups(actions, selected_criteria) }
-
+    context "when actions are provided that only have a single grouping criterion" do
+      let(:selected_criteria) { [living_uk, visiting_driving] }
+      let(:filtered_actions) { [action1, action2] }
+      let(:grouped_citizen_actions) { described_class.populate_citizen_groups(filtered_actions, selected_criteria) }
       it "produces an array of group hashes" do
         grouped_actions_fixture = [
           {
             group: group_living_uk,
-            actions: [action1, action2, action3, action6],
-            criteria: [criteria1, criteria2, criteria3],
+            actions: [action1],
+            criteria: [living_uk],
           },
           {
             group: group_visiting_eu,
-            actions: [action4],
-            criteria: [criteria4],
-          },
-          {
-            group: group_studying_eu,
-            actions: [action5],
-            criteria: [criteria5],
-          },
-          {
-            group: group_visiting_ie,
-            actions: [action6],
-            criteria: [criteria1],
+            actions: [action2],
+            criteria: [visiting_driving],
           },
         ]
+        expect(grouped_citizen_actions).to eq grouped_actions_fixture
+      end
+    end
 
-        expect(result).to eql(grouped_actions_fixture)
+    context "when actions have multiple grouping criteria" do
+      context "when the user selects ONE of the action's ANY OF criteria" do
+        let(:selected_criteria) { [living_uk, visiting_eu, visiting_bring_pet] }
+        let(:filtered_actions) { [action1, action3] }
+        let(:grouped_citizen_actions) { described_class.populate_citizen_groups(filtered_actions, selected_criteria) }
+
+        it "only shows the group matching the selected criteria" do
+          grouped_actions_fixture = [
+            {
+              group: group_living_uk,
+              actions: [action1],
+              criteria: [living_uk],
+            },
+            {
+              group: group_visiting_eu,
+              actions: [action3],
+              criteria: [living_uk, visiting_bring_pet, visiting_eu],
+            }
+          ]
+          expect(grouped_citizen_actions).to eq grouped_actions_fixture
+        end
+      end
+
+      context "when the user selects ALL of the action's ANY OF criteria" do
+        let(:selected_criteria) { [living_uk, visiting_eu, visiting_ie, visiting_bring_pet] }
+        let(:filtered_actions) { [action1, action3] }
+        let(:grouped_citizen_actions) { described_class.populate_citizen_groups(filtered_actions, selected_criteria) }
+
+        it "shows all of the groups that have matching criteria, and duplicates actions" do
+          grouped_actions_fixture = [
+            {
+              group: group_living_uk,
+              actions: [action1],
+              criteria: [living_uk],
+            },
+            {
+              group: group_visiting_eu,
+              actions: [action3],
+              criteria: [living_uk, visiting_bring_pet, visiting_eu, visiting_ie],
+            },
+            {
+              group: group_visiting_ie,
+              actions: [action3],
+              criteria: [living_uk, visiting_bring_pet, visiting_eu, visiting_ie],
+            },
+
+          ]
+          expect(grouped_citizen_actions).to eq grouped_actions_fixture
+        end
       end
     end
   end
-
   describe "#populate_business_groups" do
     let(:action1) { FactoryBot.build(:brexit_checker_action, criteria: %w[owns-operates-business-organisation automotive]) }
     let(:action2) { FactoryBot.build(:brexit_checker_action, criteria: %w[aero-space]) }
