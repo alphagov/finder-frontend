bdeca69aed996ec19573b00ddb654f119b7364b4
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-09-21 13:58:51 +0100

    Add feature tests for new account path
    We want to be able to merge this feature into main and keep existing
    functionality, only switching them over when we are ready.
    
    As a result we've duplicated some of the tests here. The pre-existing
    tests for email signups from the transition results page will run if no
    feature flag is set or it is false.
    
    If the feature flag is true, then we will run the new set of tests.
    These include both the tests for the account signup feature and a
    modified email signup feature (given it's location has had to changed).
    
    Once the test is live we can go back and clear up the old code and
    remove the feature flag.

--- spec/features/brexit_checker/email_signup_spec.rb
+++ spec/features/brexit_checker/email_signup_spec.rb
@@ -16,20 +16,50 @@ RSpec.feature "Brexit Checker email signup", type: :feature do
     }
   end
 
-  scenario "user clicks to signup to email alerts with existing subscriber list" do
-    given_im_on_the_results_page
-    and_email_alert_api_has_subscriber_list
-    then_i_click_to_signup_to_emails
-    and_i_am_taken_to_email_alert_frontend
+  context "without the GOV.UK Account feature flag" do
+    scenario "user clicks to signup to email alerts with existing subscriber list" do
+      given_im_on_the_results_page
+      and_email_alert_api_has_subscriber_list
+      then_i_click_to_subscribe # on main results page
+      then_i_click_to_subscribe # on overview of subscription page
+      and_i_am_taken_to_email_alert_frontend
+    end
+
+    scenario "user clicks to signup to email alerts without existing subscriber list" do
+      given_im_on_the_results_page
+      and_email_alert_api_does_not_have_subscriber_list
+      and_email_alert_api_creates_subscriber_list
+      then_i_click_to_subscribe # on main results page
+      then_i_click_to_subscribe # on overview of subscription page
+      and_the_subscriber_list_was_created
+      and_i_am_taken_to_email_alert_frontend
+    end
   end
 
-  scenario "user clicks to signup to email alerts without existing subscriber list" do
-    given_im_on_the_results_page
-    and_email_alert_api_does_not_have_subscriber_list
-    and_email_alert_api_creates_subscriber_list
-    then_i_click_to_signup_to_emails
-    and_the_subscriber_list_was_created
-    and_i_am_taken_to_email_alert_frontend
+  context "with the GOV.UK Account feature flag" do
+    scenario "user clicks to signup to email alerts with existing subscriber list" do
+      given_im_on_the_results_page
+      and_email_alert_api_has_subscriber_list
+      then_i_click_to_subscribe
+      and_i_am_taken_to_choose_how_to_subscribe_page
+      then_i_click_email_alerts_only
+      and_i_am_taken_to_email_alert_signup_page
+      then_i_click_to_subscribe
+      and_i_am_taken_to_email_alert_frontend
+    end
+
+    scenario "user clicks to signup to email alerts without existing subscriber list" do
+      given_im_on_the_results_page
+      and_email_alert_api_does_not_have_subscriber_list
+      and_email_alert_api_creates_subscriber_list
+      then_i_click_to_subscribe
+      and_i_am_taken_to_choose_how_to_subscribe_page
+      then_i_click_email_alerts_only
+      and_i_am_taken_to_email_alert_signup_page
+      then_i_click_to_subscribe
+      and_the_subscriber_list_was_created
+      and_i_am_taken_to_email_alert_frontend
+    end
   end
 
   def given_im_on_the_results_page
@@ -49,9 +79,12 @@ RSpec.feature "Brexit Checker email signup", type: :feature do
                         .with(body: subscriber_list.except("slug").as_json)
   end
 
-  def then_i_click_to_signup_to_emails
-    click_on "Subscribe" # on main results page
-    click_on "Subscribe" # on overview of subscription page
+  def then_i_click_email_alerts_only
+    click_on "stay up to date with email alerts only"
+  end
+
+  def then_i_click_to_subscribe
+    click_on "Subscribe"
   end
 
   def and_the_subscriber_list_was_created
@@ -60,5 +93,13 @@ RSpec.feature "Brexit Checker email signup", type: :feature do
 
   def and_i_am_taken_to_email_alert_frontend
     expect(page).to have_current_path(email_alert_frontend_signup_path(topic_id: "your-get-ready-for-brexit-results-a1a2a3a4a5"))
+  end
+
+  def and_i_am_taken_to_email_alert_signup_page
+    expect(page).to have_current_path(transition_checker_email_signup_url(c: %w[nationality-eu]))
+  end
+
+  def and_i_am_taken_to_choose_how_to_subscribe_page
+    expect(page).to have_current_path(transition_checker_save_results_url(c: %w[nationality-eu]))
   end
 end
