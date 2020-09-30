375c26d37f651afba280f3c7aa1d54a4cb7f45a4
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2020-09-30 12:38:03 +0100

    Add service to lookup location data by postcode
    The service follows the pattern of other services across GOV.UK for
    example [find local council](https://www.gov.uk/find-local-council) and
    uses Mapit to do the lookup.
    
    This data will be used to determine if postcode has local Covid-19
    restrictions by using the GSS code of the area it is in an mapping that
    to the local restrictions data.
    
    Unfortunately, a postcode can be in multiple areas, not all of which
    have a GSS code. We considered only returning the area that is
    considered a Lower Tier Local Authority (LTLA), but there is no
    consistency in how these areas are described in Mapit.
    Instead we are returning all areas for a postcode that have a GSS code,
    and expect that the caller of this service will loop through all of the
    areas and compare each GSS code to the restrictions data.

---
+++ spec/services/location_lookup_service_spec.rb
@@ -0,0 +1,77 @@
+require "spec_helper"
+require "gds_api/test_helpers/mapit"
+
+RSpec.describe LocationLookupService do
+  include GdsApi::TestHelpers::Mapit
+
+  describe "#data" do
+    it "returns location data" do
+      postcode = "E18QS"
+      areas = [
+        {
+          "ons" => "01",
+          "gss" => "E01000123",
+          "govuk_slug" => "test-one",
+          "name" => "Coruscant Planetary Council",
+          "type" => "LBO",
+          "country_name" => "England",
+        },
+        {
+          "ons" => "02",
+          "gss" => "E02000456",
+          "govuk_slug" => "test-two",
+          "name" => "Galactic Empire",
+          "type" => "GLA",
+          "country_name" => "England",
+        },
+      ]
+      stub_mapit_has_a_postcode_and_areas(postcode, [], areas)
+
+      expected_data = [
+        {
+          gss: "E01000123",
+          area_name: "Coruscant Planetary Council",
+          country: "England",
+        },
+        {
+          gss: "E02000456",
+          area_name: "Galactic Empire",
+          country: "England",
+        },
+      ]
+
+      expect(described_class.new(postcode).data).to eq(expected_data)
+    end
+
+    it "only returns locations with a gss code" do
+      postcode = "E18QS"
+      areas = [
+        {
+          "ons" => "01",
+          "gss" => "E01000123",
+          "govuk_slug" => "test-one",
+          "name" => "Coruscant Planetary Council",
+          "type" => "LBO",
+          "country_name" => "England",
+        },
+        {
+          "govuk_slug" => "test-two",
+          "name" => "Galactic Empire",
+          "type" => "GLA",
+          "country_name" => "England",
+        },
+      ]
+      stub_mapit_has_a_postcode_and_areas(postcode, [], areas)
+
+      expected_data = [
+        {
+          gss: "E01000123",
+          area_name: "Coruscant Planetary Council",
+          country: "England",
+        },
+      ]
+
+      expect(described_class.new(postcode).data).to eq(expected_data)
+    end
+  end
+end
