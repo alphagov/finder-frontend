46320199429bc29b16e166e06afdb4dee73e7eea
Michael Walker <mike@barrucadu.co.uk>
2020-10-27 18:00:36 +0000

    Enable sessions again
    We need this for CSRF on the change your answers form.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-FinderFrontend::Application.config.session_store :disabled
+FinderFrontend::Application.config.session_store :cookie_store, key: "_finder-frontend_session", expire_after: 15.minutes
