3b0a1a858d73a0c12d9cd8520f04e06c1c5fc09d
Michael Walker <mike@barrucadu.co.uk>
2020-10-28 10:56:00 +0000

    Use A/B test machinery to show signed in / out header

--- spec/controllers/brexit_checker_controller_spec.rb
+++ spec/controllers/brexit_checker_controller_spec.rb
@@ -51,4 +51,33 @@ describe BrexitCheckerController, type: :controller do
       end
     end
   end
+
+  context "accounts header AB test setup" do
+    before do
+      allow(Rails.configuration).to receive(:feature_flag_govuk_accounts).and_return(true)
+    end
+
+    %w[LoggedIn LoggedOut].each do |variant|
+      it "Variant #{variant} disables the search field" do
+        with_variant AccountExperiment: variant do
+          get :results
+          expect(response.headers["X-Slimmer-Remove-Search"]).to eq("true")
+        end
+      end
+    end
+
+    it "Variant LoggedIn requests the signed-in header" do
+      with_variant AccountExperiment: "LoggedIn" do
+        get :results
+        expect(response.headers["X-Slimmer-Show-Accounts"]).to eq("signed-in")
+      end
+    end
+
+    it "Variant LoggedOut requests the signed-out header" do
+      with_variant AccountExperiment: "LoggedOut" do
+        get :results
+        expect(response.headers["X-Slimmer-Show-Accounts"]).to eq("signed-out")
+      end
+    end
+  end
 end
