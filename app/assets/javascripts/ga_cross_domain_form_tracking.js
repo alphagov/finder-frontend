e9a825362f68ed3ea1b28a38187cb5782bee89bb
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-11-18 17:19:17 +0000

    Converted existing x-domain ga tracking to module
    The Module pattern is the preferred and future pattern for all GOV.UK js
    scripts. Its use gives convention and consistency (by namespacing and
    getting performance benefits from using prototypes).
    
    Here we shift the script into the module pattern

--- app/assets/javascripts/ga_cross_domain_form_tracking.js
+++ app/assets/javascripts/ga_cross_domain_form_tracking.js
@@ -1,25 +1,24 @@
 /* eslint-env jquery */
 /* global ga */
 
-function attachCrossDomainTrackerToInput (form, trackers) {
-  if (trackers.length) {
-    var existingAction = form.attr('action')
-    var linker = new window.gaplugins.Linker(trackers[0])
-    var trackedAction = linker.decorate(existingAction)
-    form.attr('action', trackedAction)
-  }
-}
+(function(Modules) {
+  'use strict'
 
-window.GOVUK = window.GOVUK || {}
-var GOVUK = window.GOVUK
-GOVUK.attachCrossDomainTrackerToInput = attachCrossDomainTrackerToInput
-
-$(window).on('load', function () {
-  if (window.ga !== undefined && typeof window.ga.getAll === 'function') {
-    var trackers = ga.getAll()
-    var form = $('form#account-signup')
-    if (form.length) {
-      window.GOVUK.attachCrossDomainTrackerToInput(form, trackers)
+  function AttachCrossDomainTrackerToInput () {}
+  AttachCrossDomainTrackerToInput.prototype.start = function($form) {
+    console.log("Object", window.ga)
+    console.log("undefined test", window.ga !== undefined)
+    console.log("getAll is a function", typeof window.ga.getAll === 'function')
+    if (window.ga !== undefined && typeof window.ga.getAll === 'function') {
+      console.log(window.ga.getAll())
+      var trackers = window.ga.getAll()
+      if (trackers.length) {
+        var existingAction = $form.attr('action')
+        var linker = new window.gaplugins.Linker(trackers[0])
+        var trackedAction = linker.decorate(existingAction)
+        $form.attr('action', trackedAction)
+      }
     }
   }
-})
+  Modules.AttachCrossDomainTrackerToInput = AttachCrossDomainTrackerToInput
+})(window.GOVUK.Modules)
