2be33fa13d29845bf8f0e400fdbf4a62448ec7d0
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2020-12-01 13:10:33 +0000

    Fix missing registry name change
    Follows on from previous commits

--- app/helpers/title_context_helper.rb
+++ app/helpers/title_context_helper.rb
@@ -6,7 +6,7 @@ module TitleContextHelper
     topical_events = [topical_events] if topical_events.is_a? String
 
     if topical_events && topical_events.count == 1
-      registry = Services.registries.all["full_topical_events"]
+      registry = Services.registries.all["topical_events"]
       (registry[topical_events.first] || {})["title"]
     end
   end
