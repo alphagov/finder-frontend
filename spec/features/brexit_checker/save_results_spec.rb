c09fab488f51ef26228fa1f8959192e9800a6e39
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-12-31 12:30:30 +0000

    Add feature tests for enabled accounts returning 503

--- spec/features/brexit_checker/save_results_spec.rb
+++ spec/features/brexit_checker/save_results_spec.rb
@@ -16,17 +16,37 @@ RSpec.feature "Brexit Checker create GOV.UK Account", type: :feature do
     allow_any_instance_of(OidcClient).to receive(:discover).and_return(discovery_response)
     allow_any_instance_of(OidcClient).to receive(:auth_uri).and_return({ uri: "http://account-mamager/login", state: SecureRandom.hex(16) })
     stub_email_subscription_confirmation
+    stub_request(:get, Services.accounts_api).to_return(status: 200)
   end
 
-  scenario "user clicks Create a GOV.UK account" do
-    given_im_on_the_results_page
-    then_i_click_to_subscribe
-    and_i_am_taken_to_choose_how_to_subscribe_page
-    i_see_a_create_account_button
+  context "accounts is enabled" do
+    scenario "user clicks Create a GOV.UK account" do
+      given_im_on_the_results_page
+      then_i_click_to_subscribe
+      and_i_am_taken_to_choose_how_to_subscribe_page
+      i_see_a_create_account_button
+    end
+  end
+
+  context "accounts is enabled but is returning 503" do
+    before do
+      stub_request(:get, Services.accounts_api).to_return(status: 503)
+    end
+
+    scenario "user is only given the chance to subscribe via email" do
+      given_im_on_the_results_page
+      then_i_click_to_subscribe
+      i_see_the_subscribe_by_email_page
+    end
   end
 
   def given_im_on_the_results_page
     visit transition_checker_results_url(c: %w[nationality-eu])
+  end
+
+  def i_see_the_subscribe_by_email_page
+    expect(page).to have_current_path(transition_checker_email_signup_url(c: %w[nationality-eu]))
+    expect(page).to have_content(I18n.t("brexit_checker.email_signup.sign_up_heading"))
   end
 
   def then_i_click_to_subscribe
