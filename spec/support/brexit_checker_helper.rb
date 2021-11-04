ef36b52cbe3cd6f7eb7f6f09cd44fdb085794adc
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2021-11-04 14:41:58 +0000

    Add notice to indicate closure of the checker
    This will be displayed on all question pages and the results page. This
    is a bit invasive, but it's important to flag, because people will have
    to think about their options if they want to keep getting Brexit emails.

--- spec/support/brexit_checker_helper.rb
+++ spec/support/brexit_checker_helper.rb
@@ -3,6 +3,7 @@ module BrexitCheckerHelper
     question = BrexitChecker::Question.find_by_key(key)
     expect(page).to have_content(question.text)
     expect(page).to have_content(question.caption)
+    expect(page).to have_text("The Brexit checker will be removed")
     options.each { |o| find_field(o).click }
     click_on "Continue"
   end
