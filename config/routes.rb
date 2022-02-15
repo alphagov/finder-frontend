d14aeb074980a22ce41d05633298c59b8abefe6a
Alex Baker <70726455+alexbakervr@users.noreply.github.com>
2022-02-15 11:42:37 +0000

    Testing healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/sign-in", to: proc { [200, {}, %w[OK]] }
 
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
+  get "/healthcheck/ready", to: proc { [200, {}, %w[OK]] }
 
   root to: redirect("/development") unless Rails.env.test?
   get "/development" => "development#index"
