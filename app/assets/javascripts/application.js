1ca70c87b7f709ead7e70c36c9efb5f7e2106551
Ian James <ian.james@digital.cabinet-office.gov.uk>
2022-07-05 20:08:45 +0100

    Remove stylesheets served by Static

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,7 @@
 //= require govuk_publishing_components/lib
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/checkboxes
-//= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/error-summary
-//= require govuk_publishing_components/components/feedback
 //= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/analytics/explicit-cross-domain-links
