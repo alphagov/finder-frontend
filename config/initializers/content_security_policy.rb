e6632fc304f99acec72549d84d5d54e6029bd6ce
Aga Dufrat <aga.dufrat@digital.cabinet-office.gov.uk>
2023-10-09 09:49:25 +0100

    Testing CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,1 +1,10 @@
-GovukContentSecurityPolicy.configure
+GOVUK_DOMAINS = [
+    "*.publishing.service.gov.uk",
+    "*.#{ENV['GOVUK_APP_DOMAIN_EXTERNAL'] || ENV['GOVUK_APP_DOMAIN'] || 'dev.gov.uk'}",
+    "www.gov.uk",
+    "*.dev.gov.uk",
+  ].uniq.freeze
+
+GovukContentSecurityPolicy.configure do |policy|
+    policy.frame_ancestors :self, *GOVUK_DOMAINS
+end
