c7c1d0def9cfb52f28d19c77c403b774f21da964
Matt Hill <matt.hill@digital.cabinet-office.gov.uk>
2024-01-19 15:20:31 +0000

    Configure builds
    - Create `dartsass.builds` initializer and add all Sass files to be
      compiled. See https://github.com/rails/dartsass-rails#configuring-builds
    - Create .keep
    - Update manifest file to use /builds directory
    - Ignore /builds

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,9 +2,5 @@
 //= link application.js
 //= link test-dependencies.js
 //= link live_search.js
-//= link application.css
 
-//= link components/_expander.css
-//= link components/_mobile-filters.css
-
-//= link views/_search.css
+//= link_tree ../builds
---
+++ config/initializers/dartsass.rb
@@ -0,0 +1,11 @@
+APP_STYLESHEETS = {
+  "application.scss" => "application.css",
+  "components/_expander.scss" => "components/_expander.css",
+  "components/_mobile-filters.scss" => "components/_mobile-filters.css",
+  "views/_search.scss" => "views/_search.css",
+}.freeze
+
+all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.all_stylesheets)
+Rails.application.config.dartsass.builds = all_stylesheets
+
+Rails.application.config.dartsass.build_options << " --quiet-deps"
