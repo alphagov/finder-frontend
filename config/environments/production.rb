0b589ddbb898d8280038127b05d610761af8a90a
Matt Hill <matt.hill@digital.cabinet-office.gov.uk>
2024-01-19 15:35:41 +0000

    Delete unused Sass config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,10 +31,6 @@ Rails.application.configure do
   # Compress JS using a preprocessor.
   config.assets.js_compressor = :uglifier
 
-  # Rather than use a CSS compressor, use the SASS style to perform compression.
-  config.sass.style = :compressed
-  config.sass.line_comments = false
-
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.assets.digest = true
