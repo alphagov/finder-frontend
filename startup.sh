c45d7c6180ae2ea9bda7f3e97de6d81ffd44d16a
Matt Hill <matt.hill@digital.cabinet-office.gov.uk>
2024-01-29 11:50:22 +0000

    Add watch task
    Also, turn digests off in development
    
    Remove the `/bin` folder from `.gitignore` to ensure the bin/dev file is included in the commit.

--- startup.sh
+++ startup.sh
@@ -11,7 +11,7 @@ if [[ $1 == "--live" ]] ; then
   PLEK_SERVICE_CONTENT_STORE_URI=${PLEK_SERVICE_CONTENT_STORE_URI-https://www.gov.uk/api} \
   PLEK_SERVICE_STATIC_URI=${PLEK_SERVICE_STATIC_URI-https://assets.publishing.service.gov.uk} \
   PLEK_SERVICE_WHITEHALL_FRONTEND_URI=https://www.gov.uk \
-  bundle exec rails s -p 3062
+  ./bin/dev
 else
   echo "ERROR: other startup modes are not supported"
   echo ""
