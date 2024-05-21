efc3f114bdb0e7475f7ef0b250ea506bbe5abad4
Keith Lawrence <keith.lawrence@digital.cabinet-office.gov.uk>
2024-05-21 12:03:31 +0100

    Fix deprecation message
    - Comparing Parameters with Hash deprecated, will be removed in Rails 7.2

--- spec/lib/params_cleaner_spec.rb
+++ spec/lib/params_cleaner_spec.rb
@@ -10,13 +10,13 @@ describe ParamsCleaner do
 
     it "leaves normal params alone" do
       params = ActionController::Parameters.new("foo" => "bar")
-      cleaned = ParamsCleaner.new(params).cleaned
+      cleaned = ActionController::Parameters.new(ParamsCleaner.new(params).cleaned)
       expect(cleaned).to match(params)
     end
 
     it "does not touch other types of hash-params " do
       params = ActionController::Parameters.new("foo" => { "from" => "bar", "to" => "baz" })
-      cleaned = ParamsCleaner.new(params).cleaned
+      cleaned = ActionController::Parameters.new(ParamsCleaner.new(params).cleaned)
       expect(cleaned).to match(params)
     end
 
