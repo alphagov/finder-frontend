b95a95322774f725a58082aa3e362b2389976da0
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-09-11 15:49:04 +0100

    pr comments

--- app/assets/javascripts/components/filter-panel.js
+++ app/assets/javascripts/components/filter-panel.js
@@ -9,35 +9,23 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
       this.$module = $module
       this.$button = this.$module.querySelector('.app-c-filter-panel__button')
       this.$panel = this.$module.querySelector(`#${this.$button.getAttribute('aria-controls')}`)
-      this.$button.setAttribute('aria-expanded', 'false')
-      this.$panel.setAttribute('hidden', '')
-      this.isOpen = false
     }
 
     init () {
+      this.$button.setAttribute('aria-expanded', 'false')
+      this.$panel.setAttribute('hidden', '')
       this.$button.addEventListener('click', this.onButtonClick.bind(this))
-      this.$button.addEventListener('blur', this.onButtonBlur.bind(this))
-    }
-
-    onButtonBlur () {
-      this.$button.classList.remove('app-c-filter-panel__button--focused')
     }
 
     onButtonClick (event) {
       event.preventDefault()
-      this.$button.classList.add('app-c-filter-panel__button--focused')
       this.toggle()
     }
 
     toggle () {
-      this.isOpen = !this.isOpen
-      this.$button.setAttribute('aria-expanded', `${this.isOpen}`)
-
-      if (this.isOpen) {
-        this.$panel.removeAttribute('hidden')
-      } else {
-        this.$panel.setAttribute('hidden', '')
-      }
+      const newState = this.$button.getAttribute('aria-expanded') !== 'true'
+      this.$button.setAttribute('aria-expanded', newState)
+      this.$panel.toggleAttribute('hidden')
     }
   }
 
--- spec/javascripts/components/filter-panel-spec.js
+++ spec/javascripts/components/filter-panel-spec.js
@@ -13,16 +13,13 @@ describe('Filter panel module', () => {
   const html = `<div data-module="filter-panel" class="app-c-filter-panel">
     <div class="app-c-filter-panel__header">
       <button class="app-c-filter-panel__button govuk-link" aria-controls="filter-panel" id="filters-button">
-        <span class="accordion-title">
-          <span class="app-c-filter-panel__button-icon"></span>
-          Filter and sort
-        </span>
+        <span class="app-c-filter-panel__button-icon"></span>
+        Filter and sort
       </button>
       <h2 class="gem-c-heading govuk-heading-s">
         123,456 results
       </h2>
     </div>
-
     <div class="app-c-filter-panel__content" id="filter-panel" role="region" aria-labelledby="filters-button">
       <p class="govuk-body">
         I can contain arbitrary content, usually a set of filters and sort options.
@@ -52,14 +49,6 @@ describe('Filter panel module', () => {
     filterPanel.$button.click()
     expect(filterPanel.$button.getAttribute('aria-expanded')).toBe('true')
     expect(filterPanel.$panel.hasAttribute('hidden')).toBe(false)
-    expect(filterPanel.$button.classList.contains('app-c-filter-panel__button--focused')).toBe(true)
-  })
-
-  it('default behaviour of the panel open/close is prevented', () => {
-    filterPanel.$button.addEventListener('click', (event) => {
-      expect(event.defaultPrevented).toBe(true)
-    })
-    filterPanel.$button.click()
   })
 
   it('sets the correct attributes when panel is closed', () => {
@@ -67,12 +56,13 @@ describe('Filter panel module', () => {
     filterPanel.$button.click()
     expect(filterPanel.$button.getAttribute('aria-expanded')).toBe('false')
     expect(filterPanel.$panel.hasAttribute('hidden')).toBe(true)
-    expect(filterPanel.$button.classList.contains('app-c-filter-panel__button--focused')).toBe(true)
+    expect(document.activeElement).not.toBe(filterPanel.$button)
   })
 
-  it('removes focus class when button is blurred', () => {
+  it('default behaviour of the panel open/close is prevented', () => {
+    filterPanel.$button.addEventListener('click', (event) => {
+      expect(event.defaultPrevented).toBe(true)
+    })
     filterPanel.$button.click()
-    window.GOVUK.triggerEvent(filterPanel.$button, 'blur')
-    expect(filterPanel.$button.classList.contains('app-c-filter-panel__button--focused')).toBe(false)
   })
 })
