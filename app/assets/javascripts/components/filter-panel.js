8ca8007136adbb9ec81f51af3b5a29cfce246dc9
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-09-11 06:13:41 +0100

    filter panel behaviour/styling and tests

---
+++ app/assets/javascripts/components/filter-panel.js
@@ -0,0 +1,44 @@
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {};
+
+(function (Modules) {
+  'use strict'
+
+  class FilterPanel {
+    constructor ($module) {
+      this.$module = $module
+      this.$button = this.$module.querySelector('.app-c-filter-panel__button')
+      this.$panel = this.$module.querySelector(`#${this.$button.getAttribute('aria-controls')}`)
+      this.$button.setAttribute('aria-expanded', 'false')
+      this.$panel.setAttribute('hidden', '')
+      this.isOpen = false
+    }
+
+    init () {
+      this.$button.addEventListener('click', this.onButtonClick.bind(this))
+      this.$button.addEventListener('blur', this.onButtonBlur.bind(this))
+    }
+
+    onButtonBlur () {
+      this.$button.classList.remove('app-c-filter-panel__button--focused')
+    }
+
+    onButtonClick () {
+      this.$button.classList.add('app-c-filter-panel__button--focused')
+      this.toggle()
+    }
+
+    toggle () {
+      this.isOpen = !this.isOpen
+      this.$button.setAttribute('aria-expanded', `${this.isOpen}`)
+
+      if (this.isOpen) {
+        this.$panel.removeAttribute('hidden')
+      } else {
+        this.$panel.setAttribute('hidden', '')
+      }
+    }
+  }
+
+  Modules.FilterPanel = FilterPanel
+})(window.GOVUK.Modules)
--- spec/javascripts/components/filter-panel-spec.js
+++ spec/javascripts/components/filter-panel-spec.js
@@ -1,7 +1,71 @@
-describe('Filter panel module', function () {
+describe('Filter panel module', () => {
   'use strict'
 
-  describe('TODO', function () {
-    it('should have tests')
+  let filterPanel, fixture
+
+  const loadFilterPanelComponent = (markup) => {
+    fixture = document.createElement('div')
+    document.body.appendChild(fixture)
+    fixture.innerHTML = markup
+    filterPanel = new GOVUK.Modules.FilterPanel(fixture.querySelector('.app-c-filter-panel'))
+  }
+
+  const html = `<div data-module="filter-panel" class="app-c-filter-panel">
+    <div class="app-c-filter-panel__header">
+      <button class="app-c-filter-panel__button govuk-link" aria-controls="filter-panel" id="filters-button">
+        <span class="accordion-title">
+          <span class="app-c-filter-panel__button-icon"></span>
+          Filter and sort
+        </span>
+      </button>
+      <h2 class="gem-c-heading govuk-heading-s">
+        123,456 results
+      </h2>
+    </div>
+
+    <div class="app-c-filter-panel__content" id="filter-panel" role="region" aria-labelledby="filters-button">
+      <p class="govuk-body">
+        I can contain arbitrary content, usually a set of filters and sort options.
+      </p>
+    </div>
+  </div>`
+
+  beforeEach(() => {
+    loadFilterPanelComponent(html)
+    filterPanel.init()
+  })
+
+  afterEach(() => {
+    fixture.remove()
+  })
+
+  it('panel is labelled by the open/close button', () => {
+    expect(filterPanel.$panel.getAttribute('aria-labelledby')).toBe(filterPanel.$button.id)
+  })
+
+  it('initial state of the panel is closed', () => {
+    expect(filterPanel.$button.getAttribute('aria-expanded')).toBe('false')
+    expect(filterPanel.$panel.hasAttribute('hidden')).toBe(true)
+  })
+
+  it('sets the correct attributes when panel is opened', () => {
+    filterPanel.$button.click()
+    expect(filterPanel.$button.getAttribute('aria-expanded')).toBe('true')
+    expect(filterPanel.$panel.hasAttribute('hidden')).toBe(false)
+    expect(filterPanel.$button.classList.contains('app-c-filter-panel__button--focused')).toBe(true)
+  })
+
+  it('sets the correct attributes when panel is closed', () => {
+    filterPanel.$button.click()
+    filterPanel.$button.click()
+    expect(filterPanel.$button.getAttribute('aria-expanded')).toBe('false')
+    expect(filterPanel.$panel.hasAttribute('hidden')).toBe(true)
+    expect(filterPanel.$button.classList.contains('app-c-filter-panel__button--focused')).toBe(true)
+  })
+
+  it('removes focus class when button is blurred', () => {
+    filterPanel.$button.click()
+    window.GOVUK.triggerEvent(filterPanel.$button, 'blur')
+    expect(filterPanel.$button.classList.contains('app-c-filter-panel__button--focused')).toBe(false)
   })
 })
