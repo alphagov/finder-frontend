3fe39717af0d69b6affa388d4c262cb2eacef6d4
alexbowen <alexbowen123@googlemail.com>
2024-10-04 22:21:29 +0100

    use dynamic value for the filter sections count

--- spec/components/filter_panel_spec.rb
+++ spec/components/filter_panel_spec.rb
@@ -79,6 +79,7 @@ describe "Filter panel component", type: :view do
 
   it "renders ga4 tracking attributes to open/close button element" do
     button_text = "Filter"
+    section_count = 4
 
     button_event_attributes = {
       event_name: "select_content",
@@ -86,10 +87,10 @@ describe "Filter panel component", type: :view do
       section: button_text,
       text: button_text,
       index_section: 0,
-      index_section_count: 4,
+      index_section_count: section_count,
     }
 
-    render_component(button_text:)
+    render_component(button_text:, section_count:)
 
     assert_select ".app-c-filter-panel button[data-ga4-expandable]", true
     assert_select ".app-c-filter-panel button[data-ga4-event='#{button_event_attributes.to_json}']", true
@@ -97,6 +98,7 @@ describe "Filter panel component", type: :view do
 
   it "renders ga4 tracking attributes to submit button element" do
     button_text = "Apply filters"
+    section_count = 4
 
     button_event_attributes = {
       event_name: "select_content",
@@ -108,7 +110,7 @@ describe "Filter panel component", type: :view do
       index_section_count: 4,
     }
 
-    render_component(button_text:)
+    render_component(button_text:, section_count:)
 
     assert_select ".app-c-filter-panel__action--submit[data-ga4-event='#{button_event_attributes.to_json}']", true
   end
