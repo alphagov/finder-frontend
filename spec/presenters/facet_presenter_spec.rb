39c75683051bcc1a59bd4ed6a5d2c53e49d0ede7
Christian Sutter <git@doublesignal.com>
2024-10-15 06:38:22 +0000

    FacetPresenter: Move common attributes to presenter
    All of the new facet partials have several shared attributes that are
    better off living in the presenter rather than being repeated several
    times over in the partials.
    
    - Add `FacetPresenter#section_attributes` with common attributes and use
      in facet partials
    - Remove unnecessary (unused) HTML ID on sections
    - Tweak `FacetPresenter` spec to use different values for section index
      and count to avoid confusing the values in the future

--- spec/presenters/facet_presenter_spec.rb
+++ spec/presenters/facet_presenter_spec.rb
@@ -3,8 +3,15 @@ require "spec_helper"
 RSpec.describe FacetPresenter do
   subject(:facet_presenter) { described_class.new(facet, section_index, section_count) }
 
-  let(:facet) { instance_double(Facet, key: "a_facet") }
-  let(:section_index) { 1 }
+  let(:facet) do
+    instance_double(
+      Facet,
+      key: "a_facet",
+      name: "A facet",
+      status_text: "42 selected",
+    )
+  end
+  let(:section_index) { 0 }
   let(:section_count) { 2 }
 
   it "delegates methods to the facet" do
@@ -13,7 +20,7 @@ RSpec.describe FacetPresenter do
 
   describe "#section_index" do
     it "exposes the section index as a 1-based index" do
-      expect(facet_presenter.section_index).to eq(2)
+      expect(facet_presenter.section_index).to eq(1)
     end
 
     context "when no section index is provided" do
@@ -28,6 +35,23 @@ RSpec.describe FacetPresenter do
   describe "#section_count" do
     it "exposes the section count" do
       expect(facet_presenter.section_count).to eq(2)
+    end
+  end
+
+  describe "#section_attributes" do
+    it "returns a hash of common attributes for the filter section component" do
+      expect(facet_presenter.section_attributes).to eq({
+        heading_text: "A facet",
+        status_text: "42 selected",
+        index_section: 1,
+        index_section_count: 2,
+        data_attributes: {
+          ga4_index: {
+            index_section: 1,
+            index_section_count: 2,
+          },
+        },
+      })
     end
   end
 end
