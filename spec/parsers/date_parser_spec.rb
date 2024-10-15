abc10ce00fc0e9862810a2a6c708f72811cde677
Christian Sutter <git@doublesignal.com>
2024-10-15 11:52:17 +0000

    DateParser: Tidy up spec
    This cleans up the spec and makes it use modern RSpec in preparation for
    refactoring.

--- spec/parsers/date_parser_spec.rb
+++ spec/parsers/date_parser_spec.rb
@@ -1,103 +1,109 @@
 require "spec_helper"
 
 describe DateParser do
-  # These dates have been chosen based on analytics from site search more info here: https://designpatterns.hackpad.com/Dates-vpx6XlVjIbE
-  this_year = Time.zone.now.year
-  dates = { # Zero padded, full year, various delimiters
-    "21/01/2002" => Date.new(2002, 1, 21),
-    "21.01.2002" => Date.new(2002, 1, 21),
-    "21-01-2002" => Date.new(2002, 1, 21),
-    "21 01 2002" => Date.new(2002, 1, 21),
-    "21012002" => Date.new(2002, 1, 21),
+  subject(:parsed_date) { described_class.new.parse(input) }
 
-    # Zero padded, abbreviated year, various delimiters
-    "21/01/14" => Date.new(2014, 1, 21),
-    "21.01.14" => Date.new(2014, 1, 21),
-    "21-01-14" => Date.new(2014, 1, 21),
+  context "when given a string date" do
+    # These dates have been chosen based on analytics from site search more info here: https://designpatterns.hackpad.com/Dates-vpx6XlVjIbE
+    this_year = Time.zone.now.year
+    dates = { # Zero padded, full year, various delimiters
+      "21/01/2002" => Date.new(2002, 1, 21),
+      "21.01.2002" => Date.new(2002, 1, 21),
+      "21-01-2002" => Date.new(2002, 1, 21),
+      "21 01 2002" => Date.new(2002, 1, 21),
+      "21012002" => Date.new(2002, 1, 21),
 
-    # Zero padded, abbreviated year, last century
-    "21/01/99" => Date.new(1999, 1, 21),
-    "21.01.99" => Date.new(1999, 1, 21),
-    "21-01-99" => Date.new(1999, 1, 21),
-    "21 01 99" => Date.new(1999, 1, 21),
+      # Zero padded, abbreviated year, various delimiters
+      "21/01/14" => Date.new(2014, 1, 21),
+      "21.01.14" => Date.new(2014, 1, 21),
+      "21-01-14" => Date.new(2014, 1, 21),
 
-    # Unpadded dates, various delimiters
-    "21/1/2002" => Date.new(2002, 1, 21),
-    "21-1-2002" => Date.new(2002, 1, 21),
-    "21.1.2002" => Date.new(2002, 1, 21),
-    "21 1 2002" => Date.new(2002, 1, 21),
+      # Zero padded, abbreviated year, last century
+      "21/01/99" => Date.new(1999, 1, 21),
+      "21.01.99" => Date.new(1999, 1, 21),
+      "21-01-99" => Date.new(1999, 1, 21),
+      "21 01 99" => Date.new(1999, 1, 21),
 
-    # Other dates that people have entered on site search
-    "21st january 2014" => Date.new(2014, 1, 21),
-    "21 January 2014" => Date.new(2014, 1, 21),
-    "september 2014" => Date.new(2014, 9, 1),
-    "2008" => Date.new(2008, 1, 1),
-    "1234" => Date.new(1234, 1, 1),
-    "2004/6/1" => Date.new(2004, 6, 1),
-    "09/2013" => Date.new(2013, 9, 1),
-    "22 Sept 2014" => Date.new(2014, 9, 22),
-    "010101" => Date.new(2001, 1, 1),
+      # Unpadded dates, various delimiters
+      "21/1/2002" => Date.new(2002, 1, 21),
+      "21-1-2002" => Date.new(2002, 1, 21),
+      "21.1.2002" => Date.new(2002, 1, 21),
+      "21 1 2002" => Date.new(2002, 1, 21),
 
-    # Invalid dates that should raise an error
-    "31/15/14" => nil,
-    "1@" => nil,
-    "20120" => nil,
-    "1" => nil,
-    "randomwords" => nil,
-    "Britain First" => nil,
-    "6   april    2018 to april 2018" => nil,
+      # Other dates that people have entered on site search
+      "21st january 2014" => Date.new(2014, 1, 21),
+      "21 January 2014" => Date.new(2014, 1, 21),
+      "september 2014" => Date.new(2014, 9, 1),
+      "2008" => Date.new(2008, 1, 1),
+      "1234" => Date.new(1234, 1, 1),
+      "2004/6/1" => Date.new(2004, 6, 1),
+      "09/2013" => Date.new(2013, 9, 1),
+      "22 Sept 2014" => Date.new(2014, 9, 22),
+      "010101" => Date.new(2001, 1, 1),
 
-    # Dates should be interpretted as UK not US
-    "01/11/2014" => Date.new(2014, 11, 1),
+      # Invalid dates that should raise an error
+      "31/15/14" => nil,
+      "1@" => nil,
+      "20120" => nil,
+      "1" => nil,
+      "randomwords" => nil,
+      "Britain First" => nil,
+      "6   april    2018 to april 2018" => nil,
 
-    # Future date
-    "22/09/25" => Date.new(2025, 9, 22),
+      # Dates should be interpretted as UK not US
+      "01/11/2014" => Date.new(2014, 11, 1),
 
-    # Blank dates
-    "" => nil,
-    nil => nil,
+      # Future date
+      "22/09/25" => Date.new(2025, 9, 22),
 
-    # Months only
-    "January" => Date.new(this_year, 1, 1),
-    "February" => Date.new(this_year, 2, 1),
-    "March" => Date.new(this_year, 3, 1),
-    "April" => Date.new(this_year, 4, 1),
-    "May" => Date.new(this_year, 5, 1),
-    "June" => Date.new(this_year, 6, 1),
-    "July" => Date.new(this_year, 7, 1),
-    "August" => Date.new(this_year, 8, 1),
-    "September" => Date.new(this_year, 9, 1),
-    "October" => Date.new(this_year, 10, 1),
-    "November" => Date.new(this_year, 11, 1),
-    "December" => Date.new(this_year, 12, 1),
-  }
+      # Blank dates
+      "" => nil,
+      nil => nil,
 
-  dates.each_pair do |input, expected|
-    it "returns the correct date for #{input}" do
-      expect(described_class.new.parse(input)).to eql(expected)
+      # Months only
+      "January" => Date.new(this_year, 1, 1),
+      "February" => Date.new(this_year, 2, 1),
+      "March" => Date.new(this_year, 3, 1),
+      "April" => Date.new(this_year, 4, 1),
+      "May" => Date.new(this_year, 5, 1),
+      "June" => Date.new(this_year, 6, 1),
+      "July" => Date.new(this_year, 7, 1),
+      "August" => Date.new(this_year, 8, 1),
+      "September" => Date.new(this_year, 9, 1),
+      "October" => Date.new(this_year, 10, 1),
+      "November" => Date.new(this_year, 11, 1),
+      "December" => Date.new(this_year, 12, 1),
+    }
+
+    dates.each_pair do |date, expected|
+      context "with date '#{date}'" do
+        let(:input) { date }
+
+        it { is_expected.to eql(expected) }
+
+        context "with leading whitespace" do
+          let(:input) { " #{date}" }
+
+          it { is_expected.to eql(expected) }
+        end
+
+        context "with trailing whitespace" do
+          let(:input) { "#{date} " }
+
+          it { is_expected.to eql(expected) }
+        end
+      end
     end
 
-    it "returns the correct date for #{input} with trailing whitespace" do
-      expect(described_class.new.parse("#{input} ")).to eql(expected)
+    context "for dates without years" do
+      let(:pretend_today) { Time.zone.local(2001, 3, 11) }
+      let(:input) { "26 november" }
+
+      before do
+        allow(Time).to receive(:now).and_return(pretend_today)
+      end
+
+      it { is_expected.to eql(Date.new(2001, 11, 26)) }
     end
-
-    it "returns the correct date for #{input} with preceeding whitespace" do
-      expect(described_class.new.parse(" #{input}")).to eql(expected)
-    end
-  end
-
-  it "handles dates without years correctly" do
-    date_to_parse = "26 november"
-
-    year = 2001
-
-    # Expected date is the date we've given it with the year returned by stubbed time.now
-    expected_date = Date.new(year, 11, 26)
-
-    # Stub Time.now to a known date
-    pretend_today = Time.zone.local(year, 3, 11)
-    allow(Time).to receive(:now).and_return(pretend_today)
-
-    expect(described_class.new.parse(date_to_parse)).to eql(expected_date)
   end
 end
