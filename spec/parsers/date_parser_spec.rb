a024c9bd71eb96470f8fe7cbba9a3bf9025d031f
Christian Sutter <git@doublesignal.com>
2024-10-15 14:18:01 +0000

    Implement basic `DateHashParser`
    This parses a hash as provided by the `date_input` component into a Ruby
    date for further validation and use throughout the app.

--- spec/parsers/date_parser_spec.rb
+++ spec/parsers/date_parser_spec.rb
@@ -3,11 +3,21 @@ require "spec_helper"
 describe DateParser do
   subject(:date_parser) { described_class.new(date_param) }
 
-  let(:date_param) { "13/12/1989" }
-
   describe "#parse" do
-    it "delegates to DateStringParser" do
-      expect(date_parser.parse).to eq(Date.new(1989, 12, 13))
+    context "when given a string" do
+      let(:date_param) { "13/12/1989" }
+
+      it "delegates to DateStringParser" do
+        expect(date_parser.parse).to eq(Date.new(1989, 12, 13))
+      end
+    end
+
+    context "when given a hash" do
+      let(:date_param) { { day: 17, month: 8, year: 2024 } }
+
+      it "delegates to DateHashParser" do
+        expect(date_parser.parse).to eq(Date.new(2024, 8, 17))
+      end
     end
   end
 end
