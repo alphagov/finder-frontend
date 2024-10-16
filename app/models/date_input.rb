79eedd14b7358b64a3d73896bd5d89554d28ea90
Christian Sutter <git@doublesignal.com>
2024-10-16 14:30:36 +0000

    DateInput: Allow converting to hash or string
    As we want both the old UI to be able to use query parameters intended
    for the new UI, and vice versa, for example when the user has a link to
    a search bookmarked, we need to be able to convert between the two for
    display purposes (to show the value in the form field).
    
    This adds a `#to_str` and `#to_hash` method to `DateInput` to achieve
    this, which we will use in a future commit to display the values in the
    respective form fields.

--- app/models/date_input.rb
+++ app/models/date_input.rb
@@ -1,5 +1,6 @@
 # Represents a user's date input, either based on a raw string input or a day/month/year hash as
-# resulting from the standard three-field date input.
+# resulting from the standard three-field date input, and allows converting between the two so both
+# old and new UI can work with either format when they come through as query parameters.
 class DateInput
   attr_reader :original_input, :date
 
@@ -14,5 +15,35 @@ class DateInput
 
   def to_iso8601
     date&.iso8601
+  end
+
+  # Converts the original input to a string suitable for use in the legacy UI plain text date field
+  def to_str
+    case original_input
+    when Hash
+      # e.g. 07/07/1989 (not GOV.UK standard date format, but the hint text we suggest to users)
+      date&.strftime("%0d/%0m/%-Y") || ""
+    when String
+      original_input
+    else
+      ""
+    end
+  end
+
+  def to_hash
+    case original_input
+    when Hash
+      original_input
+    when String
+      return {} if date.nil?
+
+      {
+        day: date.day,
+        month: date.month,
+        year: date.year,
+      }
+    else
+      {}
+    end
   end
 end
--- spec/models/date_input_spec.rb
+++ spec/models/date_input_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe DateInput do
   subject(:date_input) { described_class.new(original_input) }
 
-  let(:original_input) { "01/01/2020" }
+  let(:original_input) { "13/12/1989" }
   let(:date_parser) { instance_double(DateParser, parse: parsed_date) }
   let(:parsed_date) { Date.new(1989, 12, 13) }
 
@@ -39,6 +39,78 @@ describe DateInput do
 
       it "returns nil" do
         expect(date_input.to_iso8601).to be_nil
+      end
+    end
+  end
+
+  describe "#to_str" do
+    context "when original input isn't present" do
+      let(:original_input) { nil }
+
+      it "returns an empty string" do
+        expect(date_input.to_str).to eq("")
+      end
+    end
+
+    context "when original input is a string" do
+      let(:original_input) { "17 08 2024" }
+
+      it "returns the original input value" do
+        expect(date_input.to_str).to eq(original_input)
+      end
+    end
+
+    context "when original input is a hash" do
+      let(:original_input) { { day: "13", month: "12", year: "1989" } }
+
+      it "returns the original input value converted to a string" do
+        expect(date_input.to_str).to eq("13/12/1989")
+      end
+
+      context "and the date cannot be parsed" do
+        let(:parsed_date) { nil }
+
+        it "returns an empty string" do
+          expect(date_input.to_str).to eq("")
+        end
+      end
+    end
+  end
+
+  describe "#to_hash" do
+    context "when original input isn't present" do
+      let(:original_input) { nil }
+
+      it "returns an empty hash" do
+        expect(date_input.to_hash).to eq({})
+      end
+    end
+
+    context "when original input is a hash" do
+      let(:original_input) { { day: "13", month: "12", year: "1989" } }
+
+      it "returns the original input value" do
+        expect(date_input.to_hash).to eq(original_input)
+      end
+    end
+
+    context "when original input is a string" do
+      let(:original_input) { "13 12 1989" }
+
+      it "parses the string into a hash" do
+        expect(date_input.to_hash).to eq({
+          day: 13,
+          month: 12,
+          year: 1989,
+        })
+      end
+
+      context "and the date cannot be parsed" do
+        let(:parsed_date) { nil }
+
+        it "returns an empty hash" do
+          expect(date_input.to_hash).to eq({})
+        end
       end
     end
   end
