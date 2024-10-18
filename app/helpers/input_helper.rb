3bccdfb955c099cfffdfa82fcd7ae1b790b646cc
Christian Sutter <git@doublesignal.com>
2024-10-18 14:48:58 +0000

    All content finder: Track date errors
    This adds GA4 tracking (with `ga4-auto-tracker`) to form errors on the
    date field (the only place errors can occur).
    
    It also adds a spec for the `InputHelper` as it was getting a bit more
    complex than just a tidying of view code.

--- app/helpers/input_helper.rb
+++ app/helpers/input_helper.rb
@@ -2,8 +2,21 @@ module InputHelper
   def date_input(id, display_name, value, hint: nil, error_message: nil, legend_suffix: nil)
     legend_text = [display_name, legend_suffix].compact.join(" ")
     value ||= {}
+    container_data_attrs = { ga4_section: legend_text }
 
-    tag.div(data: { ga4_section: legend_text }) do
+    if error_message
+      container_data_attrs[:module] = "ga4-auto-tracker"
+      container_data_attrs[:ga4_auto] = {
+        event_name: "form_error",
+        type: "finder",
+        text: error_message,
+        section: legend_text,
+        action: "error",
+        tool_name: "Search GOV.UK",
+      }
+    end
+
+    tag.div(data: container_data_attrs) do
       render(
         "govuk_publishing_components/components/date_input",
         id:,
