67f3c1e495771ca1c63e47cf3204b4a935bd0eeb
Christian Sutter <git@doublesignal.com>
2024-11-13 15:20:20 +0000

    Add API endpoint for search suggestions
    This returns suggestions from the (internal) Search API v2, for
    consumption by the autocomplete component soon to be used across GOV.UK
    for site search.
    
    - Update `gds-api-adapters` to get access to new `autocomplete` method
      for Search API v2
    - Add basic API endpoint controller and route proxying to Search API v2
    - Reconfigure autocomplete setup to use v2 `source_key` and the new
      endpoint's path (with an override option for local development)
    - Update `govuk_publishing_components` for autocomplete tracking
      improvements

---
+++ app/controllers/api/autocompletes_controller.rb
@@ -0,0 +1,14 @@
+class Api::AutocompletesController < ApplicationController
+  def index
+    render json: autocomplete_response
+  end
+
+private
+
+  def autocomplete_response
+    Services
+      .search_api_v2
+      .autocomplete(params.require(:q))
+      .to_hash
+  end
+end
