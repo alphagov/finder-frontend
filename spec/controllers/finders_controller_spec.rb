58beb992ed7b997f7ed59a7ac7cb95596706431a
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-11-25 18:14:50 +0000

    Display GOV.UK Chat promo on certain pages
    We want to show a promo to the new GOV.UK Chat application on certain pages
    on GOV.UK.
    
    We already have this implemented in Government Frontend
    https://github.com/alphagov/government-frontend/pull/3303
    and Collections https://github.com/alphagov/collections/pull/3742.
    
    We're implementing it in:
    
    - finder_frontend
    - smart_answers
    - frontend
    
    This updates the finder show page to render the chat banner above the
    related links similar to the other applications.

--- spec/controllers/finders_controller_spec.rb
+++ spec/controllers/finders_controller_spec.rb
@@ -93,6 +93,39 @@ describe FindersController, type: :controller do
         get :show, params: { slug: "lunch-finder" }
         expect(response.status).to eq(406)
       end
+
+      context "when the base_path is in the GOVUK_CHAT_PROMO_BASE_PATHS constant" do
+        before do
+          stub_const("GovukChatPromoHelper::GOVUK_CHAT_PROMO_BASE_PATHS", [lunch_finder["base_path"]])
+        end
+
+        context "and the GOVUK_CHAT_PROMO_ENABLED env var is set to true" do
+          it "renders the GOV.UK Chat entry promo banner" do
+            ClimateControl.modify GOVUK_CHAT_PROMO_ENABLED: "true" do
+              get :show, params: { slug: "lunch-finder" }
+              expect(response.body).to have_selector(".gem-c-chat-entry")
+            end
+          end
+        end
+
+        context "and the GOVUK_CHAT_PROMO_ENABLED env var is set to false" do
+          it "does not render the GOV.UK Chat entry promo banner" do
+            ClimateControl.modify GOVUK_CHAT_PROMO_ENABLED: "false" do
+              get :show, params: { slug: "lunch-finder" }
+              expect(response.body).not_to have_selector(".gem-c-chat-entry")
+            end
+          end
+        end
+
+        context "and the GOVUK_CHAT_PROMO_ENABLED env var is not set" do
+          it "does not render the GOV.UK Chat entry promo banner" do
+            ClimateControl.modify GOVUK_CHAT_PROMO_ENABLED: nil do
+              get :show, params: { slug: "lunch-finder" }
+              expect(response.body).not_to have_selector(".gem-c-chat-entry")
+            end
+          end
+        end
+      end
     end
 
     describe "a finder content item with a default order exists" do
