4d751521e6924b70f8807dd614f4701e85d7acea
Kevin Dew <kevindew@me.com>
2024-12-11 17:43:50 +0000

    Add autocomplete to search index
    We've tried to add autocomplete to each site search input on GOV.UK.
    When reviewing this we noticed that one of them was missed, the one on
    https://www.gov.uk/search.
    
    This commit corrects this by adding the component here.

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -20,7 +20,7 @@ describe SearchController, type: :controller do
     context "when given search parameters" do
       it "redirects to the all content finder" do
         get :index, params: { q: "my search" }
-        
+
         destination = finder_path("search/all",
                                   params: { keywords: "my search", order: "relevance" })
         expect(response).to redirect_to(destination)
@@ -30,10 +30,31 @@ describe SearchController, type: :controller do
     context "when JSON is requested" do
       it "redirects to the all content finder" do
         get :index, format: :json
-        
+
         destination = finder_path("search/all",
                                   params: { order: "relevance", format: "json" })
         expect(response).to redirect_to(destination)
+      end
+    end
+
+    context "when GOVUK_DISABLE_SEARCH_AUTOCOMPLETE is not set" do
+      it "renders the search autocomplete component" do
+        ClimateControl.modify GOVUK_DISABLE_SEARCH_AUTOCOMPLETE: nil do
+          get :index
+
+          expect(response.body).to include("gem-c-search-with-autocomplete")
+        end
+      end
+    end
+
+    context "when GOVUK_DISABLE_SEARCH_AUTOCOMPLETE is set" do
+      it "renders the search component instead of the autocomplete component" do
+        ClimateControl.modify GOVUK_DISABLE_SEARCH_AUTOCOMPLETE: "1" do
+          get :index
+
+          expect(response.body).not_to include("gem-c-search-with-autocomplete")
+          expect(response.body).to include("gem-c-search")
+        end
       end
     end
   end
