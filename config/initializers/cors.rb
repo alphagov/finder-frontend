c52083a893adb984731c73beefafcb7ebb4a390c
Christian Sutter <git@doublesignal.com>
2025-01-08 10:10:56 +0000

    Adapt CORS policy for autocomplete_spec
    This updates the CORS policy to be more relaxed about GOV.UK subdomains,
    as well as allow Heroku preview apps.
    
    This adds a negligible risk that third-party Heroku hosted apps (as well
    as non-GDS operated GOV.UK subdomains) could in theory use the
    autocomplete endpoint from Javascript, but we don't consider this to
    have enough impact or likelihood to be worth mitigating against.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # Allow the autocomplete API to be accessed from any GOV.UK domain, including
-  # non-production ones. This enables autocomplete on CSV preview GOV.UK pages,
-  # which are hosted on assets.publishing.service.gov.uk.
-  # This also allows for local development usage.
+  # Allow the autocomplete API to be accessed from any GOV.UK domain, including non-production ones,
+  # as well as Heroku preview apps. Note that the header is rendered on some arbitrary GOV.UK
+  # subdomains (such as assets.publishing.service.gov.uk for CSV preview pages) so www alone is not
+  # enough, and we may need autocomplete on local dev environments and Heroku preview apps as well.
   allow do
-    origins %r{(www|dev|publishing\.service)\.gov\.uk\z}
+    origins %r{(\.gov\.uk|\.herokuapp.com)\z}
 
     resource "/api/search/autocomplete*",
              headers: :any,
