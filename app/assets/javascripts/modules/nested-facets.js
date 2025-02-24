aff444635eccbb24d221ea38b68abe377546833e
Laura Ghiorghisor <laura.ghiorghisor@digital.cabinet-office.gov.uk>
2025-02-24 15:44:01 +0000

    Add javascript to render nested facet values
    The logic in the js ensures that:
    - when an "all" main facet option is selected, the sub-facet is disabled;
    - only the relevant "sub" facet options are showing for a certain "main" facet selection;
    - the "all" sub facet options are default upon selecting a main option.
    
    The `_nested_facet` view gets rendered for each of the facets. For a "nested facet behaviour" to be accomplished we'd require at least two facets, a main and a sub facet. Nonetheless, we wouldn't want the js to be loaded twice, nor is it desirable to load it by default. So we're only loading it once for the main facet. In order to reach the subfacet select, we're using a document level selector in the js.
    
    In order to customize the js to work with any facet and subfacet keys, we're additionally sending along the keys as data attributes to the module.

---
+++ app/assets/javascripts/modules/nested-facets.js
@@ -0,0 +1,77 @@
+window.GOVUK = window.GOVUK || {}
+window.GOVUK.Modules = window.GOVUK.Modules || {};
+
+(function (Modules) {
+  'use strict'
+
+  function NestedFacets (module) {
+    this.module = module
+    this.mainFacet = document.querySelector('#' + this.module.getAttribute('data-main-facet-id'))
+    this.subFacet = document.querySelector('#' + this.module.getAttribute('data-sub-facet-id'))
+    this.options = this.instantiateOptions()
+  }
+
+  NestedFacets.prototype.init = function () {
+    this.updateFacets()
+    this.module.addEventListener('change', () => {
+      this.updateFacets()
+    })
+  }
+
+  NestedFacets.prototype.updateFacets = function () {
+    this.setSubFacetSelectDisabledState()
+    this.resetSubFacetValue()
+    this.populateSubFacets()
+  }
+
+  NestedFacets.prototype.setSubFacetSelectDisabledState = function () {
+    const mainFacetSelected = !!this.mainFacet.value
+    if (mainFacetSelected) {
+      this.subFacet.removeAttribute('disabled')
+    } else {
+      this.subFacet.setAttribute('disabled', true)
+    }
+  }
+
+  NestedFacets.prototype.populateSubFacets = function () {
+    const subFacetOptionsForSelectedMain = this.options[this.mainFacet.value]
+    const subFacet = this.subFacet
+    const subFacetOptions = subFacet.querySelectorAll('option')
+
+    for (let o = 0; o < subFacetOptions.length; o++) {
+      if (subFacetOptions[o].value) {
+        subFacetOptions[o].parentNode.removeChild(subFacetOptions[o])
+      }
+    }
+    if (subFacetOptionsForSelectedMain) {
+      for (let i = 0; i < subFacetOptionsForSelectedMain.length; i++) {
+        subFacet.appendChild(subFacetOptionsForSelectedMain[i])
+      }
+    }
+  }
+
+  NestedFacets.prototype.instantiateOptions = function () {
+    const options = {}
+    const optionElements = this.subFacet.querySelectorAll('option')
+
+    for (let o = 0; o < optionElements.length; o++) {
+      const mainFacetValue = optionElements[o].getAttribute('data-main-facet-value')
+
+      options[mainFacetValue] = options[mainFacetValue] || []
+      options[mainFacetValue].push(optionElements[o])
+    }
+    return options
+  }
+
+  NestedFacets.prototype.resetSubFacetValue = function () {
+    const selected = this.subFacet.options[this.subFacet.selectedIndex]
+    const mainFacetValue = this.mainFacet.value
+    const isOrphanedChild = selected && selected.getAttribute('data-main-facet-value') !== mainFacetValue
+
+    if (isOrphanedChild) {
+      this.subFacet.value = ''
+    }
+  }
+
+  Modules.NestedFacets = NestedFacets
+})(window.GOVUK.Modules)
