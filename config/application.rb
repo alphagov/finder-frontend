5f0e823c91c3c5f9b894c357dae7cfb441d7705f
Keith Lawrence <keith.lawrence@dsit.gov.uk>
2025-11-20 11:52:16 +0000

    Add GA4 Attribute optimisation
    - this used to be done as a side-effect of slimmer using nokogiri to renormalise
      the output HTML, but we need a little middleware to replicate it.

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ require "action_view/railtie"
 # require "action_cable/engine"
 require "rails/test_unit/railtie"
 
+require "govuk_publishing_components/middleware/ga4_optimise"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -49,5 +51,8 @@ module FinderFrontend
     config.assets.css_compressor = nil
 
     config.active_support.to_time_preserves_timezone = :zone
+
+    # Use the middleware to compact data-ga4-event/link attributes
+    config.middleware.use GovukPublishingComponents::Middleware::Ga4Optimise
   end
 end
